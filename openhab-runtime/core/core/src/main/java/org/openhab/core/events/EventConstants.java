88d1fc40bf05784d505fb364172c2e927b28caf1
Kai Kreuzer <kai@openhab.org>
2010-03-04 23:39:53 +0100

    Connected item registry to bus

---
+++ openhab-runtime/core/core/src/main/java/org/openhab/core/events/EventConstants.java
@@ -0,0 +1,13 @@
+package org.openhab.core.events;
+
+public interface EventConstants {
+
+	public static final String TOPIC_PREFIX = "openhab";
+
+	public static final String TOPIC_SEPERATOR = "/";
+	
+	public static final String SEND_COMMAND = "send";
+	public static final String UPDATE_COMMAND = "update";
+	public static final String REFRESH_COMMAND = "refresh";
+	
+}
