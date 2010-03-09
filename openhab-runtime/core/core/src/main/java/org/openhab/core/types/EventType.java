6e8c06b0b1a931d7d2858c4fb6565870a5c46404
Kai Kreuzer <kai@openhab.org>
2010-03-09 23:17:19 +0100

    added toString method for EventType

--- openhab-runtime/core/core/src/main/java/org/openhab/core/types/EventType.java
+++ openhab-runtime/core/core/src/main/java/org/openhab/core/types/EventType.java
@@ -1,5 +1,14 @@
 package org.openhab.core.types;
 
 public enum EventType {
-	COMMAND, UPDATE
+	COMMAND, UPDATE;
+	
+	public String toString() {
+		switch(this) {
+			case COMMAND: return "command";
+			case UPDATE:  return "update";
+		}
+		return "";
+	}
+
 }
