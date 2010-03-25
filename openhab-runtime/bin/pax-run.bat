603ee34cb22cbcf5dd18159800cdda7b60a45f2f
Kai Kreuzer <kai@openhab.org>
2010-03-25 11:49:45 +0100

    adapted build (pom.xmls) to use Maven3/tycho

--- openhab-runtime/bin/pax-run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-SETLOCAL
-set _SCRIPTS_=%~dp0
-
-java %JAVA_OPTS% -cp ".;%_SCRIPTS_%;%_SCRIPTS_%\pax-runner-1.4.0.jar" org.ops4j.pax.runner.Run %1 %2 %3 %4 %5 %6 %7 %8 %9 
--- openhab-runtime/bin/pax-rund.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-SETLOCAL
-set _SCRIPTS_=%~dp0
-
-java -cp ".;%_SCRIPTS_%;%_SCRIPTS_%\pax-runner-1.4.0.jar" org.ops4j.pax.runner.daemon.DaemonLauncher %1 %2 %3 %4 %5 %6 %7 %8 %9 
