6385007968ffa9c5ac07b16d1d385a1c8fa589f4
Kai Kreuzer <kai@openhab.org>
2010-04-14 23:37:55 +0200

    Added new item types

---
+++ openhab-runtime/core/org.openhab.core.library/src/main/java/org/openhab/core/library/items/DimmerItem.java
@@ -0,0 +1,61 @@
+/* 
+* openHAB, the open Home Automation Bus.
+* Copyright 2010, openHAB.org
+*
+* See the contributors.txt file in the distribution for a
+* full listing of individual contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.openhab.core.library.items;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openhab.core.library.types.OnOffType;
+import org.openhab.core.library.types.PercentType;
+import org.openhab.core.types.Command;
+import org.openhab.core.types.State;
+import org.openhab.core.types.UnDefType;
+
+public class DimmerItem extends SwitchItem {
+	
+	private static List<Class<? extends State>> acceptedDataTypes = new ArrayList<Class<? extends State>>();
+	private static List<Class<? extends Command>> acceptedCommandTypes = new ArrayList<Class<? extends Command>>();
+
+	static {
+		acceptedDataTypes.add(OnOffType.class);
+		acceptedDataTypes.add(PercentType.class);
+		acceptedDataTypes.add(UnDefType.class);
+
+		acceptedCommandTypes.add(OnOffType.class);
+		acceptedCommandTypes.add(PercentType.class);
+	}
+	
+	public DimmerItem(String name) {
+		super(name);
+	}
+
+	public void send(PercentType command) {
+		internalSend(command);
+	}
+
+	public List<Class<? extends State>> getAcceptedDataTypes() {
+		return acceptedDataTypes;
+	}
+
+	public List<Class<? extends Command>> getAcceptedCommandTypes() {
+		return acceptedCommandTypes;
+	}
+}
---
+++ openhab-runtime/core/org.openhab.core.library/src/main/java/org/openhab/core/library/items/MeasurementItem.java
@@ -0,0 +1,53 @@
+/* 
+* openHAB, the open Home Automation Bus.
+* Copyright 2010, openHAB.org
+*
+* See the contributors.txt file in the distribution for a
+* full listing of individual contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.openhab.core.library.items;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openhab.core.items.GenericItem;
+import org.openhab.core.library.types.DecimalType;
+import org.openhab.core.types.Command;
+import org.openhab.core.types.State;
+import org.openhab.core.types.UnDefType;
+
+public class MeasurementItem extends GenericItem {
+	
+	private static List<Class<? extends State>> acceptedDataTypes = new ArrayList<Class<? extends State>>();
+	private static List<Class<? extends Command>> acceptedCommandTypes = new ArrayList<Class<? extends Command>>();
+
+	static {
+		acceptedDataTypes.add(DecimalType.class);
+		acceptedDataTypes.add(UnDefType.class);
+	}
+	
+	public MeasurementItem(String name) {
+		super(name);
+	}
+
+	public List<Class<? extends State>> getAcceptedDataTypes() {
+		return acceptedDataTypes;
+	}
+
+	public List<Class<? extends Command>> getAcceptedCommandTypes() {
+		return acceptedCommandTypes;
+	}
+}
