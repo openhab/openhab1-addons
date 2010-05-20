24e4e9f8ee69b4fc5de0c711812de49e5803bf54
Kai Kreuzer <kai@openhab.org>
2010-05-20 21:29:48 +0200

    added logging information

--- bundles/designer/org.openhab.designer.core/src/main/java/org/openhab/designer/core/internal/preferences/PreferenceInitializer.java
+++ bundles/designer/org.openhab.designer.core/src/main/java/org/openhab/designer/core/internal/preferences/PreferenceInitializer.java
@@ -6,9 +6,13 @@ import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.core.runtime.preferences.IScopeContext;
 import org.openhab.designer.core.CoreActivator;
 import org.openhab.designer.core.DesignerCoreConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PreferenceInitializer extends AbstractPreferenceInitializer {
 
+	private static final Logger logger = LoggerFactory.getLogger(PreferenceInitializer.class);
+	
 	public PreferenceInitializer() {
 	}
 
@@ -16,6 +20,11 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
 		IScopeContext context = new DefaultScope();
 		IEclipsePreferences node = context.getNode(CoreActivator.getDefault().getBundle().getSymbolicName());
 		String folderPath = System.getProperty("bundles.configuration.location");
+		if(folderPath==null) {
+			logger.warn("System property 'bundles.configuration.location' has not been set!");
+		} else {
+			logger.debug("System property 'bundles.configuration.location' set to {}.", folderPath);			
+		}
 		node.put(DesignerCoreConstants.CONFIG_FOLDER_PREFERENCE, folderPath);
 	}
 
