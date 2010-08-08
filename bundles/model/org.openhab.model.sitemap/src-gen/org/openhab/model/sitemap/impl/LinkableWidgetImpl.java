a1ad6d09ba76fc6e1ad3c6cd9619bbf708d2f7b3
Kai Kreuzer <kai@openhab.org>
2010-08-08 14:45:54 +0200

    added linkable widget

---
+++ bundles/model/org.openhab.model.sitemap/src-gen/org/openhab/model/sitemap/impl/LinkableWidgetImpl.java
@@ -0,0 +1,169 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+
+ */
+package org.openhab.model.sitemap.impl;
+
+import java.util.Collection;
+
+import org.eclipse.emf.common.notify.NotificationChain;
+
+import org.eclipse.emf.common.util.EList;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.InternalEObject;
+
+import org.eclipse.emf.ecore.util.EObjectContainmentEList;
+import org.eclipse.emf.ecore.util.InternalEList;
+
+import org.openhab.model.sitemap.LinkableWidget;
+import org.openhab.model.sitemap.SitemapPackage;
+import org.openhab.model.sitemap.Widget;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Linkable Widget</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * <ul>
+ *   <li>{@link org.openhab.model.sitemap.impl.LinkableWidgetImpl#getChildren <em>Children</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @generated
+ */
+public class LinkableWidgetImpl extends WidgetImpl implements LinkableWidget
+{
+  /**
+   * The cached value of the '{@link #getChildren() <em>Children</em>}' containment reference list.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see #getChildren()
+   * @generated
+   * @ordered
+   */
+  protected EList<Widget> children;
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  protected LinkableWidgetImpl()
+  {
+    super();
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  protected EClass eStaticClass()
+  {
+    return SitemapPackage.Literals.LINKABLE_WIDGET;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public EList<Widget> getChildren()
+  {
+    if (children == null)
+    {
+      children = new EObjectContainmentEList<Widget>(Widget.class, this, SitemapPackage.LINKABLE_WIDGET__CHILDREN);
+    }
+    return children;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public NotificationChain eInverseRemove(InternalEObject otherEnd, int featureID, NotificationChain msgs)
+  {
+    switch (featureID)
+    {
+      case SitemapPackage.LINKABLE_WIDGET__CHILDREN:
+        return ((InternalEList<?>)getChildren()).basicRemove(otherEnd, msgs);
+    }
+    return super.eInverseRemove(otherEnd, featureID, msgs);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public Object eGet(int featureID, boolean resolve, boolean coreType)
+  {
+    switch (featureID)
+    {
+      case SitemapPackage.LINKABLE_WIDGET__CHILDREN:
+        return getChildren();
+    }
+    return super.eGet(featureID, resolve, coreType);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @SuppressWarnings("unchecked")
+  @Override
+  public void eSet(int featureID, Object newValue)
+  {
+    switch (featureID)
+    {
+      case SitemapPackage.LINKABLE_WIDGET__CHILDREN:
+        getChildren().clear();
+        getChildren().addAll((Collection<? extends Widget>)newValue);
+        return;
+    }
+    super.eSet(featureID, newValue);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public void eUnset(int featureID)
+  {
+    switch (featureID)
+    {
+      case SitemapPackage.LINKABLE_WIDGET__CHILDREN:
+        getChildren().clear();
+        return;
+    }
+    super.eUnset(featureID);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public boolean eIsSet(int featureID)
+  {
+    switch (featureID)
+    {
+      case SitemapPackage.LINKABLE_WIDGET__CHILDREN:
+        return children != null && !children.isEmpty();
+    }
+    return super.eIsSet(featureID);
+  }
+
+} //LinkableWidgetImpl
