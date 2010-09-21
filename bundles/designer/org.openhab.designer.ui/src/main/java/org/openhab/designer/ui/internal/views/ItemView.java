399334b2b1c8da0a183cb6ca790d63adaaa52dc2
Kai Kreuzer <kai@openhab.org>
2010-09-21 22:09:58 +0200

    removed old javadoc

--- bundles/designer/org.openhab.designer.ui/src/main/java/org/openhab/designer/ui/internal/views/ItemView.java
+++ bundles/designer/org.openhab.designer.ui/src/main/java/org/openhab/designer/ui/internal/views/ItemView.java
@@ -57,24 +57,6 @@ import org.openhab.core.items.ItemRegistry;
 import org.openhab.designer.ui.UIActivator;
 
 
-/**
- * This sample class demonstrates how to plug-in a new
- * workbench view. The view shows data obtained from the
- * model. The sample creates a dummy model on the fly,
- * but a real implementation would connect to the model
- * available either in this or another plug-in (e.g. the workspace).
- * The view is connected to the model using a content provider.
- * <p>
- * The view uses a label provider to define how model
- * objects should be presented in the view. Each
- * view can present the same model objects using
- * different labels and icons, if needed. Alternatively,
- * a single label provider can be shared between views
- * in order to ensure that objects of the same type are
- * presented in the same way everywhere.
- * <p>
- */
-
 public class ItemView extends ViewPart {
 
 	/**
