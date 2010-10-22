601985a3637eb308eda27ef038ae06d90a840783
Kai Kreuzer <kai@openhab.org>
2010-10-22 21:51:24 +0200

    added license texts for bluetooth binding

--- bundles/binding/bluetooth/src/main/java/org/openhab/binding/bluetooth/internal/BluetoothBinding.java
+++ bundles/binding/bluetooth/src/main/java/org/openhab/binding/bluetooth/internal/BluetoothBinding.java
@@ -1,3 +1,32 @@
+/**
+ * openHAB, the open Home Automation Bus.
+ * Copyright (C) 2010, openHAB.org <admin@openhab.org>
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ * Additional permission under GNU GPL version 3 section 7
+ *
+ * If you modify this Program, or any covered work, by linking or
+ * combining it with Eclipse (or a modified version of that library),
+ * containing parts covered by the terms of the Eclipse Public License
+ * (EPL), the licensors of this Program grant you additional permission
+ * to convey the resulting work.
+ */
+
 package org.openhab.binding.bluetooth.internal;
 
 import java.util.HashMap;
@@ -6,8 +35,10 @@ import java.util.Map;
 import org.openhab.binding.bluetooth.BluetoothEventHandler;
 import org.openhab.core.events.EventPublisher;
 import org.openhab.core.items.Item;
+import org.openhab.core.library.items.NumberItem;
 import org.openhab.core.library.items.StringItem;
 import org.openhab.core.library.items.SwitchItem;
+import org.openhab.core.library.types.DecimalType;
 import org.openhab.core.library.types.OnOffType;
 import org.openhab.core.library.types.StringType;
 import org.openhab.model.item.binding.BindingConfigParseException;
@@ -39,6 +70,9 @@ public class BluetoothBinding implements BluetoothEventHandler, BindingConfigRea
 	/** stores information about string items. The map has this content structure: context -> itemName */ 
 	private Map<String, String> stringItems = new HashMap<String, String>();
 	
+	/** stores information about measurement items. The map has this content structure: context -> itemName */ 
+	private Map<String, String> measurementItems = new HashMap<String, String>();
+
 	private EventPublisher eventPublisher;
 	
 	public void setEventPublisher(EventPublisher eventPublisher) {
@@ -85,7 +119,9 @@ public class BluetoothBinding implements BluetoothEventHandler, BindingConfigRea
 		if(eventPublisher!=null) {
 			// build a comma separated list of all devices in range
 			StringBuilder sb = new StringBuilder();
+			int noOfDevices = 0;
 			for(BluetoothDevice device : devices) {
+				noOfDevices++;
 				if(!device.getFriendlyName().trim().isEmpty()) {
 					sb.append(device.getFriendlyName());
 				} else {
@@ -100,6 +136,10 @@ public class BluetoothBinding implements BluetoothEventHandler, BindingConfigRea
 			for(String itemName : stringItems.values()) {
 				eventPublisher.postUpdate(itemName, StringType.valueOf(deviceList));
 			}
+			for(String itemName : measurementItems.values()) {
+				eventPublisher.postUpdate(itemName, new DecimalType(noOfDevices));
+			}
+			
 		}
 	}
 
@@ -123,18 +163,22 @@ public class BluetoothBinding implements BluetoothEventHandler, BindingConfigRea
 		if(item instanceof StringItem && bindingConfig.equals("*")) {
 			stringItems.put(context, item.getName());
 		}
+		if(item instanceof NumberItem && bindingConfig.equals("*")) {
+			measurementItems.put(context, item.getName());
+		}
 	}
 
 	@Override
 	public boolean isActive() {
 		// only say that we are active if there are any items registered for that binding
-		return switchItems.size() > 0 || stringItems.size() > 0;
+		return switchItems.size() > 0 || stringItems.size() > 0 || measurementItems.size() > 0;
 	}
 
 	@Override
 	public void removeConfigurations(String context) {
 		switchItems.remove(context);
 		stringItems.remove(context);
+		measurementItems.remove(context);
 	}
 
 }
