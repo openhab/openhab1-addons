b136d2ccd400733883096ca9706497c21e2d0b4d
kai <devnull@localhost>
2011-02-20 12:20:06 +0100

    Fixed KNX binding provider bugs after generic provider refactoring

--- bundles/binding/org.openhab.binding.knx/src/main/java/org/openhab/binding/knx/internal/KNXActivator.java
+++ bundles/binding/org.openhab.binding.knx/src/main/java/org/openhab/binding/knx/internal/KNXActivator.java
@@ -31,27 +31,27 @@ package org.openhab.binding.knx.internal;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Extension of the default OSGi bundle activator
  */
-public final class KNXActivator
-    implements BundleActivator
-{
-    /**
-     * Called whenever the OSGi framework starts our bundle
-     */
-    public void start( BundleContext bc )
-        throws Exception
-    {
-    }
+public final class KNXActivator implements BundleActivator {
+	
+	private static Logger logger = LoggerFactory.getLogger(KNXActivator.class); 
 
-    /**
-     * Called whenever the OSGi framework stops our bundle
-     */
-    public void stop( BundleContext bc )
-        throws Exception
-    {
-    }
+	/**
+	 * Called whenever the OSGi framework starts our bundle
+	 */
+	public void start(BundleContext bc) throws Exception {
+		logger.debug("KNX binding has been started.");
+	}
+
+	/**
+	 * Called whenever the OSGi framework stops our bundle
+	 */
+	public void stop(BundleContext bc) throws Exception {
+		logger.debug("KNX binding has been stopped.");
+	}
 }
-
