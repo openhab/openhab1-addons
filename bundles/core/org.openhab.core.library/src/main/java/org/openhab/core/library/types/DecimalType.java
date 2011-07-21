53e3e48e07e8ccde1b6832c56875d28862e26f4b
Kai <devnull@localhost>
2011-07-21 12:53:30 +0200

    Fixed bug in HTTP out binding when processing decimal types

--- bundles/core/org.openhab.core.library/src/main/java/org/openhab/core/library/types/DecimalType.java
+++ bundles/core/org.openhab.core.library/src/main/java/org/openhab/core/library/types/DecimalType.java
@@ -87,4 +87,29 @@ public class DecimalType implements PrimitiveType, State, Command {
 	public BigDecimal toBigDecimal() {
 		return value;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DecimalType other = (DecimalType) obj;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
 }
