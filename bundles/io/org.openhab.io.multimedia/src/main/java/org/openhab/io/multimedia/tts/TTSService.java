03958e8d2b0df7aef8d8d02b859a1c8344fda42e
Kai <devnull@localhost>
2011-08-24 22:32:16 +0200

    renamed speak to say

--- bundles/io/org.openhab.io.multimedia/src/main/java/org/openhab/io/multimedia/tts/TTSService.java
+++ bundles/io/org.openhab.io.multimedia/src/main/java/org/openhab/io/multimedia/tts/TTSService.java
@@ -39,10 +39,10 @@ package org.openhab.io.multimedia.tts;
 public interface TTSService {
 
 	/**
-	 * Speaks the text with a given voice
+	 * Says the text with a given voice
 	 * 
 	 * @param text the text to speak
 	 * @param voice the name of the voice to use or null, if the default voice should be used
 	 */
-	void speak(String text, String voice);
+	void say(String text, String voice);
 }
