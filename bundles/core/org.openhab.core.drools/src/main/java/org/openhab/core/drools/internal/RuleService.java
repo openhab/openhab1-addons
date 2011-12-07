38304d890a2431eaebb36d43bddc97885d387e19
Kai <devnull@localhost>
2011-12-07 22:29:34 +0100

    corrected path to drools changeset

--- bundles/core/org.openhab.core.drools/src/main/java/org/openhab/core/drools/internal/RuleService.java
+++ bundles/core/org.openhab.core.drools/src/main/java/org/openhab/core/drools/internal/RuleService.java
@@ -79,7 +79,7 @@ import org.slf4j.LoggerFactory;
 
 public class RuleService extends AbstractActiveService implements ManagedService, EventHandler, ItemRegistryChangeListener, StateChangeListener {
 
-	private static final String RULES_CHANGESET = "org/openhab/core/rules/changeset.xml";
+	private static final String RULES_CHANGESET = "org/openhab/core/drools/changeset.xml";
 
 	static private final Logger logger = LoggerFactory.getLogger(RuleService.class);
 	
