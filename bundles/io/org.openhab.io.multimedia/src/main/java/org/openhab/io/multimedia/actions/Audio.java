e1a501e57a59879b89cab4ac5872f86077cc017e
Kai <devnull@localhost>
2012-01-17 20:54:58 +0100

    added rule to control the volume

--- bundles/io/org.openhab.io.multimedia/src/main/java/org/openhab/io/multimedia/actions/Audio.java
+++ bundles/io/org.openhab.io.multimedia/src/main/java/org/openhab/io/multimedia/actions/Audio.java
@@ -52,6 +52,8 @@ import javazoom.jl.player.Player;
 import org.apache.commons.collections.Closure;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
+import org.openhab.core.library.types.DecimalType;
+import org.openhab.core.library.types.PercentType;
 import org.openhab.io.multimedia.internal.MultimediaActivator;
 import org.openhab.io.multimedia.tts.TTSService;
 import org.osgi.framework.BundleContext;
@@ -174,6 +176,10 @@ public class Audio {
 		} else {
 			setMasterVolumeJavaSound(volume);
 		}
+	}
+
+	static public void setMasterVolume(final PercentType percent) throws IOException {
+		setMasterVolume(percent.toBigDecimal().floatValue()/100f);
 	}
 
 	private static void setMasterVolumeMac(float volume) throws IOException {
