d819e4c0748aad7ed80b002b21b0ad8dad2d54b0
teichsta <devnull@localhost>
2012-01-24 23:11:12 +0100

    fix old- and newstate where inverted for changeTriggers

--- bundles/model/org.openhab.model.rule/src/org/openhab/model/rule/internal/engine/RuleEngine.java
+++ bundles/model/org.openhab.model.rule/src/org/openhab/model/rule/internal/engine/RuleEngine.java
@@ -190,7 +190,7 @@ public class RuleEngine implements EventHandler, ItemRegistryChangeListener, Sta
 			
 			// and now the rules, which only want to see state changes
 			if(triggerManager!=null) {
-				Iterable<Rule> rules = triggerManager.getRules(CHANGE, item, newState, oldState);
+				Iterable<Rule> rules = triggerManager.getRules(CHANGE, item, oldState, newState);
 				RuleEvaluationContext context = new RuleEvaluationContext();
 				context.newValue(QualifiedName.create(RuleContextHelper.VAR_PREVIOUS_STATE), oldState);
 				executeRules(rules, context);
