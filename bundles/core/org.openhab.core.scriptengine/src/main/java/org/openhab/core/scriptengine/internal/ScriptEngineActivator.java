dcba908b32f664019e98b86fe5752d1271df5f69
teichsta <devnull@localhost>
2012-01-30 13:33:12 +0100

    initialer checkin

---
+++ bundles/core/org.openhab.core.scriptengine/src/main/java/org/openhab/core/scriptengine/internal/ScriptEngineActivator.java
@@ -0,0 +1,76 @@
+package org.openhab.core.scriptengine.internal;
+/**
+ * openHAB, the open Home Automation Bus.
+ * Copyright (C) 2010-2012, openHAB.org <admin@openhab.org>
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ * Additional permission under GNU GPL version 3 section 7
+ *
+ * If you modify this Program, or any covered work, by linking or
+ * combining it with Eclipse (or a modified version of that library),
+ * containing parts covered by the terms of the Eclipse Public License
+ * (EPL), the licensors of this Program grant you additional permission
+ * to convey the resulting work.
+ */
+
+
+import org.openhab.core.items.ItemRegistry;
+import org.openhab.core.scriptengine.ScriptEngine;
+import org.openhab.model.core.ModelRepository;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.util.tracker.ServiceTracker;
+
+/**
+ * Extension of the default OSGi bundle activator
+ * 
+ * @author Thomas.Eichstaedt-Engelen
+ * @since 0.9.1
+ */
+public class ScriptEngineActivator implements BundleActivator {
+	
+	public static final String SCRIPT_FILEEXT = "script";
+	
+	public static ServiceTracker<ItemRegistry, ItemRegistry> itemRegistryTracker;
+	public static ServiceTracker<ModelRepository, ModelRepository> modelRepositoryTracker;
+	public static ServiceTracker<ScriptEngine, ScriptEngine> scriptEngineTracker;
+
+	/**
+	 * Called whenever the OSGi framework starts our bundle
+	 */
+	public void start(BundleContext bc) throws Exception {
+		itemRegistryTracker = new ServiceTracker<ItemRegistry, ItemRegistry>(bc, ItemRegistry.class, null);
+		itemRegistryTracker.open();
+
+		modelRepositoryTracker = new ServiceTracker<ModelRepository, ModelRepository>(bc, ModelRepository.class, null);
+		modelRepositoryTracker.open();
+
+		scriptEngineTracker = new ServiceTracker<ScriptEngine, ScriptEngine>(bc, ScriptEngine.class, null);
+		scriptEngineTracker.open();
+	}
+
+	/**
+	 * Called whenever the OSGi framework stops our bundle
+	 */
+	public void stop(BundleContext bc) throws Exception {
+		itemRegistryTracker.close();
+		modelRepositoryTracker.close();
+		scriptEngineTracker.close();
+	}
+
+}
