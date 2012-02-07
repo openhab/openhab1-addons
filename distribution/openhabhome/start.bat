e45ed43a380ee00f6b1943c4d0b8b444b09f339a
teichsta <devnull@localhost>
2012-02-07 16:56:32 +0100

    made the port-handling consistent so a discovery bundle can detect the ports jetty is started on correctly

--- distribution/openhabhome/start.bat
+++ distribution/openhabhome/start.bat
@@ -8,4 +8,4 @@ for /f "delims= tokens=1" %%c in ('dir /B /S /OD %ECLIPSEHOME%\plugins\org.eclip
  
 :: start Eclipse w/ java
 echo Launching the openHAB runtime...
-java -Declipse.ignoreApp=true -Dosgi.noShutdown=true -D32 -Djetty.home=. -Dlogback.configurationFile=logs/logback.xml -Dfelix.fileinstall.dir=addons -Djava.library.path=lib -Djava.security.auth.login.config=./etc/login.conf -Dorg.quartz.properties=./etc/quartz.properties -jar %EQUINOXJAR% %* -console
+java -Declipse.ignoreApp=true -Dosgi.noShutdown=true -Djetty.port=8080 -Djetty.port.ssl=8443 -Djetty.home=. -Dlogback.configurationFile=logs/logback.xml -Dfelix.fileinstall.dir=addons -Djava.library.path=lib -Djava.security.auth.login.config=./etc/login.conf -Dorg.quartz.properties=./etc/quartz.properties -jar %EQUINOXJAR% %* -console
