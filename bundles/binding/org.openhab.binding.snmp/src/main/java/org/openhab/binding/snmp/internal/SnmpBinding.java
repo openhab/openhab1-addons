64141544fd8958e565bfce8a4952b2ebc3f8e972
teichsta <devnull@localhost>
2012-02-29 21:43:57 +0100

    fix - now all items of a provider are being processed

--- bundles/binding/org.openhab.binding.snmp/src/main/java/org/openhab/binding/snmp/internal/SnmpBinding.java
+++ bundles/binding/org.openhab.binding.snmp/src/main/java/org/openhab/binding/snmp/internal/SnmpBinding.java
@@ -49,7 +49,6 @@ import org.snmp4j.MessageDispatcher;
 import org.snmp4j.MessageDispatcherImpl;
 import org.snmp4j.PDU;
 import org.snmp4j.Snmp;
-import org.snmp4j.TransportMapping;
 import org.snmp4j.mp.MPv1;
 import org.snmp4j.mp.MPv2c;
 import org.snmp4j.security.Priv3DES;
@@ -182,7 +181,6 @@ public class SnmpBinding implements ManagedService, CommandResponder {
 					OID oid = provider.getOID(itemName);				
 					Variable variable = pdu.getVariable(oid);
 					eventPublisher.postUpdate(itemName, new StringType(variable.toString()));
-					break;
 				}
 			}
 			
