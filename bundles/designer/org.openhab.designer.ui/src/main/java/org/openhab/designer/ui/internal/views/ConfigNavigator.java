a7c8715ec11cf28cd938040f15486a4c03f3f33f
Kai <devnull@localhost>
2012-04-19 21:04:07 +0200

    automatically refreshing the navigator on file changes in the designer

--- bundles/designer/org.openhab.designer.ui/src/main/java/org/openhab/designer/ui/internal/views/ConfigNavigator.java
+++ bundles/designer/org.openhab.designer.ui/src/main/java/org/openhab/designer/ui/internal/views/ConfigNavigator.java
@@ -28,14 +28,31 @@
  */
 package org.openhab.designer.ui.internal.views;
 
+import org.eclipse.core.resources.IResourceChangeEvent;
+import org.eclipse.core.resources.IResourceChangeListener;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.actions.ActionGroup;
 import org.eclipse.ui.navigator.CommonNavigator;
 import org.openhab.designer.core.config.ConfigurationFolderProvider;
 
 public class ConfigNavigator extends CommonNavigator {
+
+	private IResourceChangeListener changeListener;
 	
 	@Override
 	protected Object getInitialInput() {
+		changeListener = new IResourceChangeListener() {
+			public void resourceChanged(IResourceChangeEvent event) {
+				Display.getDefault().asyncExec(new Runnable() {
+					public void run() {
+						getCommonViewer().refresh();
+					}
+				});
+			}
+		};
+		ResourcesPlugin.getWorkspace().addResourceChangeListener(changeListener);
+		
 		try {
 			return ConfigurationFolderProvider.getRootConfigurationFolder().getProject();
 		} catch (Exception e) {
@@ -48,4 +65,10 @@ public class ConfigNavigator extends CommonNavigator {
 		return new ConfigNavigatorActionGroup(getCommonViewer());
 	}
 
+	@Override
+	public void dispose() {
+		super.dispose();
+		ResourcesPlugin.getWorkspace().removeResourceChangeListener(changeListener);
+		changeListener = null;
+	}
 }
