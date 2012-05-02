363850cb271982897ffe28debd1ebefb9072ce0b
teichsta <devnull@localhost>
2012-05-02 12:22:40 +0200

    Changes to amend with previous

--- bundles/model/org.openhab.model.script/xtend-gen/org/openhab/model/script/jvmmodel/ScriptIdentifiableSimpleNameProvider.java
+++ bundles/model/org.openhab.model.script/xtend-gen/org/openhab/model/script/jvmmodel/ScriptIdentifiableSimpleNameProvider.java
@@ -1,6 +1,5 @@
 package org.openhab.model.script.jvmmodel;
 
-import java.util.Arrays;
 import org.eclipse.xtext.common.types.JvmIdentifiableElement;
 import org.eclipse.xtext.common.types.JvmType;
 import org.eclipse.xtext.xbase.featurecalls.IdentifiableSimpleNameProvider;
@@ -19,11 +18,8 @@ public class ScriptIdentifiableSimpleNameProvider extends IdentifiableSimpleName
   public String getSimpleName(final JvmIdentifiableElement element) {
     if (element instanceof JvmType) {
       return _getSimpleName((JvmType)element);
-    } else if (element != null) {
-      return _getSimpleName(element);
     } else {
-      throw new IllegalArgumentException("Unhandled parameter types: " +
-        Arrays.<Object>asList(element).toString());
+      return _getSimpleName(element);
     }
   }
 }
