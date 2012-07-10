848ce14b6025a6835a26c584ecd850103c3141f7
Kai <kai@openhab.org>
2012-07-10 18:50:21 +0200

    Improvement of StringType (Thanks to Klaus Halfmann)

--- bundles/core/org.openhab.core.library.test/src/test/java/org/openhab/core/library/types/StringTypeTest.java
+++ bundles/core/org.openhab.core.library.test/src/test/java/org/openhab/core/library/types/StringTypeTest.java
@@ -28,7 +28,7 @@
  */
 package org.openhab.core.library.types;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
@@ -41,17 +41,22 @@ public class StringTypeTest {
 
 	@Test
 	public void testEquals() {
+		StringType empty     = new StringType("");
 		StringType expected1 = new StringType("expected1");
 		StringType expected2 = new StringType("expected2");
 		
+		assertEquals(empty.hashCode()    , StringType.EMPTY.hashCode());
 		assertEquals(expected1.hashCode(), new StringType("expected1").hashCode());
 		assertEquals(expected2.hashCode(), new StringType("expected2").hashCode());
-		assertEquals(false, expected1.hashCode() == new StringType("expected2").hashCode());
+		assertFalse(expected1.hashCode() == new StringType("expected2").hashCode());
 		
-		assertEquals(true, expected1.equals(new StringType("expected1")));
-		assertEquals(true, expected2.equals(new StringType("expected2")));
+		assertEquals(empty    , StringType.EMPTY);
+		assertEquals(expected1, new StringType("expected1"));
+		assertEquals(expected2, new StringType("expected2"));
 		assertEquals(false, expected1.equals(new StringType("expected2")));
 		assertEquals(false, expected2.equals(new StringType("expected1")));
+		assertEquals(false, expected1.equals(StringType.EMPTY));
+		assertEquals(false, expected2.equals(StringType.EMPTY));
 		
 		assertEquals(true, expected1.equals("expected1"));
 		assertEquals(false, expected1.equals("expected2"));
