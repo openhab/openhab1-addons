8f7028094afb205d04a7ad7b2e1aaf6308f35c60
teichsta <devnull@localhost>
2012-08-02 14:31:51 +0200

    fix for #94 - removed regex completely to allow any configuration string. OwServer will complain later if id isn't correct.

--- bundles/persistence/org.openhab.persistence.db4o/src/main/java/org/openhab/persistence/db4o/internal/Db4oPersistenceService.java
+++ bundles/persistence/org.openhab.persistence.db4o/src/main/java/org/openhab/persistence/db4o/internal/Db4oPersistenceService.java
@@ -29,15 +29,37 @@
 
 package org.openhab.persistence.db4o.internal;
 
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.repeatSecondlyForever;
+import static org.quartz.TriggerBuilder.newTrigger;
+import static org.quartz.impl.matchers.GroupMatcher.jobGroupEquals;
+
 import java.io.File;
+import java.io.FilenameFilter;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
+import java.util.Set;
 
 import org.openhab.core.items.Item;
 import org.openhab.core.persistence.FilterCriteria;
 import org.openhab.core.persistence.HistoricItem;
 import org.openhab.core.persistence.PersistenceService;
 import org.openhab.core.persistence.QueryablePersistenceService;
+import org.quartz.CronScheduleBuilder;
+import org.quartz.CronTrigger;
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.Job;
+import org.quartz.JobDetail;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.quartz.JobKey;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.SimpleTrigger;
+import org.quartz.impl.StdSchedulerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -46,6 +68,7 @@ import com.db4o.ObjectContainer;
 import com.db4o.ObjectSet;
 import com.db4o.ext.DatabaseClosedException;
 import com.db4o.ext.Db4oException;
+import com.db4o.ext.ExtObjectContainer;
 import com.db4o.query.Query;
 
 
@@ -60,8 +83,19 @@ public class Db4oPersistenceService implements QueryablePersistenceService {
 	private static final Logger logger = LoggerFactory.getLogger(Db4oPersistenceService.class);
 	
 	private static final String SERVICE_NAME = "db4o";
+	
 	private static final String DB_FOLDER_NAME = "etc/db4o";
 	private static final String DB_FILE_NAME = "store.db4o";
+
+	private static final String SCHEDULER_GROUP = "Db4o-Schedulergroup";
+
+	private int commitInterval = 5;
+
+	/** the backup interval as Cron-Expression (optional, defaults to '0 0 1 * * ?' which means every morning at 1 o'clock) */
+	private static String backupInterval = "0 0 1 * * ?";
+	
+	/** the amount of backup files allowed in DB_FOLDER_NAME (optional, defaults to '7')*/
+	private static final int maxBackups = 7;
 
 	private static ObjectContainer db;
 	
@@ -77,9 +111,13 @@ public class Db4oPersistenceService implements QueryablePersistenceService {
 		}
 	    openDbFile();
 	    Db4oItem.configure(db.ext().configure());
+	    
+	    scheduleJob();
 	}
 
 	public void deactivate() {
+		cancelAllJobs();
+		
 		if(db!=null) {
 			db.close();
 			db = null;
@@ -98,9 +136,9 @@ public class Db4oPersistenceService implements QueryablePersistenceService {
 		historicItem.setName(alias);
 		historicItem.setState(item.getState());
 		historicItem.setTimestamp(new Date());
+		
 		try {
 			db.store(historicItem);	
-			db.commit();
 			logger.debug("Stored item state '{}' -> '{}'", new String[] {historicItem.getName(), historicItem.getState().toString() } );
 		} catch(Db4oException e) {
 			db.rollback();
@@ -173,8 +211,156 @@ public class Db4oPersistenceService implements QueryablePersistenceService {
 		return query;
 	}
 
-	private void openDbFile() {
+	private static void openDbFile() {
 		db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), DB_FOLDER_NAME + File.separator + DB_FILE_NAME);
+	}
+	
+
+	/**
+	 * Schedules new quartz scheduler jobs for committing transactions and 
+	 * backing up the database
+	 */
+	private void scheduleJob() {
+		try {
+			Scheduler sched = StdSchedulerFactory.getDefaultScheduler();
+			
+			// schedule commit-job
+			JobDetail job = newJob(CommitJob.class)
+				.withIdentity("Commit_Transaction", SCHEDULER_GROUP)
+			    .build();
+
+			SimpleTrigger trigger = newTrigger()
+			    .withIdentity("Commit_Transaction", SCHEDULER_GROUP)
+			    .withSchedule(repeatSecondlyForever(commitInterval))
+			    .build();
+
+			sched.scheduleJob(job, trigger);
+			logger.debug("Scheduled Commit-Job with interval {}sec.", commitInterval);
+			
+			// schedule backup-job
+			JobDetail backupJob = newJob(CommitJob.class)
+					.withIdentity("Backup_DB", SCHEDULER_GROUP)
+				    .build();
+
+			CronTrigger backupTrigger = newTrigger()
+				    .withIdentity("Backup_DB", SCHEDULER_GROUP)
+				    .withSchedule(CronScheduleBuilder.cronSchedule(backupInterval))
+				    .build();
+
+			sched.scheduleJob(backupJob, backupTrigger);
+			logger.debug("Scheduled Backup-Job with cron expression '{}'", backupInterval);
+		} catch (SchedulerException e) {
+			logger.warn("Could not create Job: {}", e.getMessage());
+		}		
+	}
+
+	/**
+	 * Delete all quartz scheduler jobs of the group <code>Dropbox</code>.
+	 */
+	private void cancelAllJobs() {
+		try {
+			Scheduler sched = StdSchedulerFactory.getDefaultScheduler();
+			Set<JobKey> jobKeys = sched.getJobKeys(jobGroupEquals(SCHEDULER_GROUP));
+			if (jobKeys.size() > 0) {
+				sched.deleteJobs(new ArrayList<JobKey>(jobKeys));
+				logger.debug("Found {} Commit-Jobs to delete from DefaulScheduler (keys={})", jobKeys.size(), jobKeys);
+			}
+		} catch (SchedulerException e) {
+			logger.warn("Couldn't remove Commit-Job: {}", e.getMessage());
+		}		
+	}
+	
+	
+	/**
+	 * A quartz scheduler job to commit the db4o transaction frequently. There
+	 * can be only one instance of a specific job type running at the same time.
+	 * 
+	 * @author Thomas.Eichstaedt-Engelen
+	 * @since 1.0.0
+	 */
+	@DisallowConcurrentExecution
+	public static class CommitJob implements Job {
+		
+		@Override
+		public void execute(JobExecutionContext context) throws JobExecutionException {
+			long startTime = System.currentTimeMillis();
+			
+			try {
+				ExtObjectContainer extObjectContainer = db.ext();
+				System.err.println(extObjectContainer.version());
+				db.commit();
+				logger.debug("succesfully commited db4o transaction in {}ms", System.currentTimeMillis() - startTime);
+			} catch(Db4oException e) {
+				db.rollback();
+				logger.warn("Error commiting transaction : {}", e.getMessage());
+			}
+		}
+		
+	}
+	
+	/**
+	 * A quartz scheduler job to backup the db4o database frequently. It also
+	 * removes obsolete backup files if the maximum amount is reached. There
+	 * can be only one instance of a specific job type running at the same time.
+	 * 
+	 * @author Thomas.Eichstaedt-Engelen
+	 * @since 1.0.0
+	 */
+	@DisallowConcurrentExecution
+	public static class BackupJob implements Job {
+		
+		final static SimpleDateFormat DATE_FORMATTER = new SimpleDateFormat("yyyyMMddHHmmss");
+
+		@Override
+		public void execute(JobExecutionContext context) throws JobExecutionException {
+			long startTime = System.currentTimeMillis();
+			String backupFileName = DB_FOLDER_NAME + File.separator + 
+				DATE_FORMATTER.format(System.currentTimeMillis()) + "_" + DB_FILE_NAME + ".bak";
+			
+			removeObsoleteBackupFiles();
+			try {
+				ExtObjectContainer extDb = db.ext();
+				if (!extDb.isClosed()) {
+					extDb.backup(backupFileName);
+					logger.debug("succesfully created new DB4O backup '{}' in {}ms", backupFileName, System.currentTimeMillis() - startTime);
+				} else {
+					logger.debug("couldn't create DB4O backup '{}' because db is closed", backupFileName);
+				}
+			} catch(Db4oException e) {
+				logger.warn("Error creating backup '{}': {}", backupFileName, e.getMessage());
+			}
+		}
+		
+		/**
+		 * Removes the oldest x backup files from the file system.
+		 */
+		private void removeObsoleteBackupFiles() {
+			File dbFolder = new File(DB_FOLDER_NAME);
+			if (dbFolder.exists() && dbFolder.isDirectory()) {
+				File[] backupFiles = dbFolder.listFiles(new FilenameFilter() {
+					@Override
+					public boolean accept(File dir, String name) {
+						return name.endsWith(DB_FILE_NAME + ".bak");
+					}
+				});
+				
+				Arrays.sort(backupFiles);
+				if (backupFiles.length > maxBackups) {
+					logger.debug("found {} backup files but only {} are allowed. will remove the oldest {} file(s) now",
+						new Object[] { backupFiles.length, maxBackups, backupFiles.length - maxBackups });
+					
+					for (int index = 0; index < backupFiles.length - maxBackups; index++) {
+						boolean succesful = backupFiles[index].delete();
+						if (!succesful) {
+							logger.debug("couldn't delete file '{}'", backupFiles[index]);
+						} else {
+							logger.trace("successfully deleted file '{}'", backupFiles[index]);
+						}
+					}
+				}
+			}
+		}
+		
 	}
 	
 
