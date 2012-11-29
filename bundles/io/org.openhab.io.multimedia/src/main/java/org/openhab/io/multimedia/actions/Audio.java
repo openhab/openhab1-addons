e8c2040a95b96cadb107b3cbfae9af74d3bcc616
Kai <devnull@localhost>
2012-11-29 23:04:05 +0100

    Issue 159:	Support Shoutcast streams +and .pls files

--- bundles/io/org.openhab.io.multimedia/src/main/java/org/openhab/io/multimedia/actions/Audio.java
+++ bundles/io/org.openhab.io.multimedia/src/main/java/org/openhab/io/multimedia/actions/Audio.java
@@ -33,9 +33,14 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.MalformedURLException;
+import java.net.Socket;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.Collection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
@@ -65,10 +70,14 @@ public class Audio {
 
 	private static final String SOUND_DIR = "sounds";
 	private static final Logger logger = LoggerFactory.getLogger(Audio.class);
+	
+	private static final Pattern plsStreamPattern = Pattern.compile("^File[0-9]=(.+)$");
 
 	private static Float macVolumeValue = null;
 	
 	private static Player streamPlayer = null;
+	
+	private static Socket shoutCastSocket = null;
 
 	static public void playSound(String filename) {
 		try {
@@ -116,10 +125,42 @@ public class Audio {
 					}
 				}
 			}
-			InputStream is = new URL(url).openStream();
-			Player player = new Player(is);
-			streamPlayer = player;
-			playInThread(player);
+			else if (url.toLowerCase().endsWith(".pls")) {
+				InputStream is = new URL(url).openStream();
+				String urls = IOUtils.toString(is);
+				for(String line : urls.split("\n")) {
+					if(!line.isEmpty() && line.startsWith("File")) {
+						Matcher matcher = plsStreamPattern.matcher(line);
+						if (matcher.find()) {
+							url=matcher.group(1);
+							break;
+						}
+					}
+				}
+			}
+			URL streamUrl = new URL(url);
+			URLConnection connection = streamUrl.openConnection();
+			InputStream is = null;
+			if (connection.getContentType().equals("unknown/unknown")) {
+				//Java does not parse non-standard headers used by SHOUTCast
+				int port = streamUrl.getPort()>0 ? streamUrl.getPort() : 80;
+				// Manipulate User-Agent to receive a stream
+				shoutCastSocket = new Socket(streamUrl.getHost(), port);
+				
+				OutputStream os = shoutCastSocket.getOutputStream();
+				String user_agent = "WinampMPEG/5.09";
+				String req = "GET / HTTP/1.0\r\nuser-agent: " + user_agent + "\r\nIcy-MetaData: 1\r\nConnection: keep-alive\r\n\r\n";
+				os.write(req.getBytes());
+				is = shoutCastSocket.getInputStream();
+			}
+			else {
+				is = streamUrl.openStream();
+			}
+			if (is!=null) {
+				Player player = new Player(is);
+				streamPlayer = player;
+				playInThread(player);
+			}
 		} catch (JavaLayerException e) {
 			logger.error("Cannot play stream '{}': JavaLayerException - {}", url, e.getMessage());
 		} catch (MalformedURLException e) {
@@ -312,6 +353,13 @@ public class Audio {
 					player.play();
 				} catch (Exception e) {
 					throw new RuntimeException(e.getMessage());
+				} finally {
+					if (shoutCastSocket!=null) {
+						try {
+							shoutCastSocket.close();
+						} catch (IOException e) {
+						}
+					}
 				}
 			}
 		}.start();
