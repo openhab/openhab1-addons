8b2f143a127730534e00ed1aedaef341f95a85b7
Kai <kai@openhab.org>
2013-01-01 12:06:21 +0100

    updated headers to 2013

--- bundles/io/org.openhab.io.net/src/main/java/org/openhab/io/net/actions/HTTP.java
+++ bundles/io/org.openhab.io.net/src/main/java/org/openhab/io/net/actions/HTTP.java
@@ -1,6 +1,6 @@
 /**
  * openHAB, the open Home Automation Bus.
- * Copyright (C) 2010-2012, openHAB.org <admin@openhab.org>
+ * Copyright (C) 2010-2013, openHAB.org <admin@openhab.org>
  *
  * See the contributors.txt file in the distribution for a
  * full listing of individual contributors.
