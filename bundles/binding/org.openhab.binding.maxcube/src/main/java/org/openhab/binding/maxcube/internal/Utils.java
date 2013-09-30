9e0895d6319fb57ab276992bf889c3a13dd8b79c
aheil <info@aheil.de>
2013-09-30 19:51:52 +0200

    updated documentation for MAX!Cube Utils class

--- bundles/binding/org.openhab.binding.maxcube/src/main/java/org/openhab/binding/maxcube/internal/Utils.java
+++ bundles/binding/org.openhab.binding.maxcube/src/main/java/org/openhab/binding/maxcube/internal/Utils.java
@@ -32,17 +32,31 @@ import java.util.Calendar;
 import java.util.Date;
 
 /**
-* Utility class for common tasks. 
+* Utility class for common tasks within the MAX!Cube binding package. 
 * 
 * @author Andreas Heil (info@aheil.de)
 * @since 1.4.0
 */
 public final class Utils {
 	
-	public static int fromHex(String hex) {
+	/**
+	 * Returns the integer value of an  hexadecimal number (base 16).
+	 * 
+	 * @param hex
+	 * 			the hex value to be parsed into an integer 
+	 * @return the given hex value as integer
+	 */
+	public static final int fromHex(String hex) {
 		return Integer.parseInt(hex, 16);
 	}
 	
+	/**
+	 * Returns the hexadecimal number of a number of integer values.
+	 * 
+	 * @param values
+	 * 			the integer values to be converted into hexadecimal numbers
+	 * @return the given numbers as hexadecimal number
+	 */
 	public static final String toHex(int... values) {
 		String returnValue = "";
 		for (int v : values) {
@@ -51,10 +65,42 @@ public final class Utils {
 		return returnValue;
 	}
 
-	public static int fromByte(byte b) {
+	/**
+	 * Converts an Java signed byte into its general (unsigned) value as being used in other programming languages and platforms.
+	 * 
+	 * @param b
+	 * 			the byte to be converted into its integer value
+	 * @return the integer value represented by the given byte
+	 */
+	public static final int fromByte(byte b) {
 		return b & 0xFF;
 	}
 	
+	/**
+	 * Resolves the date and time based based on a three byte encoded within a MAX!Cube L message.
+	 *
+	 * Date decoding (two byte)
+	 * <pre>
+	 * Hex     Binary
+	 * 9D0B    1001110100001011
+     *   MMMDDDDDM YYYYYY
+     *   100     0        = 1000b  = 8d  = month
+     *      11101         = 11101b = 29d = day
+     *             001011 = 1011b  = 11d = year-2000  
+	 * </pre>
+	 * 
+	 * Time decoding (one byte)
+	 * <pre>
+	 * Hex     Decimal
+	 * 1F      31 * 0.5 hours = 15:30
+	 * </pre>
+	 * 
+	 * @param date
+	 * 			the date to be converted based on two bytes
+	 * @param time
+	 * 			the time to be converted based on a single byte
+	 * @return the date time based on the values provided
+	 */
 	public static Date resolveDateTime(int date, int time) {
 		
 		int month = ((date & 0xE000) >> 12)+((date & 0x80) >> 7);
