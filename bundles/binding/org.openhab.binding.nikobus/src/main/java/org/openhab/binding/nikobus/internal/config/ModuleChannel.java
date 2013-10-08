415aec8b0d65c2c567852b86fa13baabdd3ccac7
Davy Vanherbergen <davy@unknown.(none)>
2013-10-08 22:40:11 +0200

    NikobusBinding: added support for reading dim module values.

--- bundles/binding/org.openhab.binding.nikobus/src/main/java/org/openhab/binding/nikobus/internal/config/ModuleChannel.java
+++ bundles/binding/org.openhab.binding.nikobus/src/main/java/org/openhab/binding/nikobus/internal/config/ModuleChannel.java
@@ -28,66 +28,85 @@
  */
 package org.openhab.binding.nikobus.internal.config;
 
+import java.util.List;
+
 import org.openhab.binding.nikobus.internal.NikobusBinding;
 import org.openhab.binding.nikobus.internal.core.NikobusCommand;
 import org.openhab.core.library.types.OnOffType;
+import org.openhab.core.library.types.PercentType;
 import org.openhab.core.types.Command;
 import org.openhab.core.types.State;
 
 /**
- * Switch Channel. Represents a single switch channel in a Nikobus switch
+ * Module Channel. Represents a single channel in a Nikobus switch or dimmer
  * module.
  * 
  * @author Davy Vanherbergen
  * @since 1.3.0
  */
-public class SwitchModuleChannel extends AbstractNikobusItemConfig {
+public class ModuleChannel extends AbstractNikobusItemConfig {
 
-	private SwitchModuleChannelGroup channelGroup;
+	private ModuleChannelGroup channelGroup;
 
 	private State state = OnOffType.OFF;
 
+	private List<Class<? extends Command>> supportedCommands;
+
 	/**
 	 * Restricted default constructor.
+	 * 
+	 * @param supportedCommands
 	 */
-	protected SwitchModuleChannel(String name, String address,
-			SwitchModuleChannelGroup channelGroup) {
+	protected ModuleChannel(String name, String address,
+			ModuleChannelGroup channelGroup,
+			List<Class<? extends Command>> supportedCommands) {
 		super(name, address);
 		this.channelGroup = channelGroup;
+		this.supportedCommands = supportedCommands;
 	}
 
 	@Override
-	public void processCommand(Command command, NikobusBinding binding) throws Exception {
+	public void processCommand(Command command, NikobusBinding binding)
+			throws Exception {
 
-		if (!(command instanceof OnOffType)) {
-			return;
+		if (command instanceof PercentType) {
+			this.state = (PercentType) command;
+		}
+		if (command instanceof OnOffType) {
+			this.state = (OnOffType) command;
 		}
 
-		this.state = (OnOffType) command;
 		channelGroup.publishStateToNikobus(this, binding);
 	}
 
 	/**
 	 * Set the state of the nikobus channel. Changing the state will not trigger
-	 * an update command for the parent switch module.
+	 * an update command for the parent module.
 	 * 
 	 * @param newState
-	 *            ON or OFF.
+	 *            ON/OFF or percent value
 	 */
-	protected void setState(OnOffType newState) {
+	protected void setState(State newState) {
 		this.state = newState;
 	}
 
 	/**
-	 * @return current state of the channel. Can be ON or OFF.
+	 * @return current state of the channel.
 	 */
 	public State getState() {
 		return state;
 	}
 
 	@Override
-	public void processNikobusCommand(NikobusCommand command, NikobusBinding binding) {
+	public void processNikobusCommand(NikobusCommand command,
+			NikobusBinding binding) {
 		// noop, implemented by switch module channel group.
 	}
 
+	/**
+	 * @return true if percent type commands are supported.
+	 */
+	public boolean supportsPercentType() {
+		return supportedCommands.contains(PercentType.class);
+	}
 }
