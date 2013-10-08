9a55fc5fadf79bfccbb6e8cf7264e52e9f9aacb6
Till Klocke <till.klocke@gmail.com>
2013-10-08 12:01:10 +0200

    Fixed typi

--- bundles/io/org.openhab.io.transport.cul/src/main/java/de/akuz/cul/CULManager.java
+++ bundles/io/org.openhab.io.transport.cul/src/main/java/de/akuz/cul/CULManager.java
@@ -106,7 +106,7 @@ public class CULManager {
 	private static CULHandler createNewHandler(String deviceName, CULMode mode) throws CULDeviceException {
 		String deviceType = getPrefix(deviceName);
 		String deviceAddress = getRawDeviceName(deviceName);
-		logger.debug("Searching class for device type " + deviceAddress);
+		logger.debug("Searching class for device type " + deviceType);
 		Class<? extends CULHandler> culHandlerclass = deviceTypeClasses.get(deviceType);
 		if (culHandlerclass == null) {
 			throw new CULDeviceException("No class for the device type " + deviceType + " is registred");
