71a0cc2e09d8bbdd5592102a1ffd4ebaaa680b0e
Cycling Engineer <paul-github@silly-science.co.uk>
2013-10-11 00:56:57 +0100

    Added handling for chime command in Lighting1 (HomeEasy)

--- bundles/binding/org.openhab.binding.rfxcom/src/main/java/org/openhab/binding/rfxcom/internal/RFXComDataConverter.java
+++ bundles/binding/org.openhab.binding.rfxcom/src/main/java/org/openhab/binding/rfxcom/internal/RFXComDataConverter.java
@@ -23,6 +23,7 @@ import org.openhab.core.library.items.NumberItem;
 import org.openhab.core.library.items.RollershutterItem;
 import org.openhab.core.library.items.StringItem;
 import org.openhab.core.library.items.SwitchItem;
+import org.openhab.core.library.types.ChimeType;
 import org.openhab.core.library.types.DateTimeType;
 import org.openhab.core.library.types.DecimalType;
 import org.openhab.core.library.types.OnOffType;
@@ -765,6 +766,11 @@ public class RFXComDataConverter {
 					throw new NumberFormatException("Can't convert " + type
 							+ " to Command");
 				}
+				break;
+
+			case CHIME:
+					d1.command = RFXComLighting1Message.Commands.CHIME;
+					obj = d1;
 				break;
 
 			default:
