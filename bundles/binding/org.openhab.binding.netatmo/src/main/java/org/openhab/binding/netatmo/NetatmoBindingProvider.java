c158dd314cb2e2ff0ab0149703352d6ccd6dca05
Andreas Brenk <mail@andreasbrenk.com>
2013-10-17 16:21:53 +0200

    Netatmo binding: Initial commit

---
+++ bundles/binding/org.openhab.binding.netatmo/src/main/java/org/openhab/binding/netatmo/NetatmoBindingProvider.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2010-2013, openHAB.org and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.netatmo;
+
+import org.openhab.core.binding.BindingProvider;
+
+/**
+ * @author Andreas Brenk
+ * @since 1.4.0
+ */
+public interface NetatmoBindingProvider extends BindingProvider {
+
+	String getDeviceId(String itemName);
+
+	String getMeasure(String itemName);
+
+	String getModuleId(String itemName);
+
+}
