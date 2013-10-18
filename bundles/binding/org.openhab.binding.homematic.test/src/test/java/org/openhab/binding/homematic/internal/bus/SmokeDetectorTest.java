ee3c08805649d573d56e66ebb7cd4afd8e9c079a
Thomas Letsch <contact@thomas-letsch.de>
2013-10-18 10:12:08 +0200

    Homematic binding: Added more device configs

---
+++ bundles/binding/org.openhab.binding.homematic.test/src/test/java/org/openhab/binding/homematic/internal/bus/SmokeDetectorTest.java
@@ -0,0 +1,70 @@
+/**
+ * openHAB, the open Home Automation Bus.
+ * Copyright (C) 2010-2013, openHAB.org <admin@openhab.org>
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ * Additional permission under GNU GPL version 3 section 7
+ *
+ * If you modify this Program, or any covered work, by linking or
+ * combining it with Eclipse (or a modified version of that library),
+ * containing parts covered by the terms of the Eclipse Public License
+ * (EPL), the licensors of this Program grant you additional permission
+ * to convey the resulting work.
+ */
+package org.openhab.binding.homematic.internal.bus;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openhab.binding.homematic.internal.config.HomematicParameterAddress;
+import org.openhab.binding.homematic.internal.device.ParameterKey;
+import org.openhab.binding.homematic.test.HomematicBindingProviderMock;
+import org.openhab.core.library.items.SwitchItem;
+import org.openhab.core.library.types.OnOffType;
+
+public class SmokeDetectorTest extends BasicBindingTest {
+
+    private static final ParameterKey PARAMETER_KEY = ParameterKey.STATE;
+
+    @Before
+    public void setupProvider() {
+        provider.setItem(new SwitchItem(HomematicBindingProviderMock.DEFAULT_ITEM_NAME));
+        provider.setParameterAddress(HomematicParameterAddress.from(MOCK_PARAM_ADDRESS, PARAMETER_KEY.name()));
+        ccu.getPhysicalDevice(MOCK_PARAM_ADDRESS).getDeviceDescription().setType("HM-Sec-SD");
+    }
+
+    @Test
+    public void allBindingsChangedClosed() {
+        checkInitialValue(PARAMETER_KEY, Boolean.FALSE, OnOffType.OFF);
+    }
+
+    @Test
+    public void allBindingsChangedOpen() {
+        checkInitialValue(PARAMETER_KEY, Boolean.TRUE, OnOffType.ON);
+    }
+
+    @Test
+    public void receiveSensorUpdateClosed() {
+        checkValueReceived(PARAMETER_KEY, Boolean.FALSE, OnOffType.OFF);
+    }
+
+    @Test
+    public void receiveSensorUpdateOpen() {
+        checkValueReceived(PARAMETER_KEY, Boolean.TRUE, OnOffType.ON);
+    }
+
+}
--- bundles/binding/org.openhab.binding.homematic/src/main/java/org/openhab/binding/homematic/internal/bus/HomematicBinding.java
+++ bundles/binding/org.openhab.binding.homematic/src/main/java/org/openhab/binding/homematic/internal/bus/HomematicBinding.java
@@ -160,7 +160,9 @@ public class HomematicBinding extends AbstractBinding<HomematicBindingProvider> 
 
         converterFactory.addStateConverter(ParameterKey.LOWBAT.name(), OnOffType.class, BooleanOnOffConverter.class);
 
-        DeviceConfigLocator locator = new DeviceConfigLocator("HM-LC-Bl1-FM.xml", "HM-LC-BI1PBU-FM.xml", "HM-Sec-SC.xml", "HM-Sec-RHS.xml");
+        DeviceConfigLocator locator = new DeviceConfigLocator("HM-LC-Dim1L-Pl.xml", "HM-LC-BI1PBU-FM.xml", "HM-LC-Bl1-FM.xml",
+                "HM-Sec-SC.xml", "HM-Sec-RHS.xml", "HM-LC-Dim2L-SM.xml", "HM-LC-Dim2L-CV.xml", "HM-LC-Dim1L-CV.xml", "HM-LC-Dim1T-Pl.xml",
+                "HM-LC-Dim1T-CV.xml", "HM-LC-Dim2T-SM.xml", "HM-Sec-SD.xml");
         List<ConfiguredDevice> configuredDevices = locator.findAll();
         converterFactory.addConfiguredDevices(configuredDevices);
 
@@ -168,6 +170,7 @@ public class HomematicBinding extends AbstractBinding<HomematicBindingProvider> 
 
     @Override
     public void activate() {
+        logger.debug("activate");
         if (ccu != null && cbServer == null) {
             registerCallbackHandler();
         }
@@ -175,6 +178,7 @@ public class HomematicBinding extends AbstractBinding<HomematicBindingProvider> 
 
     @Override
     public void deactivate() {
+        logger.debug("deactivate");
         if (cbServer != null) {
             removeCallbackHandler(cbServer);
             cbServer = null;
@@ -261,6 +265,7 @@ public class HomematicBinding extends AbstractBinding<HomematicBindingProvider> 
 
     @Override
     public void updated(Dictionary<String, ?> config) throws ConfigurationException {
+        logger.debug("updated config={}", config);
         if (config == null) {
             return;
         }
@@ -280,10 +285,14 @@ public class HomematicBinding extends AbstractBinding<HomematicBindingProvider> 
         if (ccu != null && cbServer == null) {
             registerCallbackHandler();
         }
+        for (HomematicBindingProvider provider : providers) {
+            queryAndSendAllActualStates(provider);
+        }
     }
 
     @Override
     public void allBindingsChanged(BindingProvider provider) {
+        logger.debug("allBindingsChanged provider={}", provider);
         if (provider instanceof HomematicBindingProvider) {
             HomematicBindingProvider homematicBindingProvider = (HomematicBindingProvider) provider;
             queryAndSendAllActualStates(homematicBindingProvider);
@@ -292,6 +301,7 @@ public class HomematicBinding extends AbstractBinding<HomematicBindingProvider> 
 
     @Override
     public void bindingChanged(BindingProvider provider, String itemName) {
+        logger.debug("bindingChanged provider={}, itemName={}", provider, itemName);
         if (provider instanceof HomematicBindingProvider) {
             HomematicBindingProvider homematicBindingProvider = (HomematicBindingProvider) provider;
             initializeDeviceAndParameters(homematicBindingProvider, itemName);
