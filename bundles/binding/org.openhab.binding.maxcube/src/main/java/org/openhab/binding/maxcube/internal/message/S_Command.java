66f591bbc002205077d0ecece0c34073ea651712
aheil <info@aheil.de>
2013-11-01 10:58:06 +0100

    improved javadoc for S_command class

--- bundles/binding/org.openhab.binding.maxcube/src/main/java/org/openhab/binding/maxcube/internal/message/S_Command.java
+++ bundles/binding/org.openhab.binding.maxcube/src/main/java/org/openhab/binding/maxcube/internal/message/S_Command.java
@@ -13,44 +13,60 @@ import org.openhab.binding.maxcube.internal.Utils;
 import org.slf4j.Logger;
 
 /**
-*  Command to be send via by the MAX!Cube protocol. 
-* 
-* @author Andreas Heil (info@aheil.de)
-* @since 1.4.0
-*/
+ * Command to be send via the MAX!Cube protocol.
+ * 
+ * @author Andreas Heil (info@aheil.de)
+ * @since 1.4.0
+ */
 public class S_Command {
 
 	String baseString = "000440000000";;
 	boolean[] bits = null;
 	String rfAddress = null;
-	
-	public S_Command(String rfAddress, double setpointTemperature) {		
+
+	/**
+	 * Creates a new instance of the MAX! protocol S command.
+	 * 
+	 * @param rfAddress
+	 *            the RF address the command is for
+	 * @param setpointTemperature
+	 *            the desired setpoint temperature for the device.
+	 */
+	public S_Command(String rfAddress, double setpointTemperature) {
 		this.rfAddress = rfAddress;
-		
+
 		int setpointValue = (int) setpointTemperature * 2;
-		boolean[] setpointBits =  Utils.getBits(setpointValue);
-		
+		boolean[] setpointBits = Utils.getBits(setpointValue);
+
 		// Temperature setpoint, Temp uses 6 bits LSB6 (bit 2 tm 7),
 		// 20 deg C = bits 101000 = dec 40/2 = 20 deg C,
-		// you need 8 bits to send so add the 2 bits below (sample 10101000 = hex A8)
-		// bit 0,1 = 00 = Auto weekprog (no temp is needed, just make the whole byte 00
-		
+		// you need 8 bits to send so add the 2 bits below (sample 10101000 =
+		// hex A8)
+		// bit 0,1 = 00 = Auto weekprog (no temp is needed, just make the whole
+		// byte 00
+
 		bits = new boolean[setpointBits.length + 2];
 
 		// 01 = Permanent
-		// 10 = Temporarily 
-		bits [0] = false;
-		bits [1] = true;
-		
+		// 10 = Temporarily
+		bits[0] = false;
+		bits[1] = true;
+
 		for (int i = 0; i < setpointBits.length; i++) {
-			bits[i+2] = setpointBits[i];
+			bits[i + 2] = setpointBits[i];
 		}
 	}
 
+	/**
+	 * Returns the Base64 encoded command string to be sent via the MAX!
+	 * protocol.
+	 * 
+	 * @return the string representing the command
+	 */
 	public String getCommandString() {
-		
-		String commandString = baseString + rfAddress +  "01" + Utils.toHex(bits);
-		
+
+		String commandString = baseString + rfAddress + "01" + Utils.toHex(bits);
+
 		byte[] encodedString = Base64.encodeBase64(commandString.getBytes());
 		return "s:" + encodedString + "\r\n";
 	}
