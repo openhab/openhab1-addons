4a8cc10656a9ec2111cccfb18ab745b88998c87b
aheil <info@aheil.de>
2013-11-01 14:35:13 +0100

    reverted tcp binding to c4dfdf67198153162c72f7615318abbd008fbb87

--- bundles/binding/org.openhab.binding.tcp/src/main/java/org/openhab/binding/tcp/protocol/internal/UDPBinding.java
+++ bundles/binding/org.openhab.binding.tcp/src/main/java/org/openhab/binding/tcp/protocol/internal/UDPBinding.java
@@ -1,10 +1,30 @@
 /**
- * Copyright (c) 2010-2013, openHAB.org and others.
+ * openHAB, the open Home Automation Bus.
+ * Copyright (C) 2010-2013, openHAB.org <admin@openhab.org>
  *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ * Additional permission under GNU GPL version 3 section 7
+ *
+ * If you modify this Program, or any covered work, by linking or
+ * combining it with Eclipse (or a modified version of that library),
+ * containing parts covered by the terms of the Eclipse Public License
+ * (EPL), the licensors of this Program grant you additional permission
+ * to convey the resulting work.
  */
 package org.openhab.binding.tcp.protocol.internal;
 
@@ -68,9 +88,15 @@ public class UDPBinding extends AbstractDatagramChannelBinding<UDPBindingProvide
 		ProtocolBindingProvider provider = findFirstMatchingBindingProvider(itemName);
 
 		if(command != null ){		
-			
-			String transformedMessage = transformResponse(provider.getProtocolCommand(itemName, command),commandAsString);
-			String UDPCommandName = preAmble + transformedMessage + postAmble ;
+			String UDPCommandName = null;
+
+			if(command instanceof DecimalType) {
+				UDPCommandName = commandAsString;
+			} else {
+				UDPCommandName = provider.getProtocolCommand(itemName,command);
+			}
+
+			UDPCommandName = preAmble + UDPCommandName + postAmble ;
 
 			ByteBuffer outputBuffer = ByteBuffer.allocate(UDPCommandName.getBytes().length);
 			try {
@@ -131,9 +157,16 @@ public class UDPBinding extends AbstractDatagramChannelBinding<UDPBindingProvide
 		ProtocolBindingProvider provider = findFirstMatchingBindingProvider(itemName);
 
 		List<Class<? extends State>> stateTypeList = provider.getAcceptedDataTypes(itemName,aCommand);
+		State newState = null;
 
-		String transformedResponse = transformResponse(provider.getProtocolCommand(itemName, aCommand),theUpdate);
-		State newState = createStateFromString(stateTypeList,transformedResponse);
+		if(aCommand instanceof DecimalType) {
+			String transformedResponse = transformResponse(provider.getProtocolCommand(itemName, aCommand),theUpdate);
+			newState = createStateFromString(stateTypeList,transformedResponse);
+		} else {
+			if(provider.getProtocolCommand(itemName, aCommand).equals(theUpdate)) {
+				newState = createStateFromString(stateTypeList,aCommand.toString());
+			} 
+		}
 
 		if(newState != null) {
 			eventPublisher.postUpdate(itemName, newState);							        						
