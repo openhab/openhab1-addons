ae4306077fa74cd401217e22db96605f060d7a3d
Ben Jones <ben.jones12@gmail.com>
2013-11-07 08:27:04 +1300

    Improve error logging when endpoint not found (show endpoint id)

--- bundles/binding/org.openhab.binding.zwave/src/main/java/org/openhab/binding/zwave/internal/protocol/commandclass/ZWaveMultiInstanceCommandClass.java
+++ bundles/binding/org.openhab.binding.zwave/src/main/java/org/openhab/binding/zwave/internal/protocol/commandclass/ZWaveMultiInstanceCommandClass.java
@@ -281,7 +281,7 @@ public class ZWaveMultiInstanceCommandClass extends ZWaveCommandClass {
 			ZWaveEndpoint endpoint = this.endpoints.get(endpointId);
 			
 			if (endpoint == null){
-				logger.error("Endpoint {} not found on node {}. Cannot set command classes.", endpoint, this.getNode().getNodeId());
+				logger.error("Endpoint {} not found on node {}. Cannot set command classes.", endpointId, this.getNode().getNodeId());
 				continue;
 			}
 	
@@ -377,7 +377,7 @@ public class ZWaveMultiInstanceCommandClass extends ZWaveCommandClass {
 		ZWaveEndpoint endpoint = this.endpoints.get(endpointId);
 		
 		if (endpoint == null){
-			logger.error("Endpoint {} not found on node {}. Cannot set command classes.", endpoint, this.getNode().getNodeId());
+			logger.error("Endpoint {} not found on node {}. Cannot set command classes.", endpointId, this.getNode().getNodeId());
 			return;
 		}
 		
