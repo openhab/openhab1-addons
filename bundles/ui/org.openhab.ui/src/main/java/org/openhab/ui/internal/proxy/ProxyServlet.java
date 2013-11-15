655f44fa462497dd213a1959cd339921e175335b
Chris Jackson <chris@cd-jackson.com>
2013-11-15 23:08:19 +0000

    UI-X: Removed function to have an image/url from an item value - Considered to be a security risk.

--- bundles/ui/org.openhab.ui/src/main/java/org/openhab/ui/internal/proxy/ProxyServlet.java
+++ bundles/ui/org.openhab.ui/src/main/java/org/openhab/ui/internal/proxy/ProxyServlet.java
@@ -167,14 +167,6 @@ public class ProxyServlet extends HttpServlet {
 				}
 			}
 
-			// Test the "URL" to see if it's really an item
-			// If the state is returned, then use it as the url
-			// TODO: !!Q!!: Is this a security risk???
-			State itemState = itemUIRegistry.getItemState(widget.getItem());
-			if(itemState != null && itemState instanceof StringType) {
-				uriString = itemState.toString();
-			}
-
 			// uriString now holds the default value (if specified)
 			// We process the array to see if there's an override based on the state
 			if (widget.getItem() != null) {
