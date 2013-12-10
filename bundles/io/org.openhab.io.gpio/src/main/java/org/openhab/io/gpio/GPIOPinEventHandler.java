5ca0e974765b2cc12984b4a103faf7499628892d
Dancho Penev <dancho.penev@dancho-penev.org>
2013-12-10 20:58:46 +0200

    Changed license from GPL to EPL, changed @since to "1.4.0"

--- bundles/io/org.openhab.io.gpio/src/main/java/org/openhab/io/gpio/GPIOPinEventHandler.java
+++ bundles/io/org.openhab.io.gpio/src/main/java/org/openhab/io/gpio/GPIOPinEventHandler.java
@@ -1,30 +1,10 @@
 /**
- * openHAB, the open Home Automation Bus.
- * Copyright (C) 2010-${year}, openHAB.org <admin@openhab.org>
- * 
- * See the contributors.txt file in the distribution for a
- * full listing of individual contributors.
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 3 of the
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, see <http://www.gnu.org/licenses>.
- * 
- * Additional permission under GNU GPL version 3 section 7
- * 
- * If you modify this Program, or any covered work, by linking or 
- * combining it with Eclipse (or a modified version of that library),
- * containing parts covered by the terms of the Eclipse Public License
- * (EPL), the licensors of this Program grant you additional permission
- * to convey the resulting work.
+ * Copyright (c) 2010-2013, openHAB.org and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  */
 
 
@@ -37,7 +17,7 @@ package org.openhab.io.gpio;
  * <code>GPIOPin</code>.
  *
  * @author Dancho Penev
- * @since 1.3.1
+ * @since 1.4.0
  */
 public interface GPIOPinEventHandler {
 
