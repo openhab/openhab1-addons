345389a8a106a9405248e03e54224bc453f504c4
marcel <marcel@verpaalen.com>
2014-01-20 08:55:56 +0100

    Refactoring of variables to bring in line with code style & conventions. No functional changes

--- bundles/binding/org.openhab.binding.maxcube/src/main/java/org/openhab/binding/maxcube/internal/MaxCubeDiscover.java
+++ bundles/binding/org.openhab.binding.maxcube/src/main/java/org/openhab/binding/maxcube/internal/MaxCubeDiscover.java
@@ -8,8 +8,6 @@
  */
 package org.openhab.binding.maxcube.internal;
 
-
-
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -20,28 +18,30 @@ import java.util.Enumeration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
+/**
+* Automatic UDP discovery of a MAX!Cube Lan Gateway on the local network. 
+* 
+* @author Marcel Verpaalen, based on UDP client code of Michiel De Mey 
+* @since 1.4.0
+*/
 public final class MaxCubeDiscover {
 
 	/**
-	* Automatic UDP discovery of a MAX!Cube Lan Gateway on the local network. 
-	* 
-	* @author Marcel Verpaalen (marcel@verpaalen.com),  
-	* based on UDP client code of Michiel De Mey 
-	* @since 1.4.0
+	* Automatic UDP discovery of a MAX!Cube
+	* @return if the cube is found, returns the IP address as a string. Otherwise returns null
 	*/
-	public final static String DiscoverIP () {
+	public final static String discoverIp () {
 	
-		String MaxCubeIP = null;
-		String MaxCubeName = null;
+		String maxCubeIP = null;
+		String maxCubeName = null;
 		String rfAddress = null;
 		
 		Logger logger = LoggerFactory.getLogger(MaxCubeDiscover.class);
 		
 		//Find the MaxCube using UDP broadcast
 		try {
-			DatagramSocket i = new DatagramSocket();
-			i.setBroadcast(true);
+			DatagramSocket bcSend = new DatagramSocket();
+			bcSend.setBroadcast(true);
 
 			byte[] sendData = "eQ3Max*\0**********I".getBytes();
 
@@ -63,40 +63,40 @@ public final class MaxCubeDiscover {
 					// Send the broadcast package!
 					try {
 						DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, broadcast, 23272);
-						i.send(sendPacket);
+						bcSend.send(sendPacket);
 					} catch (Exception e) {
 						logger.debug(e.getMessage());
 						logger.debug(Utils.getStackTrace(e));
 				}
 
-				logger.trace( "Request packet sent to: " + broadcast.getHostAddress() + "; Interface: " + networkInterface.getDisplayName());
+				logger.trace( "Request packet sent to: {} Interface: {}", broadcast.getHostAddress(),  networkInterface.getDisplayName());
 			}
 		}
 
 		logger.trace( "Done looping over all network interfaces. Now waiting for a reply!");
-		i.close();
+		bcSend.close();
 
-		DatagramSocket c = new DatagramSocket(23272);
-		c.setReuseAddress(true);
+		DatagramSocket bcReceipt = new DatagramSocket(23272);
+		bcReceipt.setReuseAddress(true);
 
 		//Wait for a response
 		byte[] recvBuf = new byte[15000];
 		DatagramPacket receivePacket = new DatagramPacket(recvBuf, recvBuf.length);
-		c.receive(receivePacket);
+		bcReceipt.receive(receivePacket);
 
 		//We have a response
-		logger.trace( "Broadcast response from server: " + receivePacket.getAddress());
+		logger.trace( "Broadcast response from server: {}", receivePacket.getAddress());
 
 		//Check if the message is correct
 		String message = new String(receivePacket.getData()).trim();
 
 		if (message.startsWith("eQ3Max")) {
 			
-			MaxCubeIP=receivePacket.getAddress().getHostAddress();
-			MaxCubeName=message.substring(0, 8);
+			maxCubeIP=receivePacket.getAddress().getHostAddress();
+			maxCubeName=message.substring(0, 8);
 			rfAddress=message.substring(8, 18);
-			logger.debug("Found at: {}", MaxCubeIP);
-			logger.debug("Name    : {}", MaxCubeName);
+			logger.debug("Found at: {}", maxCubeIP);
+			logger.debug("Name    : {}", maxCubeName);
 			logger.debug("Serial  : {}", rfAddress);
 			logger.trace("Message : {}", message);	
 		} else {
@@ -104,11 +104,11 @@ public final class MaxCubeDiscover {
 		}
 
 		//Close the port!
-		c.close();
+		bcReceipt.close();
 
 		} catch (IOException ex) {
 			logger.debug(ex.toString());
 		}
-		return MaxCubeIP;
+		return maxCubeIP;
 	}
 }
