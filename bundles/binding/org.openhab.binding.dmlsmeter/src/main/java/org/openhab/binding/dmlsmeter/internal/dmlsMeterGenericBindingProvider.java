befd6c521bb2126e244c97acccd4ecc6b5a10f44
kreutpet <kreutzer.peter@gmail.com>
2014-01-21 13:37:53 +0100

    Initial add of new binding of dmls based meters
    binding created based on archetypes.
    added gobal configuration for serial port . baud and echohandling
    added dependency to external libraries
    initial implementation to get data from metter

---
+++ bundles/binding/org.openhab.binding.dmlsmeter/src/main/java/org/openhab/binding/dmlsmeter/internal/dmlsMeterGenericBindingProvider.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2010-2013, openHAB.org and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.dmlsmeter.internal;
+
+import org.openhab.binding.dmlsmeter.dmlsMeterBindingProvider;
+import org.openhab.core.binding.BindingConfig;
+import org.openhab.core.items.Item;
+import org.openhab.core.library.items.DimmerItem;
+import org.openhab.core.library.items.SwitchItem;
+import org.openhab.model.item.binding.AbstractGenericBindingProvider;
+import org.openhab.model.item.binding.BindingConfigParseException;
+
+
+/**
+ * This class is responsible for parsing the binding configuration.
+ * 
+ * @author Peter Kreutzer
+ * @since 1.4.0
+ */
+public class dmlsMeterGenericBindingProvider extends AbstractGenericBindingProvider implements dmlsMeterBindingProvider {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public String getBindingType() {
+		return "dmlsmeter";
+	}
+
+	/**
+	 * @{inheritDoc}
+	 */
+	@Override
+	public void validateItemType(Item item, String bindingConfig) throws BindingConfigParseException {
+		//if (!(item instanceof SwitchItem || item instanceof DimmerItem)) {
+		//	throw new BindingConfigParseException("item '" + item.getName()
+		//			+ "' is of type '" + item.getClass().getSimpleName()
+		//			+ "', only Switch- and DimmerItems are allowed - please check your *.items configuration");
+		//}
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void processBindingConfiguration(String context, Item item, String bindingConfig) throws BindingConfigParseException {
+		super.processBindingConfiguration(context, item, bindingConfig);
+		dmlsMeterBindingConfig config = new dmlsMeterBindingConfig();
+		
+		//parse bindingconfig here ...
+		
+		addBindingConfig(item, config);		
+	}
+	
+	
+	class dmlsMeterBindingConfig implements BindingConfig {
+		// put member fields here which holds the parsed values
+	}
+	
+	
+}
