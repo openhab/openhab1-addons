15c530f586c9de7979a391c684d506aec94b7eef
Joshua Colson <joshua.colson@gmail.com>
2014-02-09 11:14:04 -0700

    Add polling of nodes on receipt of an Application Update Request message when the node is already in the DONE state.

--- bundles/binding/org.openhab.binding.zwave/src/main/java/org/openhab/binding/zwave/internal/protocol/ZWaveController.java
+++ bundles/binding/org.openhab.binding.zwave/src/main/java/org/openhab/binding/zwave/internal/protocol/ZWaveController.java
@@ -324,26 +324,42 @@ public class ZWaveController {
 		
 		switch (updateState) {
 		case NODE_INFO_RECEIVED:
-			logger.debug("NODE {}: Application update request, node information received.", nodeId);			
-			int length = incomingMessage.getMessagePayloadByte(2);
 			ZWaveNode node = getNode(nodeId);
 			
-			node.resetResendCount();
 			
-			for (int i = 6; i < length + 3; i++) {
-				int data = incomingMessage.getMessagePayloadByte(i);
-				if(data == 0xef )  {
-					// TODO: Implement control command classes
-					break;
+			if(node.getNodeStage() == NodeStage.DONE) {
+				// if we receive an Application Update Request and the node is already
+				// fully initialised we assume this is a request to the controller to 
+				// re-get the current node values
+				logger.debug("Application update request, requesting node state.");
+
+				// reset and advance node stage to trigger the value request messages
+				node.setNodeStage(NodeStage.DYNAMIC);
+				node.advanceNodeStage(NodeStage.DONE);
+				
+				// make sure we've made it to the done stage, for some reason DYNAMIC
+				// has to be passed through twice.
+				if(node.getNodeStage() == NodeStage.DYNAMIC) 
+					node.advanceNodeStage(NodeStage.DONE);
+			} else {
+				logger.debug("NODE {}: Application update request, node information received.", nodeId);			
+				int length = incomingMessage.getMessagePayloadByte(2);
+				node.resetResendCount();
+				for (int i = 6; i < length + 3; i++) {
+					int data = incomingMessage.getMessagePayloadByte(i);
+					if(data == 0xef )  {
+						// TODO: Implement control command classes
+						break;
+					}
+					logger.debug(String.format("NODE %d: Adding command class 0x%02X to the list of supported command classes.", nodeId, data));
+					ZWaveCommandClass commandClass = ZWaveCommandClass.getInstance(data, node, this);
+					if (commandClass != null)
+						node.addCommandClass(commandClass);
 				}
-				logger.debug(String.format("NODE %d: Adding command class 0x%02X to the list of supported command classes.", nodeId, data));
-				ZWaveCommandClass commandClass = ZWaveCommandClass.getInstance(data, node, this);
-				if (commandClass != null)
-					node.addCommandClass(commandClass);
-			}
 			
-			// advance node stage.
-			node.advanceNodeStage(NodeStage.MANSPEC01);
+				// advance node stage.
+				node.advanceNodeStage(NodeStage.MANSPEC01);
+			}
 			
 			if (incomingMessage.getMessageClass() == this.lastSentMessage.getExpectedReply() && !incomingMessage.isTransActionCanceled()) {
 				notifyEventListeners(new ZWaveTransactionCompletedEvent(this.lastSentMessage));
