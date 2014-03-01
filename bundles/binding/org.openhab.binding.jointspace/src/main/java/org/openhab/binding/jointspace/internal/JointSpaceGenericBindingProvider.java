5373f7854d5cf96dcf374604915ece8afe3891c9
Lenzebo <Lenzebo@googlemail.com>
2014-03-01 17:07:52 +0100

    initial commit for jointspace binding

---
+++ bundles/binding/org.openhab.binding.jointspace/src/main/java/org/openhab/binding/jointspace/internal/JointSpaceGenericBindingProvider.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2010-2014, openHAB.org and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.jointspace.internal;
+
+import org.openhab.binding.jointspace.JointSpaceBindingProvider;
+import org.openhab.core.binding.BindingConfig;
+import org.openhab.core.items.Item;
+import org.openhab.core.library.items.DimmerItem;
+import org.openhab.core.library.items.SwitchItem;
+import org.openhab.model.item.binding.AbstractGenericBindingProvider;
+import org.openhab.model.item.binding.BindingConfigParseException;
+
+
+/**
+ * This class is responsible for parsing the binding configuration.
+ * 
+ * @author „Lenzebo“
+ * @since 1.5.0
+ */
+public class JointSpaceGenericBindingProvider extends AbstractGenericBindingProvider implements JointSpaceBindingProvider {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public String getBindingType() {
+		return "jointspace";
+	}
+
+	/**
+	 * @{inheritDoc}
+	 */
+	@Override
+	public void validateItemType(Item item, String bindingConfig) throws BindingConfigParseException {
+		//if (!(item instanceof SwitchItem || item instanceof DimmerItem)) {
+		//	throw new BindingConfigParseException("item '" + item.getName()
+		//			+ "' is of type '" + item.getClass().getSimpleName()
+		//			+ "', only Switch- and DimmerItems are allowed - please check your *.items configuration");
+		//}
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void processBindingConfiguration(String context, Item item, String bindingConfig) throws BindingConfigParseException {
+		super.processBindingConfiguration(context, item, bindingConfig);
+		JointSpaceBindingConfig config = new JointSpaceBindingConfig();
+		
+		//parse bindingconfig here ...
+		
+		addBindingConfig(item, config);		
+	}
+	
+	
+	class JointSpaceBindingConfig implements BindingConfig {
+		// put member fields here which holds the parsed values
+	}
+	
+	
+}
