14f3d08eb9f6ddbc341e7446ba1ef7c347ca84de
Jarle Hjortland <jbh@jarles-mbp.nb1.com>
2014-04-03 22:16:43 +0200

    Fixed javadoc and copyright

--- bundles/binding/org.openhab.binding.tellstick/src/main/java/org/openhab/binding/tellstick/internal/device/DimmableDevice.java
+++ bundles/binding/org.openhab.binding.tellstick/src/main/java/org/openhab/binding/tellstick/internal/device/DimmableDevice.java
@@ -1,9 +1,22 @@
+/**
+ * Copyright (c) 2010-2014, openHAB.org and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.openhab.binding.tellstick.internal.device;
 
 import org.openhab.binding.tellstick.internal.JNA;
 import org.openhab.binding.tellstick.internal.device.iface.DimmableDeviceIntf;
 
-
+/**
+ * A dimmable tellstick device.
+ * 
+ * @author jarlebh
+ * @since 1.5.0
+ */
 public class DimmableDevice extends Device implements DimmableDeviceIntf {
 
 
--- bundles/binding/org.openhab.binding.tellstick/src/main/java/org/openhab/binding/tellstick/internal/device/TellsticEventHandler.java
+++ bundles/binding/org.openhab.binding.tellstick/src/main/java/org/openhab/binding/tellstick/internal/device/TellsticEventHandler.java
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2010-2014, openHAB.org and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.openhab.binding.tellstick.internal.device;
 
 import java.util.ArrayList;
@@ -14,7 +22,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.sun.jna.Pointer;
-
+/**
+ * A callback listener to telldus that distributes to other listeners.
+ * 
+ * @author jarlebh
+ * @since 1.5.0
+ */
 public class TellsticEventHandler {
 
 	private static final Logger logger = LoggerFactory
