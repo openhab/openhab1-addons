d52fa9dd939201bfc8a4cce112e8d10b93708d52
Anthony Green <ajgreen@cantab.net>
2014-04-05 17:30:44 +0100

    Updates following review from teichsta

--- bundles/binding/org.openhab.binding.k8055/src/main/java/org/openhab/binding/k8055/internal/k8055Binding.java
+++ bundles/binding/org.openhab.binding.k8055/src/main/java/org/openhab/binding/k8055/internal/k8055Binding.java
@@ -1,30 +1,10 @@
 /**
- * openHAB, the open Home Automation Bus.
- * Copyright (C) 2010-2013, openHAB.org <admin@openhab.org>
+ * Copyright (c) 2010-2013, openHAB.org and others.
  *
- * See the contributors.txt file in the distribution for a
- * full listing of individual contributors.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, see <http://www.gnu.org/licenses>.
- *
- * Additional permission under GNU GPL version 3 section 7
- *
- * If you modify this Program, or any covered work, by linking or
- * combining it with Eclipse (or a modified version of that library),
- * containing parts covered by the terms of the Eclipse Public License
- * (EPL), the licensors of this Program grant you additional permission
- * to convey the resulting work.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  */
 package org.openhab.binding.k8055.internal;
 
@@ -56,7 +36,7 @@ import com.sun.jna.NativeLibrary;
  * K8055 Binding that polls the USB Hardware
  * 
  * @author Anthony Green
- * @since 1.3.0
+ * @since 1.4.0
  */
 public class k8055Binding extends AbstractActiveBinding<k8055BindingProvider> implements ManagedService {
 
@@ -70,11 +50,6 @@ public class k8055Binding extends AbstractActiveBinding<k8055BindingProvider> im
 	 */
 	private long refreshInterval = 1000;
 
-	// Details of the available hardware
-	static final int NUM_DIGITAL_INPUTS = 5;
-	static final int NUM_DIGITAL_OUTPUTS = 8;
-	static final int NUM_ANALOG_INPUTS = 2;
-	static final int NUM_ANALOG_OUTPUTS = 2;
 
 	static LibK8055 sysLibrary;	
 	int boardNo = 0;
@@ -99,28 +74,22 @@ public class k8055Binding extends AbstractActiveBinding<k8055BindingProvider> im
 				sysLibrary = (LibK8055) Native.synchronizedLibrary((Library) Native.loadLibrary("k8055", LibK8055.class));
 				logger.debug("Done loading native code library");
 			}
-		} catch (Exception e) {
-			logger.error("Failed to load K8055 native library " + e.getMessage(), e);
-		}
+		
 
-		if (!connected) {
-			if (sysLibrary.OpenDevice(boardNo) == boardNo) {
-				connected = true;
-				
-				// We don't really know the existing state - so this results in the state of all inputs being republished
-				lastDigitalInputs = -1;
-				
-				try {
-					Thread.sleep(100);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+			if (!connected) {
+				if (sysLibrary.OpenDevice(boardNo) == boardNo) {
+					connected = true;
+					
+					// We don't really know the existing state - so this results in the state of all inputs being republished
+					lastDigitalInputs = -1;
+					logger.info("K8055: Connect to board: " + boardNo + " succeeeded.");
+				} else {
+					logger.error("K8055: Connect to board: " + boardNo + " failed.");
 				}
-				logger.info("K8055: Connect to board: " + boardNo + " succeeeded.");
-			} else {
-				logger.error("K8055: Connect to board: " + boardNo + " failed.");
-			}
-		};
+			};
+		} catch (Exception e) {
+			logger.error("Failed to load K8055 native library.  Please check the libk8055 and jna native libraries are in the Java library path. ", e);
+		}
 
 		return connected;
 	}
@@ -170,14 +139,16 @@ public class k8055Binding extends AbstractActiveBinding<k8055BindingProvider> im
 		logger.debug("execute() method is called!");
 		try {
 			if (!connect()) { 
-				logger.error("driver.K8055.accessHardware():  Unable to access hardware.");
+				logger.error("Not connected to hardware. Skipping attempt to read inputs.");
 				return;
 			}
 
 			// Read all of the digital inputs in one go
 			long inputs = sysLibrary.ReadAllDigital();
 			
-			if (inputs < 0){throw new Exception("Connection failure");}
+			if (inputs < 0){
+			  throw new Exception("Failed to read digital inputs from hardware");
+			}
 
 			for (k8055BindingProvider provider : this.providers) {
 				for (String itemName : provider.getItemNames()) {
@@ -197,6 +168,10 @@ public class k8055Binding extends AbstractActiveBinding<k8055BindingProvider> im
 						}
 					} else if (IOType.ANALOG_IN.equals(config.ioType)) {
 						int state = sysLibrary.ReadAnalogChannel(config.ioNumber);
+						if (inputs < 0){
+							throw new Exception("Failed to read analog channel " + config.ioNumber + " from hardware");
+						}
+
 						eventPublisher.postUpdate(itemName, new DecimalType(state));
 					}
 				}
@@ -207,7 +182,7 @@ public class k8055Binding extends AbstractActiveBinding<k8055BindingProvider> im
 
 		} catch (Exception e){
 			// Connection failure
-			logger.error("K8055 Connection failure");
+			logger.error("Failed to read from hardware ", e);
 			connected = false;
 			sysLibrary.CloseDevice();
 		}
@@ -222,45 +197,52 @@ public class k8055Binding extends AbstractActiveBinding<k8055BindingProvider> im
 		logger.debug("Received command for: " + itemName + " - " + command);
 		k8055BindingConfig itemConfig = getConfigForItemName(itemName);
 
-		if (itemConfig.ioType == IOType.DIGITAL_OUT) {
-			logger.debug("Updating hardware Digital output: " + itemConfig.ioNumber);
-			if (connect()) {
-				if (OnOffType.ON.equals(command)) {
-					sysLibrary.SetDigitalChannel(itemConfig.ioNumber);
-				} else if (OnOffType.OFF.equals(command)) {
-					sysLibrary.ClearDigitalChannel(itemConfig.ioNumber);
+		try {
+			if (itemConfig.ioType == IOType.DIGITAL_OUT) {
+				logger.debug("Updating hardware Digital output: " + itemConfig.ioNumber);
+				if (connect()) {
+					if (OnOffType.ON.equals(command)) {
+						if (sysLibrary.SetDigitalChannel(itemConfig.ioNumber) < 0) { throw new Exception ("Failed to set digital channel: " + itemConfig.ioNumber); }
+					} else if (OnOffType.OFF.equals(command)) {
+						if (sysLibrary.ClearDigitalChannel(itemConfig.ioNumber) < 0) { throw new Exception ("Failed to set digital channel: " + itemConfig.ioNumber); };
+					} else {
+						logger.error("Received unknown command: " + command + " for item: " + itemName);
+					}
 				} else {
-					logger.error("Received unknown command: " + command + " for item: " + itemName);
+					logger.error("Not connected to hardware.  Command: " + command + " for item: " + itemName + " ignored");
 				}
-			} else {
-				logger.error("Not connected to hardware.  Command: " + command + " for item: " + itemName + " ignored");
-			}
-		} else if (itemConfig.ioType == IOType.ANALOG_OUT) {
-			logger.debug("Updating hardware Analog output: " + itemConfig.ioNumber);
-			// TODO: Implement Increase/decrease commands
-			if (connect()) {
-				if (OnOffType.ON.equals(command)) {
-					sysLibrary.SetAnalogChannel(itemConfig.ioNumber);
-					
-				} else if (OnOffType.OFF.equals(command)) {
-					sysLibrary.ClearAnalogChannel(itemConfig.ioNumber);
-					
-				} else if (command instanceof PercentType) {
-					// Convert 0-100% to 0-255
-					int value = Math.round((((PercentType)command ).shortValue()*255)/100);
-					sysLibrary.OutputAnalogChannel(itemConfig.ioNumber, value);
-					
-				} else if (command instanceof DecimalType) {
-					// Force number into 0-255 range.
-					sysLibrary.OutputAnalogChannel(itemConfig.ioNumber, Math.min(Math.max(((DecimalType)command).intValue(),0),255));
+			} else if (itemConfig.ioType == IOType.ANALOG_OUT) {
+				logger.debug("Updating hardware Analog output: " + itemConfig.ioNumber);
+				// TODO: Implement Increase/decrease commands
+				if (connect()) {
+					if (OnOffType.ON.equals(command)) {
+						if (sysLibrary.SetAnalogChannel(itemConfig.ioNumber) < 0) { throw new Exception ("Failed to set analog channel: " + itemConfig.ioNumber); };
+						
+					} else if (OnOffType.OFF.equals(command)) {
+						if (sysLibrary.ClearAnalogChannel(itemConfig.ioNumber)  < 0) { throw new Exception ("Failed to clear analog channel: " + itemConfig.ioNumber); };
+						
+					} else if (command instanceof PercentType) {
+						// Convert 0-100% to 0-255
+						int value = Math.round((((PercentType)command ).shortValue()*255)/100);
+						if (sysLibrary.OutputAnalogChannel(itemConfig.ioNumber, value)  < 0) { throw new Exception ("Failed to output analog channel: " + itemConfig.ioNumber); };
+						
+					} else if (command instanceof DecimalType) {
+						// Force number into 0-255 range.
+						if (sysLibrary.OutputAnalogChannel(itemConfig.ioNumber, Math.min(Math.max(((DecimalType)command).intValue(),0),255)) < 0) { throw new Exception ("Failed to output analog channel: " + itemConfig.ioNumber); };
+					} else {
+						logger.error("Received unknown command: " + command + " for item: " + itemName);
+					}
 				} else {
-					logger.error("Received unknown command: " + command + " for item: " + itemName);
+					logger.error("Not connected to hardware.  Command: " + command + " for item: " + itemName + " ignored");
 				}
-			} else {
-				logger.error("Not connected to hardware.  Command: " + command + " for item: " + itemName + " ignored");
 			}
+      
+		} catch (Exception e){
+			// Connection failure
+			logger.error("Failed to read from hardware ", e);
+			connected = false;
+			sysLibrary.CloseDevice();
 		}
-
 
 
 		// the code being executed when a command was sent on the openHAB
