f67b96bec2372736712a80060bf1cb74e4191a9d
Anthony Green <ajgreen@cantab.net>
2014-04-05 18:28:48 +0100

    Reformat code using eclipse code formatter.  Fix minor compilation warnings.

--- bundles/binding/org.openhab.binding.k8055/src/main/java/org/openhab/binding/k8055/internal/k8055Binding.java
+++ bundles/binding/org.openhab.binding.k8055/src/main/java/org/openhab/binding/k8055/internal/k8055Binding.java
@@ -10,13 +10,10 @@ package org.openhab.binding.k8055.internal;
 
 import java.util.Dictionary;
 
+import org.apache.commons.lang.StringUtils;
 import org.openhab.binding.k8055.k8055BindingProvider;
 import org.openhab.binding.k8055.internal.k8055GenericBindingProvider.k8055BindingConfig;
-
-import org.apache.commons.lang.StringUtils;
 import org.openhab.core.binding.AbstractActiveBinding;
-import org.openhab.core.library.items.ContactItem;
-import org.openhab.core.library.items.DimmerItem;
 import org.openhab.core.library.types.DecimalType;
 import org.openhab.core.library.types.OnOffType;
 import org.openhab.core.library.types.OpenClosedType;
@@ -27,10 +24,9 @@ import org.osgi.service.cm.ConfigurationException;
 import org.osgi.service.cm.ManagedService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import com.sun.jna.Library;
 import com.sun.jna.Native;
-import com.sun.jna.NativeLibrary; 
-
 
 /**
  * K8055 Binding that polls the USB Hardware
@@ -38,57 +34,63 @@ import com.sun.jna.NativeLibrary;
  * @author Anthony Green
  * @since 1.5.0
  */
-public class k8055Binding extends AbstractActiveBinding<k8055BindingProvider> implements ManagedService {
+public class k8055Binding extends AbstractActiveBinding<k8055BindingProvider>
+		implements ManagedService {
 
-	private static final Logger logger = 
-			LoggerFactory.getLogger(k8055Binding.class);
+	private static final Logger logger = LoggerFactory
+			.getLogger(k8055Binding.class);
 
-
-	/** 
-	 * the refresh interval which is used to poll values from the k8055
-	 * server (optional, defaults to 1000ms)
+	/**
+	 * the refresh interval which is used to poll values from the k8055 server
+	 * (optional, defaults to 1000ms)
 	 */
 	private long refreshInterval = 1000;
 
-
-	static LibK8055 sysLibrary;	
+	static LibK8055 sysLibrary;
 	int boardNo = 0;
 
 	/**
 	 * Previously read digital input state
 	 */
 	long lastDigitalInputs = -1;
-	
+
 	/**
 	 * Are we currently connected to the hardware?
 	 */
-	boolean connected;	
+	boolean connected;
 
 	public k8055Binding() {
 	}
 
-	protected boolean connect () {
+	protected boolean connect() {
 		try {
-			if (sysLibrary == null ) {
+			if (sysLibrary == null) {
 				logger.debug("Loading native code library...");
-				sysLibrary = (LibK8055) Native.synchronizedLibrary((Library) Native.loadLibrary("k8055", LibK8055.class));
+				sysLibrary = (LibK8055) Native
+						.synchronizedLibrary((Library) Native.loadLibrary(
+								"k8055", LibK8055.class));
 				logger.debug("Done loading native code library");
 			}
-		
 
 			if (!connected) {
 				if (sysLibrary.OpenDevice(boardNo) == boardNo) {
 					connected = true;
-					
-					// We don't really know the existing state - so this results in the state of all inputs being republished
+
+					// We don't really know the existing state - so this results
+					// in the state of all inputs being republished
 					lastDigitalInputs = -1;
-					logger.info("K8055: Connect to board: " + boardNo + " succeeeded.");
+					logger.info("K8055: Connect to board: " + boardNo
+							+ " succeeeded.");
 				} else {
-					logger.error("K8055: Connect to board: " + boardNo + " failed.");
+					logger.error("K8055: Connect to board: " + boardNo
+							+ " failed.");
 				}
-			};
+			}
+			;
 		} catch (Exception e) {
-			logger.error("Failed to load K8055 native library.  Please check the libk8055 and jna native libraries are in the Java library path. ", e);
+			logger.error(
+					"Failed to load K8055 native library.  Please check the libk8055 and jna native libraries are in the Java library path. ",
+					e);
 		}
 
 		return connected;
@@ -113,9 +115,8 @@ public class k8055Binding extends AbstractActiveBinding<k8055BindingProvider> im
 		sysLibrary = null;
 	}
 
-
 	/**
-	 * @{inheritDoc}
+	 * @{inheritDoc
 	 */
 	@Override
 	protected long getRefreshInterval() {
@@ -123,64 +124,74 @@ public class k8055Binding extends AbstractActiveBinding<k8055BindingProvider> im
 	}
 
 	/**
-	 * @{inheritDoc}
+	 * @{inheritDoc
 	 */
 	@Override
 	protected String getName() {
 		return "k8055 Refresh Service";
 	}
 
-
 	/**
-	 * @{inheritDoc}
+	 * @{inheritDoc
 	 */
 	@Override
 	protected void execute() {
 		logger.debug("execute() method is called!");
 		try {
-			if (!connect()) { 
+			if (!connect()) {
 				logger.error("Not connected to hardware. Skipping attempt to read inputs.");
 				return;
 			}
 
 			// Read all of the digital inputs in one go
 			long inputs = sysLibrary.ReadAllDigital();
-			
-			if (inputs < 0){
-			  throw new Exception("Failed to read digital inputs from hardware");
+
+			if (inputs < 0) {
+				throw new Exception(
+						"Failed to read digital inputs from hardware");
 			}
 
 			for (k8055BindingProvider provider : this.providers) {
 				for (String itemName : provider.getItemNames()) {
-					k8055BindingConfig config = provider.getItemConfig(itemName);
-					if (IOType.DIGITAL_IN.equals(config.ioType)){
+					k8055BindingConfig config = provider
+							.getItemConfig(itemName);
+					if (IOType.DIGITAL_IN.equals(config.ioType)) {
 						boolean newstate = (inputs & (1 << (config.ioNumber - 1))) != 0;
 						boolean oldstate = (lastDigitalInputs & (1 << (config.ioNumber - 1))) != 0;
-						
-						// For quick response times a very short poll interval is required.  To avoid flooding
-						// masses of events to the event bus, just post updates when changes occur (or when first connecting to hardware)
+
+						// For quick response times a very short poll interval
+						// is required. To avoid flooding
+						// masses of events to the event bus, just post updates
+						// when changes occur (or when first connecting to
+						// hardware)
 						if (lastDigitalInputs < 0 || newstate != oldstate) {
 							if (newstate) {
-								eventPublisher.postUpdate(itemName, OpenClosedType.CLOSED);
+								eventPublisher.postUpdate(itemName,
+										OpenClosedType.CLOSED);
 							} else {
-								eventPublisher.postUpdate(itemName, OpenClosedType.OPEN);
+								eventPublisher.postUpdate(itemName,
+										OpenClosedType.OPEN);
 							}
 						}
 					} else if (IOType.ANALOG_IN.equals(config.ioType)) {
-						int state = sysLibrary.ReadAnalogChannel(config.ioNumber);
-						if (inputs < 0){
-							throw new Exception("Failed to read analog channel " + config.ioNumber + " from hardware");
+						int state = sysLibrary
+								.ReadAnalogChannel(config.ioNumber);
+						if (inputs < 0) {
+							throw new Exception(
+									"Failed to read analog channel "
+											+ config.ioNumber
+											+ " from hardware");
 						}
 
-						eventPublisher.postUpdate(itemName, new DecimalType(state));
+						eventPublisher.postUpdate(itemName, new DecimalType(
+								state));
 					}
 				}
 			}
-			
-			lastDigitalInputs = inputs;
-			
 
-		} catch (Exception e){
+			lastDigitalInputs = inputs;
+
+		} catch (Exception e) {
 			// Connection failure
 			logger.error("Failed to read from hardware ", e);
 			connected = false;
@@ -190,7 +201,7 @@ public class k8055Binding extends AbstractActiveBinding<k8055BindingProvider> im
 	}
 
 	/**
-	 * @{inheritDoc}
+	 * @{inheritDoc
 	 */
 	@Override
 	protected void internalReceiveCommand(String itemName, Command command) {
@@ -199,85 +210,122 @@ public class k8055Binding extends AbstractActiveBinding<k8055BindingProvider> im
 
 		try {
 			if (itemConfig.ioType == IOType.DIGITAL_OUT) {
-				logger.debug("Updating hardware Digital output: " + itemConfig.ioNumber);
+				logger.debug("Updating hardware Digital output: "
+						+ itemConfig.ioNumber);
 				if (connect()) {
 					if (OnOffType.ON.equals(command)) {
-						if (sysLibrary.SetDigitalChannel(itemConfig.ioNumber) < 0) { throw new Exception ("Failed to set digital channel: " + itemConfig.ioNumber); }
+						if (sysLibrary.SetDigitalChannel(itemConfig.ioNumber) < 0) {
+							throw new Exception(
+									"Failed to set digital channel: "
+											+ itemConfig.ioNumber);
+						}
 					} else if (OnOffType.OFF.equals(command)) {
-						if (sysLibrary.ClearDigitalChannel(itemConfig.ioNumber) < 0) { throw new Exception ("Failed to set digital channel: " + itemConfig.ioNumber); };
+						if (sysLibrary.ClearDigitalChannel(itemConfig.ioNumber) < 0) {
+							throw new Exception(
+									"Failed to set digital channel: "
+											+ itemConfig.ioNumber);
+						}
+						;
 					} else {
-						logger.error("Received unknown command: " + command + " for item: " + itemName);
+						logger.error("Received unknown command: " + command
+								+ " for item: " + itemName);
 					}
 				} else {
-					logger.error("Not connected to hardware.  Command: " + command + " for item: " + itemName + " ignored");
+					logger.error("Not connected to hardware.  Command: "
+							+ command + " for item: " + itemName + " ignored");
 				}
 			} else if (itemConfig.ioType == IOType.ANALOG_OUT) {
-				logger.debug("Updating hardware Analog output: " + itemConfig.ioNumber);
+				logger.debug("Updating hardware Analog output: "
+						+ itemConfig.ioNumber);
 				// TODO: Implement Increase/decrease commands
 				if (connect()) {
 					if (OnOffType.ON.equals(command)) {
-						if (sysLibrary.SetAnalogChannel(itemConfig.ioNumber) < 0) { throw new Exception ("Failed to set analog channel: " + itemConfig.ioNumber); };
-						
+						if (sysLibrary.SetAnalogChannel(itemConfig.ioNumber) < 0) {
+							throw new Exception(
+									"Failed to set analog channel: "
+											+ itemConfig.ioNumber);
+						}
+						;
+
 					} else if (OnOffType.OFF.equals(command)) {
-						if (sysLibrary.ClearAnalogChannel(itemConfig.ioNumber)  < 0) { throw new Exception ("Failed to clear analog channel: " + itemConfig.ioNumber); };
-						
+						if (sysLibrary.ClearAnalogChannel(itemConfig.ioNumber) < 0) {
+							throw new Exception(
+									"Failed to clear analog channel: "
+											+ itemConfig.ioNumber);
+						}
+						;
+
 					} else if (command instanceof PercentType) {
 						// Convert 0-100% to 0-255
-						int value = Math.round((((PercentType)command ).shortValue()*255)/100);
-						if (sysLibrary.OutputAnalogChannel(itemConfig.ioNumber, value)  < 0) { throw new Exception ("Failed to output analog channel: " + itemConfig.ioNumber); };
-						
+						int value = Math.round((((PercentType) command)
+								.shortValue() * 255) / 100);
+						if (sysLibrary.OutputAnalogChannel(itemConfig.ioNumber,
+								value) < 0) {
+							throw new Exception(
+									"Failed to output analog channel: "
+											+ itemConfig.ioNumber);
+						}
+						;
+
 					} else if (command instanceof DecimalType) {
 						// Force number into 0-255 range.
-						if (sysLibrary.OutputAnalogChannel(itemConfig.ioNumber, Math.min(Math.max(((DecimalType)command).intValue(),0),255)) < 0) { throw new Exception ("Failed to output analog channel: " + itemConfig.ioNumber); };
+						if (sysLibrary.OutputAnalogChannel(itemConfig.ioNumber,
+								Math.min(Math.max(
+										((DecimalType) command).intValue(), 0),
+										255)) < 0) {
+							throw new Exception(
+									"Failed to output analog channel: "
+											+ itemConfig.ioNumber);
+						}
+						;
 					} else {
-						logger.error("Received unknown command: " + command + " for item: " + itemName);
+						logger.error("Received unknown command: " + command
+								+ " for item: " + itemName);
 					}
 				} else {
-					logger.error("Not connected to hardware.  Command: " + command + " for item: " + itemName + " ignored");
+					logger.error("Not connected to hardware.  Command: "
+							+ command + " for item: " + itemName + " ignored");
 				}
 			}
-      
-		} catch (Exception e){
+
+		} catch (Exception e) {
 			// Connection failure
 			logger.error("Failed to read from hardware ", e);
 			connected = false;
 			sysLibrary.CloseDevice();
 		}
 
-
 		// the code being executed when a command was sent on the openHAB
-		// event bus goes here. This method is only called if one of the 
+		// event bus goes here. This method is only called if one of the
 		// BindingProviders provide a binding for the given 'itemName'.
 		logger.debug("internalReceiveCommand() is called!");
 	}
 
 	/**
-	 * @{inheritDoc}
+	 * @{inheritDoc
 	 */
 	@Override
 	protected void internalReceiveUpdate(String itemName, State newState) {
 		// the code being executed when a state was sent on the openHAB
-		// event bus goes here. This method is only called if one of the 
+		// event bus goes here. This method is only called if one of the
 		// BindingProviders provide a binding for the given 'itemName'.
 		logger.debug("internalReceiveUpdate() is called!");
 	}
 
-
-
-
-
 	/**
-	 * @{inheritDoc}
+	 * @{inheritDoc
 	 */
 	@Override
-	public void updated(Dictionary<String, ?> config) throws ConfigurationException {
+	public void updated(Dictionary<String, ?> config)
+			throws ConfigurationException {
 
 		logger.debug("updated() called");
 		setProperlyConfigured(false);
 		if (config != null) {
 
-			// to override the default refresh interval one has to add a 
-			// parameter to openhab.cfg like <bindingName>:refresh=<intervalInMs>
+			// to override the default refresh interval one has to add a
+			// parameter to openhab.cfg like
+			// <bindingName>:refresh=<intervalInMs>
 			String refreshIntervalString = (String) config.get("refresh");
 			if (StringUtils.isNotBlank(refreshIntervalString)) {
 				refreshInterval = Long.parseLong(refreshIntervalString);
@@ -290,7 +338,8 @@ public class k8055Binding extends AbstractActiveBinding<k8055BindingProvider> im
 					setProperlyConfigured(true);
 				} catch (NumberFormatException e) {
 					logger.error("Invalid board number: " + boardNum);
-					throw new ConfigurationException("boardno", boardNum + " is not a valid board number.");
+					throw new ConfigurationException("boardno", boardNum
+							+ " is not a valid board number.");
 				}
 			}
 
@@ -321,33 +370,52 @@ public class k8055Binding extends AbstractActiveBinding<k8055BindingProvider> im
 	}
 
 	/**
-	 * Describes the interface of the K8055 C Library.  (Used by JNA)
+	 * Describes the interface of the K8055 C Library. (Used by JNA)
 	 * 
 	 * @author anthony
-	 *
-	 */	
+	 * 
+	 */
 
 	public interface LibK8055 extends Library {
 		/* prototypes */
 		int OpenDevice(int board_address);
+
 		int CloseDevice();
+
 		int ReadAnalogChannel(int Channelno);
+
 		int ReadAllAnalog(int[] data1, int[] data2);
+
 		int OutputAnalogChannel(int channel, int data);
-		int OutputAllAnalog(int data1,int data2);
+
+		int OutputAllAnalog(int data1, int data2);
+
 		int ClearAllAnalog();
+
 		int ClearAnalogChannel(int channel);
+
 		int SetAnalogChannel(int channel);
+
 		int SetAllAnalog();
+
 		int WriteAllDigital(int data);
+
 		int ClearDigitalChannel(int channel);
+
 		int ClearAllDigital();
+
 		int SetDigitalChannel(int channel);
+
 		int SetAllDigital();
+
 		int ReadDigitalChannel(int channel);
+
 		long ReadAllDigital();
+
 		int ResetCounter(int counternr);
+
 		long ReadCounter(int counterno);
+
 		int SetCounterDebounceTime(int counterno, int debouncetime);
 	}
 
--- bundles/binding/org.openhab.binding.k8055/src/main/java/org/openhab/binding/k8055/internal/k8055GenericBindingProvider.java
+++ bundles/binding/org.openhab.binding.k8055/src/main/java/org/openhab/binding/k8055/internal/k8055GenericBindingProvider.java
@@ -7,6 +7,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  */
 package org.openhab.binding.k8055.internal;
+
 import org.openhab.binding.k8055.k8055BindingProvider;
 import org.openhab.core.binding.BindingConfig;
 import org.openhab.core.items.Item;
@@ -15,24 +16,24 @@ import org.openhab.model.item.binding.BindingConfigParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 /**
  * This class is responsible for parsing the binding configuration.
  * 
  * @author Anthony Green
  * @since 1.5.0
  */
-public class k8055GenericBindingProvider extends AbstractGenericBindingProvider implements k8055BindingProvider {
+public class k8055GenericBindingProvider extends AbstractGenericBindingProvider
+		implements k8055BindingProvider {
 
-	private static final Logger logger = 
-			LoggerFactory.getLogger(k8055GenericBindingProvider.class);
-	
+	private static final Logger logger = LoggerFactory
+			.getLogger(k8055GenericBindingProvider.class);
+
 	// Details of the available hardware
 	static final int NUM_DIGITAL_INPUTS = 5;
 	static final int NUM_DIGITAL_OUTPUTS = 8;
 	static final int NUM_ANALOG_INPUTS = 2;
 	static final int NUM_ANALOG_OUTPUTS = 2;
-	
+
 	/**
 	 * {@inheritDoc}
 	 */
@@ -41,78 +42,92 @@ public class k8055GenericBindingProvider extends AbstractGenericBindingProvider 
 	}
 
 	/**
-	 * @{inheritDoc}
+	 * @{inheritDoc
 	 */
 	@Override
-	public void validateItemType(Item item, String bindingConfig) throws BindingConfigParseException {
-		k8055BindingConfig config = parseBindingConfig(bindingConfig);
-	
-		// TODO: Validate binding types
-	
+	public void validateItemType(Item item, String bindingConfig)
+			throws BindingConfigParseException {
+		parseBindingConfig(bindingConfig);
 	}
-	
+
 	/**
-	 * Parse binding string.  Examples of what is allowed are:
+	 * Parse binding string. Examples of what is allowed are:
 	 * 
-	 * DIGITAL_IN:1
-	 * ANALOG_IN:2
-	 * DIGITAL_OUT:3 
+	 * DIGITAL_IN:1 ANALOG_IN:2 DIGITAL_OUT:3
 	 * 
 	 */
 	@Override
-	public void processBindingConfiguration(String context, Item item, String bindingConfig) throws BindingConfigParseException {
+	public void processBindingConfiguration(String context, Item item,
+			String bindingConfig) throws BindingConfigParseException {
 		super.processBindingConfiguration(context, item, bindingConfig);
-		logger.debug("processing binding config: " + item.getName() + "; " + bindingConfig);
-		addBindingConfig(item, parseBindingConfig(bindingConfig));		
+		logger.debug("processing binding config: " + item.getName() + "; "
+				+ bindingConfig);
+		addBindingConfig(item, parseBindingConfig(bindingConfig));
 	}
-	
-	protected k8055BindingConfig parseBindingConfig(String bindingConfig) throws BindingConfigParseException {
+
+	protected k8055BindingConfig parseBindingConfig(String bindingConfig)
+			throws BindingConfigParseException {
 		k8055BindingConfig config = new k8055BindingConfig();
-		
+
 		String[] configParts = bindingConfig.split(":");
 		if (configParts.length != 2) {
-			throw new BindingConfigParseException("Unable to parse k8055 binding string: " + bindingConfig + ".  Incorrect number of colons.");
+			throw new BindingConfigParseException(
+					"Unable to parse k8055 binding string: " + bindingConfig
+							+ ".  Incorrect number of colons.");
 		}
-		
+
 		try {
 			config.ioNumber = Integer.parseInt(configParts[1]);
 			config.ioType = IOType.valueOf(configParts[0]);
 		} catch (NumberFormatException e) {
-			throw new BindingConfigParseException("Unable to parse k8055 binding string: " + bindingConfig + ". Could not parse input number: " + configParts[1]);
+			throw new BindingConfigParseException(
+					"Unable to parse k8055 binding string: " + bindingConfig
+							+ ". Could not parse input number: "
+							+ configParts[1]);
 		} catch (IllegalArgumentException e) {
-			throw new BindingConfigParseException("Unable to parse k8055 binding string: " + bindingConfig + ". Invalid input type: " + configParts[0]);
-		}		
-		
+			throw new BindingConfigParseException(
+					"Unable to parse k8055 binding string: " + bindingConfig
+							+ ". Invalid input type: " + configParts[0]);
+		}
+
 		// Verify config is actually valid given the hardware
 		if (config.ioNumber < 1) {
-			throw new BindingConfigParseException("Unable to parse k8055 binding string: " + bindingConfig + ". IO channel must be greater than equal to 1 ");
-		} else if ((config.ioNumber > NUM_DIGITAL_INPUTS && config.ioType.equals(IOType.DIGITAL_IN))
-				|| (config.ioNumber > NUM_DIGITAL_OUTPUTS && config.ioType.equals(IOType.DIGITAL_OUT))
-				|| (config.ioNumber > NUM_ANALOG_INPUTS && config.ioType.equals(IOType.ANALOG_IN))
-				|| (config.ioNumber > NUM_ANALOG_OUTPUTS && config.ioType.equals(IOType.ANALOG_OUT))
-				) {
-			throw new BindingConfigParseException("Unable to parse k8055 binding string: " + bindingConfig + ". IO channel number was greater than the number of physicl channels ");
+			throw new BindingConfigParseException(
+					"Unable to parse k8055 binding string: " + bindingConfig
+							+ ". IO channel must be greater than equal to 1 ");
+		} else if ((config.ioNumber > NUM_DIGITAL_INPUTS && config.ioType
+				.equals(IOType.DIGITAL_IN))
+				|| (config.ioNumber > NUM_DIGITAL_OUTPUTS && config.ioType
+						.equals(IOType.DIGITAL_OUT))
+				|| (config.ioNumber > NUM_ANALOG_INPUTS && config.ioType
+						.equals(IOType.ANALOG_IN))
+				|| (config.ioNumber > NUM_ANALOG_OUTPUTS && config.ioType
+						.equals(IOType.ANALOG_OUT))) {
+			throw new BindingConfigParseException(
+					"Unable to parse k8055 binding string: "
+							+ bindingConfig
+							+ ". IO channel number was greater than the number of physicl channels ");
 		}
-		
+
 		return config;
 	}
-	
+
 	public class k8055BindingConfig implements BindingConfig {
-		
+
 		/**
 		 * Type of input or output
 		 */
 		IOType ioType;
-		
+
 		/**
 		 * Which Input or Output number is this?
 		 */
 		int ioNumber;
 	}
-	
+
 	@Override
 	public k8055BindingConfig getItemConfig(String itemName) {
 		return (k8055BindingConfig) bindingConfigs.get(itemName);
 	}
-	
+
 }
