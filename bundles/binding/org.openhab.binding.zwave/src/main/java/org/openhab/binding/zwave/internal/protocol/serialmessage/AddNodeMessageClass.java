7453e90fc965f1c8c9ae62111c02ce107f877237
Chris Jackson <chris@cd-jackson.com>
2014-05-04 21:31:17 +0100

    Ignore node 0 when including nodes

--- bundles/binding/org.openhab.binding.zwave/src/main/java/org/openhab/binding/zwave/internal/protocol/serialmessage/AddNodeMessageClass.java
+++ bundles/binding/org.openhab.binding.zwave/src/main/java/org/openhab/binding/zwave/internal/protocol/serialmessage/AddNodeMessageClass.java
@@ -89,7 +89,9 @@ public class AddNodeMessageClass extends ZWaveCommandProcessor {
 		case ADD_NODE_STATUS_DONE:
 			logger.debug("Done.");
 			zController.sendData(doRequestStop());
-			zController.notifyEventListeners(new ZWaveInclusionEvent(ZWaveInclusionEvent.Type.IncludeDone, incomingMessage.getMessagePayloadByte(2)));
+			// If the node ID is 0, ignore!
+			if(incomingMessage.getMessagePayloadByte(2) != 0)
+				zController.notifyEventListeners(new ZWaveInclusionEvent(ZWaveInclusionEvent.Type.IncludeDone, incomingMessage.getMessagePayloadByte(2)));
 			break;
 		case ADD_NODE_STATUS_FAILED:
 			logger.debug("Failed.");
