37a4574ffa0b9b2c32de25c83e09c3819b93f7cc
Sebastian Held <sebastian.held@gmx.de>
2014-05-31 15:43:58 +0200

    starting new binding: EcoTouch

---
+++ bundles/binding/org.openhab.binding.ecotouch/src/main/java/org/openhab/binding/ecotouch/internal/EcoTouchGenericBindingProvider.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2010-2014, openHAB.org and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.ecotouch.internal;
+
+import org.openhab.binding.ecotouch.EcoTouchBindingProvider;
+import org.openhab.core.binding.BindingConfig;
+import org.openhab.core.items.Item;
+import org.openhab.core.library.items.DimmerItem;
+import org.openhab.core.library.items.SwitchItem;
+import org.openhab.model.item.binding.AbstractGenericBindingProvider;
+import org.openhab.model.item.binding.BindingConfigParseException;
+
+
+/**
+ * This class is responsible for parsing the binding configuration.
+ * 
+ * @author Sebastian Held <sebastian.held@gmx.de>
+ * @since 1.5.0
+ */
+public class EcoTouchGenericBindingProvider extends AbstractGenericBindingProvider implements EcoTouchBindingProvider {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public String getBindingType() {
+		return "ecotouch";
+	}
+
+	/**
+	 * @{inheritDoc}
+	 */
+	@Override
+	public void validateItemType(Item item, String bindingConfig) throws BindingConfigParseException {
+		//if (!(item instanceof SwitchItem || item instanceof DimmerItem)) {
+		//	throw new BindingConfigParseException("item '" + item.getName()
+		//			+ "' is of type '" + item.getClass().getSimpleName()
+		//			+ "', only Switch- and DimmerItems are allowed - please check your *.items configuration");
+		//}
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void processBindingConfiguration(String context, Item item, String bindingConfig) throws BindingConfigParseException {
+		super.processBindingConfiguration(context, item, bindingConfig);
+		EcoTouchBindingConfig config = new EcoTouchBindingConfig();
+		
+		//parse bindingconfig here ...
+		
+		addBindingConfig(item, config);		
+	}
+	
+	
+	class EcoTouchBindingConfig implements BindingConfig {
+		// put member fields here which holds the parsed values
+	}
+	
+	
+}
