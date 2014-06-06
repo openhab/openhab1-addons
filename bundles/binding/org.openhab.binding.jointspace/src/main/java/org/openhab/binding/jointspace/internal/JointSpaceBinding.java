fb617de5b988f25f045a1189b8da5feec27b1e81
Lenzebo <Lenzebo@googlemail.com>
2014-06-06 23:45:44 +0200

    moved return statement

--- bundles/binding/org.openhab.binding.jointspace/src/main/java/org/openhab/binding/jointspace/internal/JointSpaceBinding.java
+++ bundles/binding/org.openhab.binding.jointspace/src/main/java/org/openhab/binding/jointspace/internal/JointSpaceBinding.java
@@ -126,20 +126,21 @@ public class JointSpaceBinding extends AbstractActiveBinding<JointSpaceBindingPr
 			}
 			else
 			{
+
 				logger.debug("couldn't establish network connection [host '{}' port '{}' timeout '{}']", new Object[] {ip, 0, timeout});
+				//if TV is not on, then we won't be able to poll it.
+				return;
 			}
 		} 
 		catch (SocketTimeoutException se) {
 			logger.debug("timed out while connecting to host '{}' port '{}' timeout '{}'", new Object[] {ip, 0, timeout});
+			return;
 		}
 		catch (IOException ioe) {
 			logger.debug("couldn't establish network connection [host '{}' port '{}' timeout '{}']", new Object[] {ip, 0, timeout});
-		}
-		//if TV is not on, then we won't be able to poll it.
-		if (!success)
-		{
 			return;
 		}
+
 		for (JointSpaceBindingProvider provider : providers) {
 			for (String itemName : provider.getItemNames()) {
 				String tvcommand = provider.getTVCommand(itemName, "POLL");
