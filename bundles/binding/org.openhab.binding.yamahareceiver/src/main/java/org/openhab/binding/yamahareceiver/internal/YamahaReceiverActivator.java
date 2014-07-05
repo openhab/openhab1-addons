cb1c49dbc3e61b73dde1b3f221d5e892a04423fc
Ben Jones <ben.jones12@gmail.com>
2014-07-05 14:29:03 +1200

    Initial checkin of Yamaha receiver binding - ready for code review

---
+++ bundles/binding/org.openhab.binding.yamahareceiver/src/main/java/org/openhab/binding/yamahareceiver/internal/YamahaReceiverActivator.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2010-2014, openHAB.org and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.yamahareceiver.internal;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Extension of the default OSGi bundle activator
+ * 
+ * @author Eric Thill
+ * @since 1.6.0
+ */
+public final class YamahaReceiverActivator implements BundleActivator {
+
+	private static Logger logger = LoggerFactory.getLogger(YamahaReceiverActivator.class); 
+	
+	private static BundleContext context;
+	
+	/**
+	 * Called whenever the OSGi framework starts our bundle
+	 */
+	public void start(BundleContext bc) throws Exception {
+		context = bc;
+		logger.debug("YamahaReceiver binding has been started.");
+	}
+
+	/**
+	 * Called whenever the OSGi framework stops our bundle
+	 */
+	public void stop(BundleContext bc) throws Exception {
+		context = null;
+		logger.debug("YamahaReceiver binding has been stopped.");
+	}
+	
+	/**
+	 * Returns the bundle context of this bundle
+	 * @return the bundle context
+	 */
+	public static BundleContext getContext() {
+		return context;
+	}
+	
+}
