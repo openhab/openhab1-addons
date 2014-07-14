23092a502e5f13bf2a920ad015c9b476e19e72e2
dhock999 <dhock999@gmail.com>
2014-07-14 11:47:03 -0700

    Added setpoint_scale to Thermostat Setpoint
    This allows specifying either C (0) or F(1) temperature scales

--- bundles/binding/org.openhab.binding.zwave/src/main/java/org/openhab/binding/zwave/internal/converter/ZWaveThermostatSetpointConverter.java
+++ bundles/binding/org.openhab.binding.zwave/src/main/java/org/openhab/binding/zwave/internal/converter/ZWaveThermostatSetpointConverter.java
@@ -19,6 +19,7 @@ import org.openhab.binding.zwave.internal.protocol.SerialMessage;
 import org.openhab.binding.zwave.internal.protocol.ZWaveController;
 import org.openhab.binding.zwave.internal.protocol.ZWaveNode;
 import org.openhab.binding.zwave.internal.protocol.commandclass.ZWaveThermostatSetpointCommandClass;
+import org.openhab.binding.zwave.internal.protocol.commandclass.ZWaveMultiLevelSensorCommandClass.SensorType;
 import org.openhab.binding.zwave.internal.protocol.commandclass.ZWaveThermostatSetpointCommandClass.SetpointType;
 import org.openhab.binding.zwave.internal.protocol.commandclass.ZWaveThermostatSetpointCommandClass.ZWaveThermostatSetpointValueEvent;
 import org.openhab.binding.zwave.internal.protocol.event.ZWaveCommandClassValueEvent;
@@ -34,6 +35,7 @@ import org.slf4j.LoggerFactory;
  * {@link ZWaveThermostatSetpointCommandClass}. Implements polling of the setpoint
  * status and receiving of setpoint events.
  * @author Matthew Bowman
+ * @author Dave Hock
  * @since 1.4.0
  */
 public class ZWaveThermostatSetpointConverter extends
@@ -87,6 +89,7 @@ public class ZWaveThermostatSetpointConverter extends
 			Map<String, String> arguments) {
 		ZWaveStateConverter<?,?> converter = this.getStateConverter(item, event.getValue());
 		String setpointType = arguments.get("setpoint_type");
+		String scale = arguments.get("setpoint_scale");
 		ZWaveThermostatSetpointValueEvent setpointEvent = (ZWaveThermostatSetpointValueEvent)event;
 		
 		if (converter == null) {
@@ -98,7 +101,23 @@ public class ZWaveThermostatSetpointConverter extends
 		if (setpointType != null && SetpointType.getSetpointType(Integer.parseInt(setpointType)) != setpointEvent.getSetpointType())
 			return;
 		
-		State state = converter.convertFromValueToState(event.getValue());
+		Object val = event.getValue();
+		// Perform a scale conversion if needed
+		if (scale != null && Integer.parseInt(scale) != setpointEvent.getScale()) {
+			// For temperature, there are only two scales, so we simplify the conversion
+			if(setpointEvent.getScale() == 0) {
+				// Scale is celsius, convert to fahrenheit
+				double c = ((BigDecimal)val).doubleValue();
+				val = new BigDecimal((c * 9.0 / 5.0) + 32.0 );
+			}
+			else if(setpointEvent.getScale() == 1) {
+				// Scale is fahrenheit, convert to celsius
+				double f = ((BigDecimal)val).doubleValue();
+				val = new BigDecimal((f - 32.0) * 5.0 / 9.0 );					
+			}
+		}
+
+		State state = converter.convertFromValueToState(val);
 		this.getEventPublisher().postUpdate(item.getName(), state);
 		
 	}
@@ -111,7 +130,11 @@ public class ZWaveThermostatSetpointConverter extends
 			ZWaveThermostatSetpointCommandClass commandClass, int endpointId,
 			Map<String, String> arguments) {
 		ZWaveCommandConverter<?,?> converter = this.getCommandConverter(command.getClass());
+		String scaleString = arguments.get("setpoint_scale");
 		String setpointType = arguments.get("setpoint_type");
+		
+		int scale = 0;
+		if (scaleString !=null ) scale= Integer.parseInt(scaleString);
 
 		if (converter == null) {
 			logger.warn("No converter found for item = {}, node = {} endpoint = {}, ignoring command.", item.getName(), node.getNodeId(), endpointId);
@@ -121,9 +144,9 @@ public class ZWaveThermostatSetpointConverter extends
 		SerialMessage serialMessage;
 		
 		if (setpointType != null) {
-			serialMessage = node.encapsulate(commandClass.setMessage(SetpointType.getSetpointType(Integer.parseInt(setpointType)),(BigDecimal)converter.convertFromCommandToValue(item, command)), commandClass, endpointId);
+			serialMessage = node.encapsulate(commandClass.setMessage(scale, SetpointType.getSetpointType(Integer.parseInt(setpointType)),(BigDecimal)converter.convertFromCommandToValue(item, command)), commandClass, endpointId);
 		} else {
-			serialMessage = node.encapsulate(commandClass.setMessage((BigDecimal)converter.convertFromCommandToValue(item, command)), commandClass, endpointId);
+			serialMessage = node.encapsulate(commandClass.setMessage(scale, (BigDecimal)converter.convertFromCommandToValue(item, command)), commandClass, endpointId);
 		}
 		
 		if (serialMessage == null) {
