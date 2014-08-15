5c02021873934f5d48cb8d1a804622bc6f0d93b6
Tom De Vlaminck <tom_de_vlaminck@hotmail.com>
2014-08-16 00:01:12 +0200

    Added EPL licence for code of Flavio

--- bundles/binding/org.openhab.binding.bticino/src/main/java/be/devlaminck/openwebnet/MonitorSessionThread.java
+++ bundles/binding/org.openhab.binding.bticino/src/main/java/be/devlaminck/openwebnet/MonitorSessionThread.java
@@ -3,6 +3,34 @@
  * Based on code from Mauro Cicolella (as part of the FREEDOMOTIC framework)
  * (https://github.com/freedomotic/freedomotic/tree/master/plugins/devices/openwebnet)
  * and on code of Flavio Fcrisciani (https://github.com/fcrisciani/java-myhome-library)
+ * released under EPL
+ *  
+ * openHAB, the open Home Automation Bus.
+ * Copyright (C) 2010-2012, openHAB.org <admin@openhab.org>
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ * Additional permission under GNU GPL version 3 section 7
+ *
+ * If you modify this Program, or any covered work, by linking or
+ * combining it with Eclipse (or a modified version of that library),
+ * containing parts covered by the terms of the Eclipse Public License
+ * (EPL), the licensors of this Program grant you additional permission
+ * to convey the resulting work.
  * 
  * @author Tom De Vlaminck
  * @serial 1.0
