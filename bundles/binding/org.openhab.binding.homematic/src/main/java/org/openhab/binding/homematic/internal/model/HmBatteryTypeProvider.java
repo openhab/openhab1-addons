6260c124f48f996410b6c48cbba87f9c53bcb16d
Gerhard Riegler <gerrieg.openhab@icloud.com>
2014-08-31 18:13:34 +0200

    Fixed Winmatic battery type.

--- bundles/binding/org.openhab.binding.homematic/src/main/java/org/openhab/binding/homematic/internal/model/HmBatteryTypeProvider.java
+++ bundles/binding/org.openhab.binding.homematic/src/main/java/org/openhab/binding/homematic/internal/model/HmBatteryTypeProvider.java
@@ -80,7 +80,7 @@ public class HmBatteryTypeProvider {
 		batteries.put("HM-WDS30-TO", new HmBattery(HmBatteryType.AAA, 2));
 		batteries.put("HM-SEC-SCO", new HmBattery(HmBatteryType.AAA, 1));
 
-		batteries.put("HM-Sec-Win", new HmBattery(HmBatteryType.WINMATIC_ACCU, 1));
+		batteries.put("HM-SEC-WIN", new HmBattery(HmBatteryType.WINMATIC_ACCU, 1));
 	}
 
 	/**
