ee5a1eb21ed07441dccd5537d46851f31d96b73a
Neil <neil.renaud@gmail.com>
2014-09-03 13:54:40 +0100

    Changing to polling binding

--- bundles/binding/org.openhab.binding.lightwaverf/src/main/java/org/openhab/binding/lightwaverf/internal/LightwaveRFBinding.java
+++ bundles/binding/org.openhab.binding.lightwaverf/src/main/java/org/openhab/binding/lightwaverf/internal/LightwaveRFBinding.java
@@ -9,7 +9,7 @@
 package org.openhab.binding.lightwaverf.internal;
 
 import org.openhab.binding.lightwaverf.LightwaveRFBindingProvider;
-import org.openhab.core.binding.AbstractBinding;
+import org.openhab.core.binding.AbstractActiveBinding;
 import org.openhab.core.types.Command;
 import org.openhab.core.types.State;
 import org.slf4j.Logger;
@@ -20,33 +20,30 @@ import org.slf4j.LoggerFactory;
  * @author Neil Renaud
  * @since 1.6
  */
-public class LightwaveRFBinding extends AbstractBinding<LightwaveRFBindingProvider> {
+public class LightwaveRFBinding extends AbstractActiveBinding<LightwaveRFBindingProvider> {
 
-	private static final Logger logger = LoggerFactory.getLogger(LightwaveRFBinding.class);
+	private final Logger logger = LoggerFactory.getLogger(LightwaveRFBinding.class);
+	private static final long POLLING_INTERVAL = 100; //100ms
+	private static final String THREAD_NAME = "LightwaveRF Receiver Thread";
 	private LightwaveRFReceiver receiver = null;
 	private LightwaveRFSender sender = null;
-	
-	public LightwaveRFBinding() {
-	}
 
-
+	@Override
 	public void activate() {
-		receiver = new LightwaveRFReceiver();
-		receiver.start();
-		sender = new LightwaveRFSender();
-		sender.start();
+//		receiver = new LightwaveRFReceiver();
+//		receiver.start();
+//		sender = new LightwaveRFSender();
+//		sender.start();
 	}
 
+	@Override
 	public void deactivate() {
-		receiver.stop();
-		receiver = null;
-		sender.stop();
-		sender = null;
+//		receiver.stop();
+//		receiver = null;
+//		sender.stop();
+//		sender = null;
 	}
 
-	/**
-	 * @{inheritDoc}
-	 */
 	@Override
 	protected void internalReceiveCommand(String itemName, Command command) {
 		// the code being executed when a command was sent on the openHAB
@@ -55,14 +52,26 @@ public class LightwaveRFBinding extends AbstractBinding<LightwaveRFBindingProvid
 		logger.debug("internalReceiveCommand() is called!");
 	}
 
-	/**
-	 * @{inheritDoc}
-	 */
 	@Override
 	protected void internalReceiveUpdate(String itemName, State newState) {
 		// the code being executed when a state was sent on the openHAB
 		// event bus goes here. This method is only called if one of the 
 		// BindingProviders provide a binding for the given 'itemName'.
-		logger.debug("internalReceiveCommand() is called!");
+		logger.debug("internalReceiveUpdate() is called!");
+	}
+
+	@Override
+	protected void execute(){
+		logger.debug("Execute called");
+	}
+
+	@Override
+	protected long getRefreshInterval(){
+		return POLLING_INTERVAL;
+	}
+
+	@Override
+	protected String getName(){
+		return THREAD_NAME;
 	}
 }
