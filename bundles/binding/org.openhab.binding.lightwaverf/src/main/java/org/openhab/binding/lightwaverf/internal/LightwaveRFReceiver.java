e76b07b8e0ec20754fc6c7879ef3993523a202ba
Neil <neil.renaud@gmail.com>
2014-09-04 17:26:59 +0100

    Updated the Sender and receiver to work correctly and deleted connecter that isn't needed

--- bundles/binding/org.openhab.binding.lightwaverf/src/main/java/org/openhab/binding/lightwaverf/internal/LightwaveRFReceiver.java
+++ bundles/binding/org.openhab.binding.lightwaverf/src/main/java/org/openhab/binding/lightwaverf/internal/LightwaveRFReceiver.java
@@ -1,89 +1,112 @@
 package org.openhab.binding.lightwaverf.internal;
-
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.util.concurrent.CopyOnWriteArrayList;
-import java.io.IOException;
-
+import java.util.concurrent.CountDownLatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class LightwaveRFReceiver implements Runnable {
-	private static Logger logger = LoggerFactory.getLogger(LightwaveRFReceiver.class);
-	private static final int PORT = 9760;
-	
-	private final CopyOnWriteArrayList<LightwaveRFMessageListener> listeners = new CopyOnWriteArrayList<LightwaveRFMessageListener>();
-
-	private boolean running = true;
-	private DatagramSocket receiveSocket;
-
- 	public LightwaveRFReceiver(){}
-
- 	public synchronized void stop(){
-		running = false;
-		receiveSocket.close();
-	}
- 	
- 	public synchronized void start(){
- 		running = true;
- 		initialiseSockets();
- 		new Thread(this).start();
- 	}
-
-   	/**
-   	 * Initialise receive sockets for UDP
-   	 */
-	private void initialiseSockets(){
-		try{
-			receiveSocket = new DatagramSocket(PORT);
-   		} catch (IOException e){
-			logger.error("Error initalising socket", e);
-		}
-	}
- 	
-	/**
-	 * Run method, this will listen to the socket and receive messages
-	 */
-	@Override
-	public void run() {
-		logger.info("Starting LightwaveRFReceiver");
-		while(running) {
-			String message = receiveUDP();
-			logger.debug("Message received: " + message);
-			notifyListeners(message);
-		}
-		logger.info("Stopping LightwaveRFReceiver");
- 	}
-
- 	//Network sockets to deal with receiving UDP responses from LightwaveRF box.  UDP response is specific to LightwaveRF
-	public String receiveUDP(){
- 		String receivedMessage = "";
-		try {
-			byte[] receiveData = new byte[1024];
-			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-			receiveSocket.receive(receivePacket);
-			receivedMessage = new String(receivePacket.getData(), 0, receivePacket.getLength());
- 		}
-		catch (IOException e) {
-			if(!(running == false && receiveSocket.isClosed())){
-				// If running isn't false and the socket isn't closed log the error
-				logger.error("Error receiving message", e);
-			}
-		}
-		return receivedMessage;
-	}
-	
-	public void addListener(LightwaveRFMessageListener listener){
-		listeners.add(listener);
-	}
-	
-	public void removeListener(LightwaveRFMessageListener listener){
-		listeners.remove(listener);
-	}
-	
-	private void notifyListeners(String message){
-		for(LightwaveRFMessageListener listener : listeners){
-			listener.messageRecevied(message);
-		}
-	}
+    private static Logger logger = LoggerFactory.getLogger(LightwaveRFReceiver.class);
+    private static final int PORT = 9760;
+    private final CopyOnWriteArrayList<LightwaveRFMessageListener> listerns = new CopyOnWriteArrayList<LightwaveRFMessageListener>();
+    private CountDownLatch latch = new CountDownLatch(0);
+    private boolean running = true;
+    private DatagramSocket receiveSocket;
+    public LightwaveRFReceiver() {}
+    /**
+     * Start the LightwaveRFReceiver
+     * Will set running true, initialise the socket and start the thread.
+     */
+    public synchronized void start() {
+        logger.info("Starting LightwaveRFReceiver");
+        running = true;
+        latch = new CountDownLatch(1);
+        initialiseSockets();
+        new Thread(this).start();
+    }
+    /**
+    * Stop the LightwaveRFSender
+    * Will closae the sockect wait for the thread to exit and null the socket
+    */
+    public synchronized void stop() {
+        logger.info("Stopping LightwaveRFReceiver");
+        running = false;
+        receiveSocket.close();
+        try {
+            latch.await();
+        } 		catch(InterruptedException e) {
+            logger.error("Error waiting for shutdown to complete", e);
+        }
+        receiveSocket = null;
+        logger.info("LightwaveRFReceiver Stopped");
+    }
+    /**
+    * Initialise receive sockets for UDP
+    */
+    private void initialiseSockets() {
+        try {
+            receiveSocket = new DatagramSocket(PORT);
+        } catch (IOException e) {
+            logger.error("Error initalising socket", e);
+        }
+    }
+    /**
+    * Run method, this will listen to the socket and receive messages.
+    * The blocking is stopped when the socket is closed.
+    */
+    @Override
+    public void run() {
+        logger.info("LightwaveRFReceiver Started");
+        while(running) {
+            try {
+                String message = receiveUDP();
+                logger.debug("Message received: " + message);
+                notifyListners(message);
+            } 			catch (IOException e) {
+                if(!(running == false && receiveSocket.isClosed())) {
+                    // If running isn't false and the socket isn't closed log the error
+                    logger.error("Error receiving message", e);
+                }
+            }
+        }
+        latch.countDown();
+    }
+    /**
+     	 * Receive the next UDP packet on the socket
+     	 * @return
+    	 * @throws IOException
+     */
+    public String receiveUDP() throws IOException {
+        String receivedMessage = "";
+        byte[] receiveData = new byte[1024];
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        receiveSocket.receive(receivePacket);
+        receivedMessage = new String(receivePacket.getData(), 0, receivePacket.getLength());
+        return receivedMessage;
+    }
+    /**
+    * Add listener to be notified of messages received on the socket
+    * @param listener
+    */
+    public void addListener(LightwaveRFMessageListener listener) {
+        listerns.add(listener);
+    }
+    /**
+     * Remove listener to stop being notified of messages being received on the socket.
+    * @param listener
+     */
+    public void removeListener(LightwaveRFMessageListener listener) {
+        listerns.remove(listener);
+    }
+    /**
+    * Notify all listeners of a message
+    * @param message
+    */
+    private void notifyListners(String message) {
+        for(LightwaveRFMessageListener listener : listerns) {
+            listener.messageRecevied(message);
+        }
+    }
 }
--- bundles/binding/org.openhab.binding.lightwaverf/src/main/java/org/openhab/binding/lightwaverf/internal/LightwaveRFSender.java
+++ bundles/binding/org.openhab.binding.lightwaverf/src/main/java/org/openhab/binding/lightwaverf/internal/LightwaveRFSender.java
@@ -1,119 +1,153 @@
 package org.openhab.binding.lightwaverf.internal;
-
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.LinkedBlockingQueue;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class LightwaveRFSender implements Runnable {
-	private static Logger logger = LoggerFactory.getLogger(LightwaveRFSender.class);
-	private static final int LIGHTWAVE_PORT_IN = 9760; // Port into Lightwave Wifi hub.
-	private static final String BROADCAST_ADDRESS = "255.255.255.255";  // Broadcast UDP address.
-	private static final String STOP_MESSAGE = "StopMessage";
-	private int messageCount = 0;
-	private DatagramSocket transmitSocket; // Socket for UDP transmission to LWRF port 9760
-	private BlockingQueue<String> queue; // Simple queue to queue up UDP transmission that could be from a polling thread, or direct commands through API  	
-	private boolean running = false;
-	
-	public LightwaveRFSender() {
-		queue = new LinkedBlockingQueue<String>();
-	}
+    private static final Logger logger = LoggerFactory.getLogger(LightwaveRFSender.class);
+    private static final String STOP_MESSAGE = "StopMessage";
+    // Poll time so we don't flood the LightwaveRF hub
+    private static final int POLL_TIME = 250;
+    // LightwaveRF WIFI hub port.
+    private static final int LIGHTWAVE_PORT_IN = 9760;
+    // LightwaveRF WIFI hub IP Address or broadcast address
+    private static final String LIGHTWAVE_IP_ADDRESS = "255.255.255.255";
+    // Used to send messages
+    private final InetAddress ipAddress;
+    // Latch used to ensure we shutdown.
+    private CountDownLatch latch = new CountDownLatch(0);
+    // Simple queue of UDP transmission
+    private BlockingQueue<String> queue = new LinkedBlockingQueue<String>();
+    // Boolean to indicate if we are running
+    private boolean running = false;
+    // LightwaveRF messageId
+    private int messageId = 0;
+    private DatagramSocket transmitSocket = null;
 
-	/*
-	 * Initialise transmit sockets for UDP
-	 */
-	public void initialiseSockets(){
-		try{
-			transmitSocket = new DatagramSocket();
-		} catch (IOException e){
-			logger.error("Error initalising socket", e);
-		}
-	}
-
- 	public void start(){
- 		running = true;
-		initialiseSockets();
-		new Thread(this).start();
- 	}
-
-	public void stop() {
-		running = false;
-		addStopMessage();
-	} 	
-	/*
-	 * Run thread, pulling off any items from the UDP commands buffer, then send across network
-	 * (non-Javadoc)
-	 * @see java.lang.Thread#run()
-	 */
-	public void run() {
-		logger.info("LightwaveRF Sender Running");
-		while(running){
-			try{
-				String commandToSend = queue.take();
-				if(commandToSend.equals(STOP_MESSAGE)){
-					netsendUDP(commandToSend);
-				}
-				else{
-					transmitSocket.close();
-				}
-			}
-			catch(InterruptedException e){
-				logger.error("Error waiting on queue", e);
-			}
-		}
-		logger.info("LightwaveRF Sender Stopping");
-	}
+    public LightwaveRFSender() throws UnknownHostException {
+        ipAddress =  InetAddress.getByName(LIGHTWAVE_IP_ADDRESS);
+    }
+    /**
+     * Start the LightwaveRFSender
+     * Will set running to true, initalise the socket and start the sender thread
+     */
+    public synchronized void start() {
+        logger.info("Starting LightwaveRFSender");
+        running = true;
+        latch = new CountDownLatch(1);
+        initialiseSockets();
+        new Thread(this).start();
+    }
 
     /**
- 	 * Add UDP commands to a buffer.
-  	 */
-	public void sendUDP(String command){
-		try{
-			queue.put(command);
-		}
-		catch(InterruptedException e){
-			logger.error("Error adding command[" + command + "] to queue", e);
-		}
-	}
-	
-	private void addStopMessage(){
-		try {
-			queue.put(STOP_MESSAGE);
-		} catch (InterruptedException e) {
-			logger.error("Error stoping LightwaveRFSender", e);
-		}
-	}
+     * Stop the LightwaveRFReseiver
+     * Will set running to false, add a stop message to the queue so that it stops when empty,
+     * close and set the sockect to null
+     */
+    public synchronized void stop() {
+        logger.info("Stopping LightwaveRFSender");
+        running = false;
+        addStopMessage();
+        try {
+            latch.await();
+        } catch(InterruptedException e) {
+            logger.error("Error waiting for shutdown to complete", e);
+        }
+        transmitSocket.close();
+        transmitSocket = null;
+        logger.info("LightwaveRFSender Stopped");
+    }
 
-	/*
-	 * Send the UDP commands from the buffer, waiting a period of time before sending next, so as not to flood UDP socket on LWRF 9760 port
-	 */
-	private void netsendUDP(String command){
-		command = messageCount + command;
-		incrementMessageCount();
-		try {
-			logger.info("Sending command[" + command + "]");
-			byte[] sendData = new byte[1024];
-			sendData = command.getBytes();
-			InetAddress ipAddress =  InetAddress.getByName(BROADCAST_ADDRESS);
-			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, LIGHTWAVE_PORT_IN); //Send broadcast UDP to 9760 port
-			transmitSocket.send(sendPacket);
-		}
-		catch (IOException e) {
-			logger.error("Error sending command[" + command + "]", e);
-		}
-	}
+    /**
+      * Initialise transmit sockets for UDP publishing
+      */
+    private void initialiseSockets() {
+        try {
+            transmitSocket = new DatagramSocket();
+        } catch (IOException e) {
+            logger.error("Error initalising socket", e);
+        }
+    }
 
- 	/**
-	 * Increment message counter, so different messages have different IDs
-	 * Important for getting corresponding OK acknowledgements from port 9761 tagged with the same counter value
-	 */
-	private void incrementMessageCount(){
-		if (messageCount <=998) messageCount++;
-		else messageCount = 000;
-	}
+    /**
+     * Run thread, pulling off any items from the UDP commands buffer, then send across network
+     */
+    @Override
+    public void run() {
+        logger.info("LightwaveRFSender Started");
+        while(running) {
+            try {
+                String commandToSend = queue.take();
+                if(!commandToSend.equals(STOP_MESSAGE)) {
+                    netsendUDP(commandToSend);
+                } else {
+                    logger.info("Stop message received");
+                    break;
+                }
+                Thread.sleep(POLL_TIME);
+            } catch(InterruptedException e) {
+                logger.error("Error waiting on queue", e);
+            }
+        }
+        latch.countDown();
+    }
+
+    /**
+    * Add UDP commands to queue.
+    */
+    public void sendUDP(String command) {
+        try {
+            if(running) {
+                queue.put(command);
+            } else {
+                logger.info("Message not added to queue as we are shutting down Message[" + command + "]");
+            }
+        } catch(InterruptedException e) {
+            logger.error("Error adding command[" + command + "] to queue", e);
+        }
+    }
+
+    /**
+     * Add a stop message so we start shutting down
+     */
+    private void addStopMessage() {
+        try {
+            queue.put(STOP_MESSAGE);
+        } catch (InterruptedException e) {
+            logger.error("Error stoping LightwaveRFSender", e);
+        }
+    }
+
+    /**
+    * Send the UDP commands
+     */
+    private void netsendUDP(String command) {
+        command = messageId + command;
+        incrementMessageId();
+        try {
+            logger.info("Sending command[" + command + "]");
+            byte[] sendData = new byte[1024];
+            sendData = command.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, LIGHTWAVE_PORT_IN);
+            transmitSocket.send(sendPacket);
+        } 		catch (IOException e) {
+            logger.error("Error sending command[" + command + "]", e);
+        }
+    }
+
+    /**
+     * Increment message counter, so different messages have different IDs
+     * Important for getting corresponding OK acknowledgements from port 9761 tagged with the same counter value
+     */
+    private void incrementMessageId() {
+        if (messageId <=998) messageId++;
+        else messageId = 000;
+    }
 }
