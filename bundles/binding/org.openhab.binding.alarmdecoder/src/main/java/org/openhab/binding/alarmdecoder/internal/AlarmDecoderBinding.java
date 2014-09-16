5625c762fd70e1a630ab64da8772c2ef611b16f7
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2014-09-16 15:24:59 -0400

    fixed two bugs: 1) pom.xml was stil referencing the 1-wire binding (cut and paste error) 2) need to parse RF messages numeric value as hexadecimal!!!

--- bundles/binding/org.openhab.binding.alarmdecoder/src/main/java/org/openhab/binding/alarmdecoder/internal/AlarmDecoderBinding.java
+++ bundles/binding/org.openhab.binding.alarmdecoder/src/main/java/org/openhab/binding/alarmdecoder/internal/AlarmDecoderBinding.java
@@ -434,7 +434,7 @@ public class AlarmDecoderBinding extends AbstractActiveBinding<AlarmDecoderBindi
 			throw new MessageParseException("need 2 comma separated fields in msg");
 		}
 		try {
-			int numeric = Integer.parseInt(parts[1]);
+			int numeric = Integer.parseInt(parts[1], 16);
 			ArrayList<AlarmDecoderBindingConfig> bcl = getItems(ADMsgType.RFX, parts[0], null);
 			for (AlarmDecoderBindingConfig c : bcl) {
 				if (c.hasFeature("data")) {
