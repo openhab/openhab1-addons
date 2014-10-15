c5264338e5436dd6d5ed398da14fb9efc3ff3d38
clinique <glhopital@gmail.com>
2014-10-15 08:43:05 +0200

    Code review corrections

--- bundles/binding/org.openhab.binding.netatmo/src/main/java/org/openhab/binding/netatmo/internal/NetatmoMeasureType.java
+++ bundles/binding/org.openhab.binding.netatmo/src/main/java/org/openhab/binding/netatmo/internal/NetatmoMeasureType.java
@@ -1,5 +1,22 @@
+/**
+ * Copyright (c) 2010-2013, openHAB.org and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.openhab.binding.netatmo.internal;
+
 import org.apache.commons.lang.StringUtils;
+
+/**
+ * @author Gaël L'hopital
+ * @since 1.6.0
+ * 
+ * This enum holds all the different measures and states available
+ * to be retrieved by the Netatmo binding
+ */
 public enum NetatmoMeasureType {
 	CO2("co2"),
 	TEMPERATURE("Temperature"),
