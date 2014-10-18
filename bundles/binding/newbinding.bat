d1feca3e357845343b2487d2a2b402c17854d1d5
Lehane Kellett <lehane@mm-wave.com>
2014-10-18 13:08:02 +0100

    Added missing files

---
+++ bundles/binding/newbinding.bat
@@ -0,0 +1,6 @@
+mvn archetype:generate -B -DarchetypeGroupId=org.openhab.archetype 
+-DarchetypeArtifactId=org.openhab.archetype.binding
+-DarchetypeVersion=1.6.0-SNAPSHOT -Dauthor=g8kmh -Dversion=1.6.0-SNAPSHOT
+-DartifactId=org.openhab.binding.plclogo
+-Dpackage=org.openhab.binding.plclogo
+-Dbinding-name=PLCLogo
