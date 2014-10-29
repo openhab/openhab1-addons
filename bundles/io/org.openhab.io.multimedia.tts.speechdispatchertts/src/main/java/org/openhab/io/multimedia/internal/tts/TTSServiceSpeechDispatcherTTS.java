9079cf94bff0fcae66faf3d43d8e0de157951850
sysadmin <glhopital@gmail.com>
2014-10-29 17:03:57 +0100

    Resolved module loading issue Allow configuration of multiple Speech Dispatcher devices

--- bundles/io/org.openhab.io.multimedia.tts.speechdispatchertts/src/main/java/org/openhab/io/multimedia/internal/tts/TTSServiceSpeechDispatcherTTS.java
+++ bundles/io/org.openhab.io.multimedia.tts.speechdispatchertts/src/main/java/org/openhab/io/multimedia/internal/tts/TTSServiceSpeechDispatcherTTS.java
@@ -8,13 +8,19 @@
  */
 package org.openhab.io.multimedia.internal.tts;
 
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.openhab.io.multimedia.tts.TTSService;
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import speechd.ssip.SSIPClient;
-import speechd.ssip.SSIPException;
-import speechd.ssip.SSIPPriority;
 
 /**
  * This is a pure Java TTS service implementation, based on SpeechDispatcherTTS.
@@ -23,45 +29,104 @@ import speechd.ssip.SSIPPriority;
  * @since 1.6.0
  * 
  */
-public class TTSServiceSpeechDispatcherTTS implements TTSService {
+public class TTSServiceSpeechDispatcherTTS implements TTSService, ManagedService {
 
 	private static final Logger logger = LoggerFactory.getLogger(TTSServiceSpeechDispatcherTTS.class);
-	private static SSIPClient speechDispatcherClient;
-
+	
+	SpeechDispatcherConnection defaultOutput = null;
+	
+	/** Map table to store all available speech dispatchers configured by the user */
+	protected Map<String, SpeechDispatcherConnection> deviceConfigCache = null;
+	
+	/** RegEx to validate a config <code>'^(.*?)\\.(host|port)$'</code> */
+	private static final Pattern EXTRACT_CONFIG_PATTERN = Pattern.compile("^(.*?)\\.(host|port)$");
+	
 	public void activate() {		
-		try {
-			speechDispatcherClient = new SSIPClient("openhab", null, null);
-		} catch (SSIPException e) {
-			logger.error("Error connecting to SpeechDispatcher TTS: " + e.getLocalizedMessage(), e);
-		}
+		logger.debug("Activate");
 	}
 
 	public void deactivate() {
-		if (speechDispatcherClient != null) {
-			try {
-				speechDispatcherClient.close();
-				speechDispatcherClient =null;
-			} catch (SSIPException e) {
-				logger.error("Error closing connection to SpeechDispatcher TTS: " + e.getLocalizedMessage(), e);
-			}		
-		}
+		logger.debug("Deactivate");
+		closeAllConnections();
+	}
+
+	private void closeAllConnections() {
+		 for (Entry<String, SpeechDispatcherConnection> deviceConfig : deviceConfigCache.entrySet() ) {
+			 SpeechDispatcherConnection connection = deviceConfig.getValue();
+			 connection.closeConnection();
+		 }
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
 	public void say(String text, String voiceName, String outputDevice) {
-		if (speechDispatcherClient==null) {
-			logger.error("SpeechDispatcher TTS is not available");
-			return;
+		SpeechDispatcherConnection connection = null;
+		if (outputDevice != null) {
+			connection = deviceConfigCache.get(outputDevice);
+		} else {
+			connection = defaultOutput;
 		}
-		if(text==null) {
-			return;
+		
+		if (connection != null) {
+			connection.say(text, voiceName);	
+		} else {
+			logger.error("Output device not configured [{}]", outputDevice);
 		}
-		try {
-			speechDispatcherClient.say(SSIPPriority.MESSAGE, text);
-		} catch (SSIPException e) {
-			logger.error("Error sending text to SpeechDispatcher TTS: " + e.getLocalizedMessage(), e);
+		
+	}
+
+	public void updated(Dictionary<String, ?> properties) throws ConfigurationException {
+
+		if (properties != null) {
+			Enumeration<String> keys = properties.keys();
+			
+			if ( deviceConfigCache == null ) {
+				deviceConfigCache = new HashMap<String, SpeechDispatcherConnection>();
+			}
+			
+			while (keys.hasMoreElements()) {
+				String key = (String) keys.nextElement();
+
+				// the property-key enumeration can contain additional keys that we
+				// don't want to process here ...
+				if ("service.pid".equals(key) || "os".equals(key)) {
+					continue;
+				}
+				
+				Matcher matcher = EXTRACT_CONFIG_PATTERN.matcher(key);
+
+				if (!matcher.matches()) {
+					logger.debug("Given config key '" + key + "' does not follow the expected pattern '<id>.<host|port>'");
+					continue;
+				}
+
+				matcher.reset();
+				matcher.find();
+
+				String deviceId = matcher.group(1);
+
+				SpeechDispatcherConnection deviceConfig = deviceConfigCache.get(deviceId);
+
+				if (deviceConfig == null) {
+					deviceConfig = new SpeechDispatcherConnection();
+					deviceConfigCache.put(deviceId, deviceConfig);
+					defaultOutput = deviceConfig;
+				}
+
+				String configKey = matcher.group(2);
+				String value = (String) properties.get(key);
+
+				if ("host".equals(configKey)) {
+					deviceConfig.host = value;
+				} else if ("port".equals(configKey)) {
+					deviceConfig.port = value;
+				} else {
+					throw new ConfigurationException(configKey, "the given configKey '" + configKey + "' is unknown");
+				}
+			}
 		}
 	}
 }
+
+
