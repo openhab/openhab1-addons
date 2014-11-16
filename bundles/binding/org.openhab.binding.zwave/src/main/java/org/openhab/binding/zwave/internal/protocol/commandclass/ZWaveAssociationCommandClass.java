1a5ce48a3e0929cbcd75ee4c3fd531e0b1dd7b91
Chris Jackson <chris@cd-jackson.com>
2014-11-16 13:47:36 +0000

    Fix association logging bug

--- bundles/binding/org.openhab.binding.zwave/src/main/java/org/openhab/binding/zwave/internal/protocol/commandclass/ZWaveAssociationCommandClass.java
+++ bundles/binding/org.openhab.binding.zwave/src/main/java/org/openhab/binding/zwave/internal/protocol/commandclass/ZWaveAssociationCommandClass.java
@@ -225,7 +225,7 @@ public class ZWaveAssociationCommandClass extends ZWaveCommandClass
 	 */
 	protected void processGroupingsReport(SerialMessage serialMessage, int offset) {
 		maxGroups = serialMessage.getMessagePayloadByte(offset + 1);
-		logger.debug("NODE {}: processGroupingsReport number of groups {}", getNode(), maxGroups);
+		logger.debug("NODE {}: processGroupingsReport number of groups {}", getNode().getNodeId(), maxGroups);
 
 		initialiseDone = true;
 
