6386e20714ecdb0cb0918455b04dd4bceb7c3937
Krzysztof Goworek <krzysztof@connectv.com>
2014-11-27 15:26:05 +0100

    Added ability to force arm and clear alarms. Other minor changes.

--- bundles/binding/org.openhab.binding.satel/src/main/java/org/openhab/binding/satel/internal/types/ZoneState.java
+++ bundles/binding/org.openhab.binding.satel/src/main/java/org/openhab/binding/satel/internal/types/ZoneState.java
@@ -17,7 +17,7 @@ package org.openhab.binding.satel.internal.types;
 public enum ZoneState implements StateType {
 	armed(0x09), really_armed(0x0a), armed_mode_2(0x0b), armed_mode_3(0x0c), first_code_entered(0x0d), entry_time(0x0e), exit_time_gt_10(
 			0x0f), exit_time_lt_10(0x10), temporary_blocked(0x11), blocked_for_guard(0x12), alarm(0x13), fire_alarm(
-			0x14), alarm_memory(0x15), fire_alarm_memory(0x16), violated_zones(0x25), verified_alarms(0x27), armed_mode_1(
+			0x14), alarm_memory(0x15), fire_alarm_memory(0x16), violated_inputs(0x25), verified_alarms(0x27), armed_mode_1(
 			0x2a), warning_alarms(0x2b);
 
 	private byte refreshCommand;
