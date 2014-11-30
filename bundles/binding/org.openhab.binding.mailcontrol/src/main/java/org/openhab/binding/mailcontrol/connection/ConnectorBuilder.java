af43e708b96c987e2f0b6402a42decf63558dbf8
Andrey Pereverzin <bertoluCCi>
2014-11-30 21:02:46 +0000

    Add license headers

--- bundles/binding/org.openhab.binding.mailcontrol/src/main/java/org/openhab/binding/mailcontrol/connection/ConnectorBuilder.java
+++ bundles/binding/org.openhab.binding.mailcontrol/src/main/java/org/openhab/binding/mailcontrol/connection/ConnectorBuilder.java
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2010-2013, openHAB.org and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.openhab.binding.mailcontrol.connection;
 
 import static org.creek.accessemail.connector.mail.MailPropertiesStorage.MAIL_USERNAME_PROPERTY;
