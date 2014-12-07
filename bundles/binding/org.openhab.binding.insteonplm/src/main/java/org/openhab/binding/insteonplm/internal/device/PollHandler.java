7b676cbad16ca12a1ad573defb6a20a3299ad6a1
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2014-12-06 20:22:24 -0500

    InsteonPLM binding: added parameters to command and message handlers to implement support for multi-button keypads like 2487S.

--- bundles/binding/org.openhab.binding.insteonplm/src/main/java/org/openhab/binding/insteonplm/internal/device/PollHandler.java
+++ bundles/binding/org.openhab.binding.insteonplm/src/main/java/org/openhab/binding/insteonplm/internal/device/PollHandler.java
@@ -9,6 +9,8 @@
 package org.openhab.binding.insteonplm.internal.device;
 
 import java.io.IOException;
+import java.util.HashMap;
+
 import org.openhab.binding.insteonplm.internal.message.FieldException;
 import org.openhab.binding.insteonplm.internal.message.Msg;
 import org.slf4j.Logger;
@@ -23,6 +25,7 @@ import org.slf4j.LoggerFactory;
 public abstract class PollHandler {
 	private static final Logger logger = LoggerFactory.getLogger(PollHandler.class);		
 	DeviceFeature m_feature = null;
+	HashMap<String, String> m_parameters = new HashMap<String, String>();
 	/**
 	 * Constructor
 	 * @param feature The device feature being polled
@@ -38,6 +41,8 @@ public abstract class PollHandler {
 	 */
 	public abstract Msg makeMsg(InsteonDevice device);
 
+	public void setParameters(HashMap<String, String> hm) { m_parameters = hm; }
+	
 	public static class DefaultPollHandler extends PollHandler {
 		DefaultPollHandler(DeviceFeature f) { super(f); }
 		@Override
@@ -54,9 +59,15 @@ public abstract class PollHandler {
 			return m;
 		}
 	}
-
-	public static class ContactPollHandler extends PollHandler {
-		ContactPollHandler(DeviceFeature f) { super(f); }
+	/**
+	 * Handler for e.g. the KeypadLinc devices which have multiple
+	 * LED buttons. By sending cmd1 = 0x19, cmd2 = 0x01, the status
+	 * of the individual LED buttons will be returned in the response 
+	 * @author Bernd Pfrommer
+	 *
+	 */
+	public static class LEDBitPollHandler extends PollHandler {
+		LEDBitPollHandler(DeviceFeature f) { super(f); }
 		@Override
 		public Msg makeMsg(InsteonDevice d) {
 			Msg m = null;
@@ -83,19 +94,21 @@ public abstract class PollHandler {
 	}
 	/**
 	 * Factory method for creating handlers of a given name using java reflection
-	 * @param name the name of the handler to create
+	 * @param m_pollHandler the name of the handler to create
 	 * @param f the feature for which to create the handler
 	 * @return the handler which was created
 	 */
-	public static <T extends PollHandler> T s_makeHandler(String name, DeviceFeature f) {
-		String cname = PollHandler.class.getName() + "$" + name;
+	public static <T extends PollHandler> T s_makeHandler(HandlerEntry ph, DeviceFeature f) {
+		String cname = PollHandler.class.getName() + "$" + ph.getName();
 		try {
 			Class<?> c = Class.forName(cname);
 			@SuppressWarnings("unchecked")
 			Class<? extends T> dc = (Class <? extends T>) c;
-			return dc.getDeclaredConstructor(DeviceFeature.class).newInstance(f);
+			T phc = dc.getDeclaredConstructor(DeviceFeature.class).newInstance(f);
+			phc.setParameters(ph.getParams());
+			return phc;
 		} catch (Exception e) {
-			logger.error("error trying to create message handler: {}", name, e);
+			logger.error("error trying to create message handler: {}", ph.getName(), e);
 		}
 		return null;
 	}
