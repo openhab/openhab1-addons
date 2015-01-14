63766a147c67d651411b37ac859756a5af997322
Jürgen Richtsfeld <juergen.richtsfeld@gmail.com>
2015-01-14 21:20:26 +0100

    add javadoc to ConditionalAction

--- bundles/action/org.openhab.action.conditionalaction/src/main/java/org/openhab/action/conditionalaction/internal/ConditionalAction.java
+++ bundles/action/org.openhab.action.conditionalaction/src/main/java/org/openhab/action/conditionalaction/internal/ConditionalAction.java
@@ -28,6 +28,11 @@ import org.slf4j.LoggerFactory;
 
 /**
  * This class contains the methods that are made available in scripts and rules for ConditionalAction.
+ * <p>
+ * The purpose of this is to have a easy way to prevent commands being sent to an item sill allowing clean scripts.
+ * <p>
+ * The idea (especially with the support for using a group item as a disabledItem) is to have several reasons
+ * for disabling some automation (e.g. disable shutter movement by manual disable, too much wind, or window contacts).
  * 
  * @author Juergen Richtsfeld
  * @since 1.7.0
@@ -37,6 +42,16 @@ public class ConditionalAction {
 	private static final String DISABLED_POSTFIX = "_Disabled";
 	private static final Logger logger = LoggerFactory.getLogger(ConditionalAction.class);
 
+	/**
+	 * Same as {@link #sendConditionalCommand(Item, Item, String)} but the condition item is resolved via a name 
+	 * convention (same as the action item name postfixed by &quot;_Disabled&quot;). 
+	 * 
+	 * @param actionItem the item that should receive a command
+	 * @param commandString the command to send to the item
+	 * @return if the command was sent
+	 * 
+	 * @see #sendConditionalCommand(Item, Item, String)
+	 */
 	@ActionDoc(text="A method that sends a command the the actionItem if a item with the same name + _Disabled is OFF", 
 			returns="<code>true</code>, if the command was sent to the actionItem and <code>false</code> otherwise.")
 	public static boolean sendConditionalCommand(
@@ -57,6 +72,16 @@ public class ConditionalAction {
 		return false;
 	}
 	
+	/**
+	 * Sends the given command to the given actionItem only if the aDisabledItem argument is disabled. The other way
+	 * round, if the aDisabledItem is enabled, the command is not sent to the actionItem.<br />
+	 * If the aDisabledItem is a group item, any of the members is enough to be enabled to prevent sending the command.
+	 * 
+	 * @param actionItem the item that receives the command if the disabledItem is disabled
+	 * @param aDisabledItem the item that prevents sending the command if enabled
+	 * @param commandString the command to be sent.
+	 * @return if the command was sent to the actionItem.
+	 */
 	@ActionDoc(text="A method that sends a command to the actionItem if the disabledItem is OFF", 
 			returns="<code>true</code>, if the command was sent to the actionItem and <code>false</code> otherwise.")
 	public static boolean sendConditionalCommand(
