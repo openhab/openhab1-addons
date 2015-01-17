c2aa4ba132fefef3e66d6ba6b8e7cc41a5e368f0
Gerhard Riegler <gerrieg.openhab@icloud.com>
2015-01-17 09:16:22 +0100

    Initial contribution of Astro-Action.

---
+++ bundles/action/org.openhab.action.astro/src/main/java/org/openhab/action/astro/internal/AstroActionService.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2010-2015, openHAB.org and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.action.astro.internal;
+
+import java.util.Dictionary;
+
+import org.openhab.core.scriptengine.action.ActionService;
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedService;
+
+/**
+ * This class registers an OSGi service for the Astro action.
+ * 
+ * @author Gerhard Riegler
+ * @since 1.7.0
+ */
+public class AstroActionService implements ActionService, ManagedService {
+
+	public void activate() {
+	}
+
+	public void deactivate() {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String getActionClassName() {
+		return Astro.class.getCanonicalName();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Class<?> getActionClass() {
+		return Astro.class;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void updated(Dictionary<String, ?> properties) throws ConfigurationException {
+	}
+
+}
---
+++ bundles/action/org.openhab.action.astro/src/main/java/org/openhab/action/astro/internal/AstroConfig.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2010-2015, openHAB.org and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.action.astro.internal;
+
+import java.util.Date;
+
+/**
+ * Holds the rule method parameters.
+ * 
+ * @author Gerhard Riegler
+ * @since 1.7.0
+ */
+public class AstroConfig {
+	private Date date;
+	private double latitude;
+	private double longitude;
+
+	/**
+	 * Creates a new AstroConfig.
+	 */
+	public AstroConfig(Date date, double latitude, double longitude) {
+		this.date = date;
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+
+	/**
+	 * Returns the date to calculate astro data.
+	 */
+	public Date getDate() {
+		return date;
+	}
+
+	/**
+	 * The latitude to calculate astro data.
+	 */
+	public double getLatitude() {
+		return latitude;
+	}
+
+	/**
+	 * The longitude to calculate astro data.
+	 */
+	public double getLongitude() {
+		return longitude;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null || !(obj instanceof AstroConfig)) {
+			return false;
+		}
+		AstroConfig config = (AstroConfig) obj;
+		return date.getTime() == config.getDate().getTime() && latitude == config.getLatitude()
+				&& longitude == config.getLongitude();
+	}
+}
