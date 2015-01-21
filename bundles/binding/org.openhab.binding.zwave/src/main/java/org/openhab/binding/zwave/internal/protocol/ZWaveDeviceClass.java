7619937211edd147253f0b05e828209fbee34b72
Chris Jackson <chris@cd-jackson.com>
2015-01-21 20:45:35 +0000

    Update garage door device class to door sensor

--- bundles/binding/org.openhab.binding.zwave/src/main/java/org/openhab/binding/zwave/internal/protocol/ZWaveDeviceClass.java
+++ bundles/binding/org.openhab.binding.zwave/src/main/java/org/openhab/binding/zwave/internal/protocol/ZWaveDeviceClass.java
@@ -226,7 +226,7 @@ public class ZWaveDeviceClass {
 		STATIC_CONTOLLER(0x02, "Static Controller"), 
 		AV_CONTROL_POINT(0x03, "A/V Control Point"), 
 		DISPLAY(0x06, "Display"),
-		GARAGE_DOOR(0x07, "Garage Door"),
+		DOOR_SENSOR(0x07, "Door Sensor"),
 		THERMOSTAT(0x08, "Thermostat"), 
 		WINDOW_COVERING(0x09, "Window Covering"), 
 		REPEATER_SLAVE(0x0f, "Repeater Slave"), 
@@ -307,7 +307,7 @@ public class ZWaveDeviceClass {
 					return new CommandClass[0];
 				case REMOTE_CONTROLLER:
 				case STATIC_CONTOLLER:
-				case GARAGE_DOOR:
+				case DOOR_SENSOR:
 				case REPEATER_SLAVE:
 				case TOGGLE_SWITCH:
 				case REMOTE_SWITCH:
@@ -416,6 +416,8 @@ public class ZWaveDeviceClass {
 		ROUTING_SENSOR_MULTILEVEL(1, Generic.MULTILEVEL_SENSOR, "Routing Multilevel Sensor"),
 
 		SIMPLE_METER(1, Generic.METER, "Simple Meter"),
+		
+		DOOR_SENSOR(1, Generic.DOOR_SENSOR, "Door Sensor"),
 
 		DOOR_LOCK(1, Generic.ENTRY_CONTROL, "Door Lock"),
 		ADVANCED_DOOR_LOCK(2, Generic.ENTRY_CONTROL, "Advanced Door Lock"),
