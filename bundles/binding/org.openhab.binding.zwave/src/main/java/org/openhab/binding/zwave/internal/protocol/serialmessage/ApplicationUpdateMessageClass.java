2ace2bc5160a7c6a48815ab27e4cb5337a50ca5a
Chris Jackson <chris@cd-jackson.com>
2015-01-31 21:57:43 +0000

    Move UpdateState class into Application Update Handler

--- bundles/binding/org.openhab.binding.zwave/src/main/java/org/openhab/binding/zwave/internal/protocol/serialmessage/ApplicationUpdateMessageClass.java
+++ bundles/binding/org.openhab.binding.zwave/src/main/java/org/openhab/binding/zwave/internal/protocol/serialmessage/ApplicationUpdateMessageClass.java
@@ -8,11 +8,12 @@
  */
 package org.openhab.binding.zwave.internal.protocol.serialmessage;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.openhab.binding.zwave.internal.protocol.SerialMessage;
-import org.openhab.binding.zwave.internal.protocol.UpdateState;
 import org.openhab.binding.zwave.internal.protocol.ZWaveController;
 import org.openhab.binding.zwave.internal.protocol.ZWaveNode;
-import org.openhab.binding.zwave.internal.protocol.ZWaveNodeState;
 import org.openhab.binding.zwave.internal.protocol.SerialMessage.SerialMessageClass;
 import org.openhab.binding.zwave.internal.protocol.commandclass.ZWaveCommandClass;
 import org.openhab.binding.zwave.internal.protocol.commandclass.ZWaveWakeUpCommandClass;
@@ -62,7 +63,7 @@ public class ApplicationUpdateMessageClass  extends ZWaveCommandProcessor {
 			// Remember that we've received this so we can continue initialisation
 			node.setApplicationUpdateReceived(true);
 
-			// If we're finished initialisation, then we can treat this a little like a HAIL
+			// If we're finished initialisation, then we can treat this like a HAIL
 			if(node.getNodeInitializationStage() == ZWaveNodeInitStage.DONE) {
 				// If this node supports associations, then assume this should be handled through that mechanism
 				if(node.getCommandClass(CommandClass.ASSOCIATION) == null) {
@@ -127,5 +128,69 @@ public class ApplicationUpdateMessageClass  extends ZWaveCommandProcessor {
 		checkTransactionComplete(lastSentMessage, incomingMessage);
 
 		return result;
+	}
+	
+	/**
+	 * Update state enumeration. Indicates the type of application update state that was sent.
+	 * @author Jan-Willem Spuij
+	 * @ since 1.3.0
+	 */
+	public enum UpdateState {
+		NODE_INFO_RECEIVED(0x84, "Node info received"),
+		NODE_INFO_REQ_DONE(0x82, "Node info request done"),
+		NODE_INFO_REQ_FAILED(0x81, "Node info request failed"),
+		ROUTING_PENDING(0x80, "Routing pending"),
+		NEW_ID_ASSIGNED(0x40, "New ID Assigned"),
+		DELETE_DONE(0x20, "Delete done"),
+		SUC_ID(0x10, "SUC ID");
+		
+		/**
+		 * A mapping between the integer code and its corresponding update state
+		 * class to facilitate lookup by code.
+		 */
+		private static Map<Integer, UpdateState> codeToUpdateStateMapping;
+
+		private int key;
+		private String label;
+
+		private UpdateState(int key, String label) {
+			this.key = key;
+			this.label = label;
+		}
+
+		private static void initMapping() {
+			codeToUpdateStateMapping = new HashMap<Integer, UpdateState>();
+			for (UpdateState s : values()) {
+				codeToUpdateStateMapping.put(s.key, s);
+			}
+		}
+
+		/**
+		 * Lookup function based on the update state code.
+		 * Returns null when there is no update state with code i.
+		 * @param i the code to lookup
+		 * @return enumeration value of the update state.
+		 */
+		public static UpdateState getUpdateState(int i) {
+			if (codeToUpdateStateMapping == null) {
+				initMapping();
+			}
+			
+			return codeToUpdateStateMapping.get(i);
+		}
+
+		/**
+		 * @return the key
+		 */
+		public int getKey() {
+			return key;
+		}
+
+		/**
+		 * @return the label
+		 */
+		public String getLabel() {
+			return label;
+		}
 	}
 }
