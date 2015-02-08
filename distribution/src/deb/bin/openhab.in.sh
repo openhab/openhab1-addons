340ce1a5fc51478ec1f7ca68c45bd890ce98b210
Theo Weiss <theo.weiss@gmail.com>
2015-02-08 18:01:14 +0100

    deb files, persistence rrd4j and db4o fix permission problems

--- distribution/src/deb/bin/openhab.in.sh
+++ distribution/src/deb/bin/openhab.in.sh
@@ -5,6 +5,7 @@ OPENHAB_CONFIGURATIONS_DIR="${OPENHAB_CONF_DIR}/configurations"
 OPENHAB_DIR="/usr/share/openhab"
 OPENHAB_LOG_DIR="/var/log/openhab"
 OPENHAB_HOME_DIR="/var/lib/openhab"
+OPENHAB_USER_DATA_DIR="${OPENHAB_HOME_DIR}"
 OPENHAB_WORKSPACE_DIR="${OPENHAB_HOME_DIR}/workspace"
 JETTY_CONFDIR="${OPENHAB_CONF_DIR}/jetty/etc"
 if [ -z "$USER_AND_GROUP" ]; then
