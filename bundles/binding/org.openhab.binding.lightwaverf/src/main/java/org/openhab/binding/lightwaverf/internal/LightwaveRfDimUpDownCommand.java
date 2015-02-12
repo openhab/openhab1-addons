beb525ff1e3a6004705630d9115e17fc18fdc3d6
foxy82 <neil.renaud@gmail.com>
2015-02-12 13:24:44 +0000

    Update LightwaveRfDimUpDownCommand.java

--- bundles/binding/org.openhab.binding.lightwaverf/src/main/java/org/openhab/binding/lightwaverf/internal/LightwaveRfDimUpDownCommand.java
+++ bundles/binding/org.openhab.binding.lightwaverf/src/main/java/org/openhab/binding/lightwaverf/internal/LightwaveRfDimUpDownCommand.java
@@ -2,26 +2,23 @@ package org.openhab.binding.lightwaverf.internal;
 
 public class LightwaveRfDimUpDownCommand implements LightwaveRFCommand {
 
-    private final String roomId;
-    private final String deviceId;
-    private final boolean up;
-    
 	/**
 	 * Commands could be but this currently doesnt work: 
 	 *     100,!R2D3F> (Dim Up)
 	 *     101,!R2D3F< (Dim down)
 	 */ 
     
+    public LightwaveRfDimUpDownCommand(String message){
+    	throw new IllegalArgumentException("Not implemented");
+    }
+    
     public LightwaveRfDimUpDownCommand(String roomId, String deviceId, boolean up) {
-        this.roomId = roomId;
-        this.deviceId = deviceId;
-        this.up = up;
+    	throw new IllegalArgumentException("Not implemented");
     }
 
     @Override
     public String getLightwaveRfCommandString() {
-        // TODO Auto-generated method stub
-        return null;
+    	throw new IllegalArgumentException("Not implemented");
     }
 
 }
