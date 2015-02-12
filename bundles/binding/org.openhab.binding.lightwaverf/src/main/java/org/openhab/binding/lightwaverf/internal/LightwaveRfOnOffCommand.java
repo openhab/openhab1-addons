dbbc0550ddda9d30d3b1ad29997e7a7a1a39420c
foxy82 <neil.renaud@gmail.com>
2015-02-12 13:07:39 +0000

    Update LightwaveRfOnOffCommand.java

--- bundles/binding/org.openhab.binding.lightwaverf/src/main/java/org/openhab/binding/lightwaverf/internal/LightwaveRfOnOffCommand.java
+++ bundles/binding/org.openhab.binding.lightwaverf/src/main/java/org/openhab/binding/lightwaverf/internal/LightwaveRfOnOffCommand.java
@@ -1,7 +1,20 @@
-package com.Lightwave;
+package org.openhab.binding.lightwaverf.internal;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LightwaveRfOnOffCommand implements LightwaveRFCommand {
-    
+
+	/**
+	 * Commands are like: 
+	 *     100,!R2D3F0 (Off)
+	 *     101,!R2D3F1 (On)
+	 */
+	
+	private static final Pattern REG_EXP = Pattern.compile("([0-9]{1,3}),!R([0-9])D([0-9])F([0,1])");
+	private static final String ON_FUNCTION = "1";
+	private static final String OFF_FUNCTION = "0";
+	
     private final String roomId;
     private final String deviceId;
     private final boolean on;
@@ -12,7 +25,23 @@ public class LightwaveRfOnOffCommand implements LightwaveRFCommand {
         this.on = on;
     }
     
-    @Override
+    public LightwaveRfOnOffCommand(String message) {
+    	Matcher matcher = REG_EXP.matcher(message);
+    	roomId = matcher.group(1);
+    	deviceId = matcher.group(2);
+    	String function = matcher.group(3);
+    	if(ON_FUNCTION.equals(function)){
+    		on = true;
+    	}
+    	else if(OFF_FUNCTION.equals(function)){
+    		on = false;
+    	}
+    	else {
+    		throw new IllegalArgumentException("Received Message is invalid: " + message);
+    	}
+	}
+
+	@Override
     public String getLightwaveRfCommandString() {
         char funtion = on ? '1' : '0';
         return "!R" + roomId + "D" + deviceId + "F" + funtion; 
