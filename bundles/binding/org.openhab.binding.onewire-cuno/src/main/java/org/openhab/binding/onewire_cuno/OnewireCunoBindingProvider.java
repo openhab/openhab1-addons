d5c01a9ae7751d02953ece1c657a316b98360d11
Robert Czerwonka <querdenker2k@gmx.de>
2015-02-16 21:51:52 +0100

    add onewire binding for CUNO, based on cul-transport-io

---
+++ bundles/binding/org.openhab.binding.onewire-cuno/src/main/java/org/openhab/binding/onewire_cuno/OnewireCunoBindingProvider.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2010-2015, openHAB.org and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.onewire_cuno;
+
+import org.openhab.binding.onewire_cuno.internal.OnewireCunoBindingConfig;
+import org.openhab.core.binding.BindingProvider;
+
+/**
+ * @author Robert Delbrück
+ * @since 1.7.0
+ */
+public interface OnewireCunoBindingProvider extends BindingProvider {
+
+	public OnewireCunoBindingConfig getConfigForItemName(String itemName);
+
+	public OnewireCunoBindingConfig getConfigForAddress(String address);
+
+}
