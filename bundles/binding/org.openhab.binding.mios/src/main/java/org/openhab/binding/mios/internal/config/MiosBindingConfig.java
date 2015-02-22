a807041da646296f69d784657f2449bcffc471e6
Jack Sleuters <jack.sleuters@gmail.com>
2015-02-22 15:42:19 +0100

    Binding now also allows for Rollershutter items

--- bundles/binding/org.openhab.binding.mios/src/main/java/org/openhab/binding/mios/internal/config/MiosBindingConfig.java
+++ bundles/binding/org.openhab.binding.mios/src/main/java/org/openhab/binding/mios/internal/config/MiosBindingConfig.java
@@ -575,12 +575,13 @@ public abstract class MiosBindingConfig implements BindingConfig {
 	public void validateItemType(Item item) throws BindingConfigParseException {
 		Class<? extends Item> t = getItemType();
 
-		if (!((t == StringItem.class) || (t == SwitchItem.class) || (t == DimmerItem.class) || (t == NumberItem.class)
-				|| (t == ContactItem.class) || (t == DateTimeItem.class))) {
+		if (!((t == StringItem.class) || (t == SwitchItem.class) || (t == RollershutterItem.class)
+				|| (t == DimmerItem.class) || (t == NumberItem.class) || (t == ContactItem.class) 
+				|| (t == DateTimeItem.class))) {
 
 			throw new BindingConfigParseException(
 					String.format(
-							"Item %s is of type %s, but only String, Switch, Dimmer, Number, Contact and DataTime Items are allowed.",
+							"Item %s is of type %s, but only String, Switch, Rollershutter, Dimmer, Number, Contact and DateTime Items are allowed.",
 							item.getName(), item.getClass().getSimpleName()));
 
 		}
