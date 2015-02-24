a11acb3efa5c1774cfc2b77bd9e117b2232ed173
Thomas Eichstädt-Engelen <thomas@openhab.org>
2015-02-24 12:10:09 +0100

    removed trailing blanks

--- distribution/openhabhome/start.sh
+++ distribution/openhabhome/start.sh
@@ -14,20 +14,20 @@ cp=$(find $eclipsehome -name "org.eclipse.equinox.launcher_*.jar" | sort | tail 
 
 echo Launching the openHAB runtime...
 java \
-	-Dosgi.clean=true \ 
-	-Declipse.ignoreApp=true \ 
+	-Dosgi.clean=true \
+	-Declipse.ignoreApp=true \
 	-Dosgi.noShutdown=true  \
 	-Djetty.port=$HTTP_PORT  \
-	-Djetty.port.ssl=$HTTPS_PORT \ 
+	-Djetty.port.ssl=$HTTPS_PORT \
 	-Djetty.home=.  \
-	-Dlogback.configurationFile=configurations/logback.xml \ 
-	-Dfelix.fileinstall.dir=addons -Dfelix.fileinstall.filter=.*\\.jar \ 
+	-Dlogback.configurationFile=configurations/logback.xml \
+	-Dfelix.fileinstall.dir=addons -Dfelix.fileinstall.filter=.*\\.jar \
 	-Djava.library.path=lib \
-	-Djava.security.auth.login.config=./etc/login.conf \ 
+	-Djava.security.auth.login.config=./etc/login.conf \
 	-Dorg.quartz.properties=./etc/quartz.properties \
 	-Dequinox.ds.block_timeout=240000 \
 	-Dequinox.scr.waitTimeOnBlock=60000 \
 	-Dfelix.fileinstall.active.level=4 \
 	-Djava.awt.headless=true \
 	-jar $cp $* \
-	-console 
+	-console
