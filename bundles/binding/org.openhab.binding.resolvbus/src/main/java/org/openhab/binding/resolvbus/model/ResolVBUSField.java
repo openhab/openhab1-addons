faf6360405096e2d97ef02df0f0e9a75f4b86a2e
Michael Heckmann <michael@Michaels-MacBook-Pro-3.local>
2015-03-12 11:46:29 +0100

    Initial commit with empty classes and new XML File

---
+++ bundles/binding/org.openhab.binding.resolvbus/src/main/java/org/openhab/binding/resolvbus/model/ResolVBUSField.java
@@ -0,0 +1,19 @@
+package org.openhab.binding.resolvbus.model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = { "name", "factor", "unit", "offset", "bitSize", "format", "commonUsage" })
+@XmlRootElement(name = "field")
+
+public class ResolVBUSField {
+
+	
+	
+}
