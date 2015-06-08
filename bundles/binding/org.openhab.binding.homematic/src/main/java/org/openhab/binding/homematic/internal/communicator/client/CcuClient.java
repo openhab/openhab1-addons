b19582b400e5cd40f201098e9ffac6cddb43ecaf
Gerhard Riegler <gerrieg.openhab@icloud.com>
2015-06-08 16:29:14 +0200

    Raised connection timeout for CcuClient

--- bundles/binding/org.openhab.binding.homematic/src/main/java/org/openhab/binding/homematic/internal/communicator/client/CcuClient.java
+++ bundles/binding/org.openhab.binding.homematic/src/main/java/org/openhab/binding/homematic/internal/communicator/client/CcuClient.java
@@ -82,7 +82,7 @@ public class CcuClient extends BaseHomematicClient {
 
 		httpClient = new HttpClient(new SimpleHttpConnectionManager(true));
 		HttpClientParams params = httpClient.getParams();
-		params.setConnectionManagerTimeout(5000);
+		params.setConnectionManagerTimeout(15000);
 		params.setSoTimeout(30000);
 		params.setContentCharset("ISO-8859-1");
 	}
