130c25b33944bd086c10343eced4333b6c1d9993
Paolo Denti <paolo.denti@gmail.com>
2015-06-14 17:26:00 +0200

    .

--- bundles/binding/org.openhab.binding.sapp/src/main/java/org/openhab/binding/sapp/internal/SappGenericBindingProvider.java
+++ bundles/binding/org.openhab.binding.sapp/src/main/java/org/openhab/binding/sapp/internal/SappGenericBindingProvider.java
@@ -47,7 +47,7 @@ public class SappGenericBindingProvider extends AbstractGenericBindingProvider i
 
 	/**
 	 * {@inheritDoc}
-	 * [A/I/O/V]:[1-2500]:[1-8,L,H,*]
+	 * [A/I/O/V]:[1-2500]:[1-16,L,H,*]
 	 */
 	@Override
 	public void processBindingConfiguration(String context, Item item, String bindingConfig) throws BindingConfigParseException {
