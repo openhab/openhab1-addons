d152c1ad0fa2c85a4661409bb881523276c3eed8
Michael Heckmann <mail@michael-heckmann.de>
2015-09-01 21:52:16 +0200

    Update ScriptIdentifiableSimpleNameProvider.java

--- bundles/model/org.openhab.model.script/xtend-gen/org/openhab/model/script/jvmmodel/ScriptIdentifiableSimpleNameProvider.java
+++ bundles/model/org.openhab.model.script/xtend-gen/org/openhab/model/script/jvmmodel/ScriptIdentifiableSimpleNameProvider.java
@@ -1,6 +1,13 @@
-package org.openhab.model.script.jvmmodel;
+/**
+ * Copyright (c) 2010-2015, openHAB.org and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.core.script.jvmmodel;
 
-import java.util.Arrays;
 import org.eclipse.xtext.common.types.JvmIdentifiableElement;
 import org.eclipse.xtext.common.types.JvmType;
 import org.eclipse.xtext.xbase.featurecalls.IdentifiableSimpleNameProvider;
@@ -12,17 +19,15 @@ public class ScriptIdentifiableSimpleNameProvider extends IdentifiableSimpleName
   }
   
   protected String _getSimpleName(final JvmIdentifiableElement element) {
-    return super.getSimpleName(element);
+    String _simpleName = super.getSimpleName(element);
+    return _simpleName;
   }
   
   public String getSimpleName(final JvmIdentifiableElement element) {
     if (element instanceof JvmType) {
       return _getSimpleName((JvmType)element);
-    } else if (element != null) {
-      return _getSimpleName(element);
     } else {
-      throw new IllegalArgumentException("Unhandled parameter types: " +
-        Arrays.<Object>asList(element).toString());
+      return _getSimpleName(element);
     }
   }
 }
