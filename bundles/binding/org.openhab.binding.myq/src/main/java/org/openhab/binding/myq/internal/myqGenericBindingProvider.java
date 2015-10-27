1a5d890461fb5634e8d5ccf309f9c7478e16b6da
computergeek1507 <scooter_seh@yahoo.com>
2015-10-26 22:36:00 -0400

    Added myq basic layout for now

---
+++ bundles/binding/org.openhab.binding.myq/src/main/java/org/openhab/binding/myq/internal/myqGenericBindingProvider.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) 2010-2015, openHAB.org and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.myq.internal;
+
+import org.openhab.binding.myq.myqBindingProvider;
+import org.openhab.core.binding.BindingConfig;
+import org.openhab.core.items.Item;
+import org.openhab.core.library.items.DimmerItem;
+import org.openhab.core.library.items.SwitchItem;
+import org.openhab.model.item.binding.AbstractGenericBindingProvider;
+import org.openhab.model.item.binding.BindingConfigParseException;
+
+
+/**
+ * This class is responsible for parsing the binding configuration.
+ * 
+ * @author scooter_seh
+ * @since 1.8.0
+ */
+public class myqGenericBindingProvider extends AbstractGenericBindingProvider implements myqBindingProvider 
+{
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public String getBindingType() {
+		return "myq";
+	}
+
+	/**
+	 * @{inheritDoc}
+	 */
+	@Override
+	public void validateItemType(Item item, String bindingConfig) throws BindingConfigParseException 
+	{
+		//if (!(item instanceof SwitchItem)) {
+		//	throw new BindingConfigParseException("item '" + item.getName()
+		//			+ "' is of type '" + item.getClass().getSimpleName()
+		//			+ "', only SwitchItems are allowed - please check your *.items configuration");
+		//}
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void processBindingConfiguration(String context, Item item, String bindingConfig) throws BindingConfigParseException 
+	{
+		super.processBindingConfiguration(context, item, bindingConfig);
+		myqBindingConfig config = new myqBindingConfig();
+		
+		//parse bindingconfig here ...
+		
+		addBindingConfig(item, config);		
+	}
+	
+	
+	/**
+	 * This is a helper class holding binding specific configuration details
+	 * 
+	 * @author scooter_seh
+	 * @since 1.8.0
+	 */
+	class myqBindingConfig implements BindingConfig 
+	{
+		// put member fields here which holds the parsed values
+	}
+	
+	
+}
