91425b916eff4984f1aa37f78567aeed54feebd0
Scott <scooter_seh@yahoo.com>
2015-10-29 15:37:19 -0400

    added import java.util.Map back

--- bundles/binding/org.openhab.binding.myq/src/main/java/org/openhab/binding/myq/internal/LoginData.java
+++ bundles/binding/org.openhab.binding.myq/src/main/java/org/openhab/binding/myq/internal/LoginData.java
@@ -9,6 +9,7 @@
 package org.openhab.binding.myq.internal;
 
 import java.io.IOException;
+import java.util.Map;
 
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
