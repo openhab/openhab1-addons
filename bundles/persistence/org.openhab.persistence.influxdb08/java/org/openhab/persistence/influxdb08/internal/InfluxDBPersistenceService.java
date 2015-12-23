674f3ad886230c901b27882064d65b0900e1b385
Theo Weiss <theo.weiss@gmail.com>
2015-12-23 21:29:47 +0100

    rename influxdb to influxdb08

--- bundles/persistence/org.openhab.persistence.influxdb08/java/org/openhab/persistence/influxdb08/internal/InfluxDBPersistenceService.java
+++ bundles/persistence/org.openhab.persistence.influxdb08/java/org/openhab/persistence/influxdb08/internal/InfluxDBPersistenceService.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.openhab.persistence.influxdb.internal;
+package org.openhab.persistence.influxdb08.internal;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
