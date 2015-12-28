83274f50eab0156edab0590087de493bd4198f59
root <root@nathan.broadsoftlabs.com>
2015-12-28 13:38:59 -0500

    Removed unneeded files

--- bundles/binding/org.openhab.binding.mojio/src/main/java/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-javac -cp "/opt/openhab/server/plugins/*:/root/openhab.copy/bundles/io/org.openhab.io.rest.lib/lib/*:./" Test.java
-java -cp "/opt/openhab/server/plugins/*:/root/openhab.copy/bundles/io/org.openhab.io.rest.lib/lib/*:./" Test $*
-find . -name '*.class' -exec rm -f \{\} \;
