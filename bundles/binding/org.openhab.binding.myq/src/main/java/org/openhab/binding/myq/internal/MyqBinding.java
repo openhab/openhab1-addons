ec02cc4d216b7bd8293cc312ae95039cf00482cb
Rob Nielsen <rob.nielsen@yahoo.com>
2016-01-16 09:37:29 -0600

    Fix for defect 3808, remove extra Unicode character 0x0xfeff.

--- bundles/binding/org.openhab.binding.myq/src/main/java/org/openhab/binding/myq/internal/MyqBinding.java
+++ bundles/binding/org.openhab.binding.myq/src/main/java/org/openhab/binding/myq/internal/MyqBinding.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-﻿/**
+/**
  * Copyright (c) 2010-2015, openHAB.org and others.
  *
  * All rights reserved. This program and the accompanying materials
