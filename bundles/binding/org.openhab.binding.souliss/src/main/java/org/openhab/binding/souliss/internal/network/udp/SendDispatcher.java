79f55cb51ebcfdcc69b36de44f881821a2baa340
Thomas Eichstädt-Engelen <thomas@openhab.org>
2016-01-31 17:05:15 +0100

    fix build
    Signed-off-by: Thomas Eichstädt-Engelen <thomas@openhab.org>

--- bundles/binding/org.openhab.binding.souliss/src/main/java/org/openhab/binding/souliss/internal/network/udp/SendDispatcher.java
+++ bundles/binding/org.openhab.binding.souliss/src/main/java/org/openhab/binding/souliss/internal/network/udp/SendDispatcher.java
@@ -295,7 +295,7 @@ public class SendDispatcher {
                         // traduce il comando inviato con lo stato previsto e
                         // poi fa il confronto con lo stato attuale
                         if (logger.isDebugEnabled() && typ != null) {
-                            String s1 = Integer.toHexString(state);
+                            String s1 = Integer.toHexString((int) typ.getState());
                             String sStateMemoria = s1.length() < 2 ? "0x0" + s1.toUpperCase() : "0x" + s1.toUpperCase();
 
                             String sCmd = Integer.toHexString(packetsList.get(i).packet.getData()[j]);
