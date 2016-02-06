a1678ef55ba7b593f1b5d91d06f8e844fd673864
Pshatsillo <Pshatsillo@gmail.com>
2016-02-06 11:21:09 +0300

    Initial commit of new binding Noolite

---
+++ bundles/binding/org.openhab.binding.noolite/src/main/java/org/openhab/binding/noolite/internal/NooliteBinding.java
@@ -0,0 +1,323 @@
+/**
+ * Copyright (c) 2010-2015, openHAB.org and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.noolite.internal;
+
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.openhab.binding.noolite.NooliteBindingProvider;
+import org.openhab.binding.noolite.internal.noolite4j.RX2164;
+import org.openhab.binding.noolite.internal.noolite4j.watchers.BatteryState;
+import org.openhab.binding.noolite.internal.noolite4j.watchers.Notification;
+import org.openhab.binding.noolite.internal.noolite4j.watchers.Watcher;
+import org.openhab.core.binding.AbstractActiveBinding;
+import org.openhab.core.events.EventPublisher;
+import org.openhab.core.library.types.DecimalType;
+import org.openhab.core.library.types.OnOffType;
+import org.openhab.core.library.types.StringType;
+import org.openhab.core.types.Command;
+import org.openhab.core.types.State;
+import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Implement this class if you are going create an actively polling service like
+ * querying a Website/Device.
+ * 
+ * @author Petr Shatsillo
+ * @since 1.0.0
+ */
+public class NooliteBinding extends AbstractActiveBinding<NooliteBindingProvider> {
+
+    private static final Logger logger = LoggerFactory.getLogger(NooliteBinding.class);
+    // private boolean isSetPublisher;
+
+    /**
+     * The BundleContext. This is only valid when the bundle is ACTIVE. It is
+     * set in the activate() method and must not be accessed anymore once the
+     * deactivate() method was called or before activate() was called.
+     */
+    private BundleContext bundleContext;
+
+    private static EventPublisher ep;
+    /**
+     * the refresh interval which is used to poll values from the Noolite server
+     * (optional, defaults to 60000ms)
+     */
+    private long refreshInterval = 60000;
+    private boolean rx, tx;
+    TXChoose pc;
+    RX2164 rxw = new RX2164();;
+
+    public NooliteBinding() {
+    }
+
+    /**
+     * Called by the SCR to activate the component with its configuration read
+     * from CAS
+     * 
+     * @param bundleContext
+     *            BundleContext of the Bundle that defines this component
+     * @param configuration
+     *            Configuration properties for this component obtained from the
+     *            ConfigAdmin service
+     */
+    public void activate(final BundleContext bundleContext, final Map<String, Object> configuration) {
+	this.bundleContext = bundleContext;
+
+	// the configuration is guaranteed not to be null, because the component
+	// definition has the
+	// configuration-policy set to require. If set to 'optional' then the
+	// configuration may be null
+
+	// to override the default refresh interval one has to add a
+	// parameter to openhab.cfg like <bindingName>:refresh=<intervalInMs>
+	String refreshIntervalString = (String) configuration.get("refresh");
+	if (StringUtils.isNotBlank(refreshIntervalString)) {
+	    refreshInterval = Long.parseLong(refreshIntervalString);
+	}
+
+	String rxS = (String) configuration.get("RX");
+
+	if (StringUtils.isNotBlank(rxS)) {
+
+	    if (rxS.equals("On")) {
+		rx = true;
+	    }
+	}
+
+	String PC = (String) configuration.get("PC");
+
+	if (StringUtils.isNotBlank(PC)) {
+
+	    if (PC.equals("8")) {
+		tx = true;
+		pc = new TXChoose((byte) 8);
+	    } else if (PC.equals("16")) {
+		tx = true;
+		pc = new TXChoose((byte) 16);
+	    } else if (PC.equals("32")) {
+		tx = true;
+		pc = new TXChoose((byte) 32);
+	    }
+	}
+
+	setProperlyConfigured(true);
+
+	if (tx) {
+
+	    pc.open();
+	}
+
+	if (rx) {
+
+	    Watcher watcher = new Watcher() {
+
+		@Override
+		public void onNotification(Notification notification) {
+		    updateValues(notification);
+		}
+	    };
+	    rxw.open();
+	    rxw.addWatcher(watcher);
+	    rxw.start();
+	}
+    }
+
+    protected void updateValues(Notification notification) {
+
+	for (NooliteBindingProvider provider : providers) {
+	    for (String itemname : provider.getItemNames()) {
+
+		if ((provider.getType(itemname).equals("Receive"))
+			&& (provider.getChannel(itemname).equals("bindflag"))) {
+		    if (notification.getType().name().equals("BIND"))
+			eventPublisher.postUpdate(itemname, OnOffType.OFF);
+		}
+
+		if ((provider.getType(itemname).equals("Receive")) && (provider.getChannel(itemname).equals("test"))) {
+		    eventPublisher.postUpdate(itemname, StringType.valueOf(String.valueOf(notification.getChannel())));
+		}
+
+		if ((provider.getType(itemname).equals("Receive"))
+			&& (String.valueOf(notification.getChannel()).equals(provider.getChannel(itemname)))) {
+
+		    logger.debug("Channel #" + provider.getChannel(itemname));
+		    logger.debug("Type: " + provider.getType(itemname));
+		    logger.debug("DeviceType: " + provider.getDeviceType(itemname));
+		    logger.debug("Command: " + notification.getType().name());
+		    
+		    String[] DeviceType = provider.getDeviceType(itemname).split("_");
+		    if (notification.getType().name().equals("UNBIND"))
+			rxw.unbindChannel(notification.getChannel());
+
+		    if (DeviceType[0].contains("PT11")) {
+			if (notification.getType().name().equals("TURN_OFF")) {
+			    break;
+			} else if (DeviceType[1].equals("t")) {
+			    eventPublisher.postUpdate(itemname,
+				    DecimalType.valueOf(String.valueOf(notification.getValue("temp"))));
+			} else if (DeviceType[1].equals("h")) {
+			    eventPublisher.postUpdate(itemname,
+				    DecimalType.valueOf(String.valueOf(notification.getValue("humi"))));
+			} else if (DeviceType[1].equals("batt")) {
+			    BatteryState battery = (BatteryState) notification.getValue("battery");
+			    eventPublisher.postUpdate(itemname, StringType.valueOf(String.valueOf(battery.name())));
+			}
+		    } else if (DeviceType[0].contains("PU")) {
+			eventPublisher.postUpdate(itemname,
+				StringType.valueOf(String.valueOf(notification.getType().name())));
+		    }
+
+		}
+
+	    }
+	}
+    }
+
+    /**
+     * Called by the SCR when the configuration of a binding has been changed
+     * through the ConfigAdmin service.
+     * 
+     * @param configuration
+     *            Updated configuration properties
+     */
+    public void modified(final Map<String, Object> configuration) {
+	// update the internal configuration accordingly
+    }
+
+    /**
+     * Called by the SCR to deactivate the component when either the
+     * configuration is removed or mandatory references are no longer satisfied
+     * or the component has simply been stopped.
+     * 
+     * @param reason
+     *            Reason code for the deactivation:<br>
+     *            <ul>
+     *            <li>0 – Unspecified
+     *            <li>1 – The component was disabled
+     *            <li>2 – A reference became unsatisfied
+     *            <li>3 – A configuration was changed
+     *            <li>4 – A configuration was deleted
+     *            <li>5 – The component was disposed
+     *            <li>6 – The bundle was stopped
+     *            </ul>
+     */
+    public void deactivate(final int reason) {
+	this.bundleContext = null;
+	if (rx)
+	    rxw.close();
+	if (tx)
+	    pc.close();
+    }
+
+    /**
+     * @{inheritDoc}
+     */
+    @Override
+    protected long getRefreshInterval() {
+	return refreshInterval;
+    }
+
+    /**
+     * @{inheritDoc}
+     */
+    @Override
+    protected String getName() {
+	return "Noolite Refresh Service";
+    }
+
+    /**
+     * @{inheritDoc}
+     */
+    @Override
+    protected void execute() {
+
+    }
+
+    /**
+     * @{inheritDoc}
+     */
+    @Override
+    protected void internalReceiveCommand(String itemName, Command command) {
+
+	logger.debug("internalReceiveCommand({},{}) is called!", itemName, command);
+
+	for (NooliteBindingProvider provider : providers) {
+	    for (String itemname : provider.getItemNames()) {
+		if ((itemname.equals(itemName)) && (provider.getChannel(itemName).equals("bind"))) {
+
+		    if (provider.getType(itemName).equals("Receive")) {
+			rxw.bindChannel(Byte.parseByte(command.toString()));
+		    } else {
+			pc.bindChannel(Byte.parseByte(command.toString()));
+		    }
+		    logger.debug("binding " + command.toString() + " channel");
+
+		} else if ((itemname.equals(itemName)) && (provider.getChannel(itemName).equals("unbind"))) {
+
+		    if (provider.getType(itemName).equals("Receive")) {
+			rxw.unbindChannel(Byte.parseByte(command.toString()));
+			logger.debug("unbinding Receive " + command.toString() + " channel");
+		    } else {
+			pc.unbindChannel(Byte.parseByte(command.toString()));
+			logger.debug("unbinding Send " + command.toString() + " channel");
+		    }
+		    logger.debug("unbinding " + command.toString() + " channel");
+
+		} else if ((itemname.equals(itemName)) && (provider.getChannel(itemName).equals("unbindAll"))) {
+
+		    if (provider.getType(itemName).equals("Receive")) {
+			rxw.unbindAllChannels();
+		    }
+		    logger.debug("unbinding all channels");
+
+		} else if ((itemname.equals(itemName))
+			&& (provider.getItemType(itemname).toString().contains("SwitchItem")) && !(provider.getChannel(itemName).equals("bind"))) {
+
+		    if (command.toString().equals("ON")) {
+			pc.turnOn(Byte.parseByte(provider.getChannel(itemName)));
+			logger.debug(provider.getChannel(itemName));
+		    } else if (command.toString().equals("OFF")) {
+			pc.turnOff(Byte.parseByte(provider.getChannel(itemName)));
+			logger.debug(provider.getChannel(itemName));
+		    }
+		} else if ((itemname.equals(itemName))
+			&& (provider.getItemType(itemname).toString().contains("DimmerItem")) ) {
+		    pc.setLevel(Byte.parseByte(provider.getChannel(itemName)),(byte) Integer.parseInt(command.toString()));
+			//logger.debug(provider.getChannel(itemName));
+		} else if ((itemname.equals(itemName))
+			&& (provider.getItemType(itemname).toString().contains("StringItem")) ) {
+		    String[] Colors = command.toString().split(",");
+		    
+		    byte red = (byte)(Integer.parseInt(Colors[0])*2.55);
+		    byte green = (byte)(Integer.parseInt(Colors[1])*2.55);
+		    byte blue = (byte)(Integer.parseInt(Colors[2])*2.55);
+		    
+		    pc.setLevelRGB(Byte.parseByte(provider.getChannel(itemName)), red , green, blue);
+		    
+		}
+
+	    }
+	}
+    }
+
+    /**
+     * @{inheritDoc}
+     */
+    @Override
+    protected void internalReceiveUpdate(String itemName, State newState) {
+	// the code being executed when a state was sent on the openHAB
+	// event bus goes here. This method is only called if one of the
+	// BindingProviders provide a binding for the given 'itemName'.
+	logger.debug("internalReceiveUpdate({},{}) is called!", itemName, newState);
+    }
+
+}
---
+++ bundles/binding/org.openhab.binding.noolite/src/main/java/org/openhab/binding/noolite/internal/noolite4j/RX2164.java
@@ -0,0 +1,331 @@
+/*
+ * Copyright 2014 Nikolay A. Viguro
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openhab.binding.noolite.internal.noolite4j;
+
+/**
+ * This class is responsible for RX dongle operations.
+ * 
+ * @author  Nikolay Viguro
+ * @since 1.0.0
+ */
+
+import java.nio.ByteBuffer;
+
+import org.openhab.binding.noolite.internal.noolite4j.watchers.BatteryState;
+import org.openhab.binding.noolite.internal.noolite4j.watchers.CommandType;
+import org.openhab.binding.noolite.internal.noolite4j.watchers.DataFormat;
+import org.openhab.binding.noolite.internal.noolite4j.watchers.Notification;
+import org.openhab.binding.noolite.internal.noolite4j.watchers.SensorType;
+import org.openhab.binding.noolite.internal.noolite4j.watchers.Watcher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.usb4java.Context;
+import org.usb4java.DeviceHandle;
+import org.usb4java.LibUsb;
+import org.usb4java.LibUsbException;
+
+public class RX2164 {
+
+    private static final long READ_UPDATE_DELAY_MS = 200L;
+    private static final short VENDOR_ID = 5824; // 0x16c0;
+    private static final short PRODUCT_ID = 1500; // 0x05dc;
+    private final Context context = new Context();
+    private Watcher watcher = null;
+    private byte availableChannels = 64;
+    private boolean shutdown = false;
+    private DeviceHandle handle;
+    private boolean pause = false;
+    private static final Logger logger = LoggerFactory.getLogger(RX2164.class);
+
+    public void addWatcher(Watcher watcher) {
+	this.watcher = watcher;
+    }
+
+    public void open() throws LibUsbException {
+
+	logger.debug("Opening RX2164");
+
+	int result = LibUsb.init(context);
+	if (result != LibUsb.SUCCESS) {
+	    try {
+		throw new LibUsbException("Failed to itialize libusb", result);
+	    } catch (LibUsbException e) {
+		logger.debug("Failed to itialize libusb: ", result);
+		e.printStackTrace();
+	    }
+	}
+
+	handle = LibUsb.openDeviceWithVidPid(context, VENDOR_ID, PRODUCT_ID);
+
+	if (handle == null) {
+	    logger.debug("Device RX2164 not found!");
+	    return;
+	}
+
+	if (LibUsb.kernelDriverActive(handle, 0) == 1) {
+	    LibUsb.detachKernelDriver(handle, 0);
+	}
+
+	int ret = LibUsb.setConfiguration(handle, 1);
+
+	if (ret != LibUsb.SUCCESS) {
+	    logger.debug("Error configuring RX2164");
+	    LibUsb.close(handle);
+	    if (ret == LibUsb.ERROR_BUSY) {
+		logger.debug("Device RX2164 busy");
+	    }
+	    return;
+	}
+
+	LibUsb.claimInterface(handle, 0);
+    }
+
+    public void close() {
+	logger.debug("Closing RX2164");
+	shutdown = true;
+
+	if (handle != null)
+	    LibUsb.close(handle);
+
+	LibUsb.exit(context);
+    }
+
+    public void start() {
+	logger.debug("Starting incoming monitor for RX2164");
+
+	new Thread(new Runnable() {
+	    @Override
+	    public void run() {
+
+		int togl;
+
+		int tmpTogl = -10000;
+
+		ByteBuffer buf = ByteBuffer.allocateDirect(8);
+
+		while (!shutdown) {
+
+		    if (!pause) {
+			LibUsb.controlTransfer(handle,
+				(byte) (LibUsb.REQUEST_TYPE_CLASS | LibUsb.RECIPIENT_INTERFACE | LibUsb.ENDPOINT_IN),
+				(byte) 0x9, (short) 0x300, (short) 0, buf, 100L);
+		    }
+
+		    togl = buf.get(0) & 63;
+
+		    if (togl != tmpTogl) {
+
+			Notification notification = new Notification();
+
+			notification.setBuffer(buf);
+
+			byte channel = (byte) (buf.get(1) + 1);
+			byte action = buf.get(2);
+			byte dataFormat = buf.get(3);
+
+			logger.debug("Incoming message for RX2164");
+			logger.debug("TOGL value: " + togl);
+			logger.debug("Command: " + CommandType.getValue(action).name());
+			logger.debug("Channel: " + channel);
+
+			if (dataFormat == DataFormat.NO_DATA.ordinal()) {
+			    logger.debug("Count of data: no");
+			    notification.setDataFormat(DataFormat.NO_DATA);
+			} else if (dataFormat == DataFormat.ONE_BYTE.ordinal()) {
+			    logger.debug("Count of data: 1 byte");
+			    notification.setDataFormat(DataFormat.ONE_BYTE);
+			} else if (dataFormat == DataFormat.TWO_BYTE.ordinal()) {
+			    logger.debug("Count of data: 2 bytes");
+			    notification.setDataFormat(DataFormat.TWO_BYTE);
+			} else if (dataFormat == DataFormat.FOUR_BYTE.ordinal()) {
+			    logger.debug("Count of data: 4 bytes");
+			    notification.setDataFormat(DataFormat.FOUR_BYTE);
+			} else if (dataFormat == DataFormat.LED.ordinal()) {
+			    logger.debug("Count of data: LED format");
+			    notification.setDataFormat(DataFormat.LED);
+			} else {
+			    logger.debug("Count of data: unknown type");
+			    notification.setDataFormat(DataFormat.NO_DATA);
+			}
+
+			notification.setChannel(channel);
+
+			switch (CommandType.getValue(action)) {
+			case TURN_ON:
+			    notification.setType(CommandType.TURN_ON);
+			    watcher.onNotification(notification);
+			    break;
+			case TURN_OFF:
+			    notification.setType(CommandType.TURN_OFF);
+			    watcher.onNotification(notification);
+			    break;
+			case SET_LEVEL:
+			    notification.setType(CommandType.SET_LEVEL);
+			    notification.addData("level", buf.get(4));
+			    logger.debug("Device level: " + buf.get(4));
+			    watcher.onNotification(notification);
+			    break;
+			case SWITCH:
+			    notification.setType(CommandType.SWITCH);
+			    watcher.onNotification(notification);
+			    break;
+			case SLOW_TURN_ON:
+			    notification.setType(CommandType.SLOW_TURN_ON);
+			    watcher.onNotification(notification);
+			    break;
+			case SLOW_TURN_OFF:
+			    notification.setType(CommandType.SLOW_TURN_OFF);
+			    watcher.onNotification(notification);
+			    break;
+			case STOP_DIM_BRIGHT:
+			    notification.setType(CommandType.STOP_DIM_BRIGHT);
+			    watcher.onNotification(notification);
+			    break;
+			case REVERT_SLOW_TURN:
+			    notification.setType(CommandType.REVERT_SLOW_TURN);
+			    watcher.onNotification(notification);
+			    break;
+			case RUN_SCENE:
+			    notification.setType(CommandType.RUN_SCENE);
+			    watcher.onNotification(notification);
+			    break;
+			case RECORD_SCENE:
+			    notification.setType(CommandType.RECORD_SCENE);
+			    watcher.onNotification(notification);
+			    break;
+			case BIND:
+			    notification.setType(CommandType.BIND);
+
+			    if (notification.getDataFormat() == DataFormat.ONE_BYTE) {
+				// Тип сенсора
+				notification.addData("sensortype", SensorType.values()[(buf.get(4) & 0xff)]);
+			    }
+
+			    watcher.onNotification(notification);
+			    break;
+			case UNBIND:
+			    notification.setType(CommandType.UNBIND);
+			    watcher.onNotification(notification);
+			    break;
+			case SLOW_RGB_CHANGE:
+			    notification.setType(CommandType.SLOW_RGB_CHANGE);
+			    watcher.onNotification(notification);
+			    break;
+			case SWITCH_COLOR:
+			    notification.setType(CommandType.SWITCH_COLOR);
+			    watcher.onNotification(notification);
+			    break;
+			case SWITCH_MODE:
+			    notification.setType(CommandType.SWITCH_MODE);
+			    watcher.onNotification(notification);
+			    break;
+			case SWITCH_SPEED_MODE:
+			    notification.setType(CommandType.SWITCH_SPEED_MODE);
+			    watcher.onNotification(notification);
+			    break;
+			case BATTERY_LOW:
+			    notification.setType(CommandType.BATTERY_LOW);
+			    watcher.onNotification(notification);
+			    break;
+			case TEMP_HUMI:
+			    notification.setType(CommandType.TEMP_HUMI);
+
+			    int intTemp = ((buf.get(5) & 0x0f) << 8) + (buf.get(4) & 0xff);
+
+			    if (intTemp >= 0x800) {
+				intTemp = intTemp - 0x1000;
+			    }
+
+			    double temp = (double) intTemp / 10;
+
+			    notification.addData("battery", BatteryState.values()[(buf.get(5) >> 7) & 1]);
+
+			    notification.addData("temp", temp);
+
+			    notification.addData("sensortype", SensorType.values()[((buf.get(5) >> 4) & 7)]);
+
+			    notification.addData("humi", buf.get(6) & 0xff);
+
+			    notification.addData("analog", buf.get(7) & 0xff);
+
+			    watcher.onNotification(notification);
+			    break;
+
+			default:
+			    logger.debug("Unknown command: " + action);
+			}
+		    }
+
+		    try {
+			Thread.sleep(READ_UPDATE_DELAY_MS);
+		    } catch (InterruptedException e) {
+			logger.error("Error: " + e.getMessage());
+			e.printStackTrace();
+		    }
+
+		    tmpTogl = togl;
+		    buf.clear();
+		}
+	    }
+	}).start();
+    }
+
+    public void bindChannel(byte channel) {
+	if (channel > availableChannels - 1) {
+	    logger.error("Error! Channel count is less than your value!");
+	    return;
+	}
+
+	ByteBuffer buf = ByteBuffer.allocateDirect(8);
+	buf.put((byte) 1);
+	buf.put((byte) (channel - 1));
+
+	logger.debug("Buffer Content: " + buf.get(0) + " " + buf.get(1) + " " + buf.get(2) + " " + buf.get(3) + " "
+		+ buf.get(4) + " " + buf.get(5) + " " + buf.get(6) + " " + buf.get(7));
+
+	pause = true;
+	LibUsb.controlTransfer(handle, (byte) (LibUsb.REQUEST_TYPE_CLASS | LibUsb.RECIPIENT_INTERFACE), (byte) 0x9,
+		(short) 0x300, (short) 0, buf, 100L);
+	pause = false;
+    }
+
+    public void unbindChannel(byte channel) {
+	if (channel - 1 > availableChannels - 1) {
+	    logger.error("Error! Channel count is less than your value!");
+	    return;
+	}
+
+	ByteBuffer buf = ByteBuffer.allocateDirect(8);
+	buf.put((byte) 3);
+	buf.put((byte) (channel - 1));
+
+	pause = true;
+	LibUsb.controlTransfer(handle, (byte) (LibUsb.REQUEST_TYPE_CLASS | LibUsb.RECIPIENT_INTERFACE), (byte) 0x9,
+		(short) 0x300, (short) 0, buf, 100L);
+	pause = false;
+    }
+
+    public void unbindAllChannels() {
+	ByteBuffer buf = ByteBuffer.allocateDirect(8);
+	buf.put((byte) 4);
+
+	pause = true;
+	LibUsb.controlTransfer(handle, (byte) (LibUsb.REQUEST_TYPE_CLASS | LibUsb.RECIPIENT_INTERFACE), (byte) 0x9,
+		(short) 0x300, (short) 0, buf, 100);
+	pause = false;
+    }
+}
