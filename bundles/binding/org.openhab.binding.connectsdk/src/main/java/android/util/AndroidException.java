07c79ebb52c410651d5216da90bccc3f1b6c3f29
sprehn <sebastian.prehn@gmx.de>
2016-02-21 14:05:30 +0100

    Revert "Initial version of connect sdk binding. POC implementation on Channel and Volume selection."
    This reverts commit 59e5a1fa4abaabeea487ccb17c31b4a7e2bfeb0c.
    
    # Conflicts:
    #	bundles/binding/pom.xml
    #	pom.xml

--- bundles/binding/org.openhab.binding.connectsdk/src/main/java/android/util/AndroidException.java
+++
@@ -1,7 +0,0 @@
-package android.util;
-public class AndroidException extends java.lang.Exception {
-public  AndroidException() { super (); }
-public  AndroidException(java.lang.String name) { super(name); }
-public  AndroidException(java.lang.String name, java.lang.Throwable cause) { super(name, cause); }
-public  AndroidException(java.lang.Exception cause) { super(cause); }
-}
