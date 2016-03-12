03af47da223baed1c576db47e8d61c8df2c459d6
Stefan Roellin <stefan@roellin-baumann.ch>
2016-03-12 16:13:29 +0100

    cherry-picked from #4161
    Signed-off-by: Thomas Eichstädt-Engelen <thomas@openhab.org>

--- bundles/binding/org.openhab.binding.fritzboxtr064/src/main/java/org/openhab/binding/fritzboxtr064/internal/Helper.java
+++ bundles/binding/org.openhab.binding.fritzboxtr064/src/main/java/org/openhab/binding/fritzboxtr064/internal/Helper.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010-2015, openHAB.org and others.
+ * Copyright (c) 2010-2016, openHAB.org and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -8,9 +8,8 @@
  */
 package org.openhab.binding.fritzboxtr064.internal;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.StringWriter;
+import java.io.Writer;
 
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
@@ -19,57 +18,67 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
-import org.apache.xml.serialize.OutputFormat;
-import org.apache.xml.serialize.XMLSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.DOMImplementation;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
+import org.w3c.dom.ls.DOMImplementationLS;
+import org.w3c.dom.ls.LSOutput;
+import org.w3c.dom.ls.LSSerializer;
 
 /***
  * Static Helper methods
- * 
+ *
  * @author gitbock
  * @since 1.8.0
  */
 public class Helper {
-	
-	
-	/***
-	 * Helper method which converts XML Document into pretty formatted string
-	 * @param doc to convert
-	 * @return converted XML as String
-	 */
-	public static String documentToString(Document doc){
-		String strMsg ="";
-		OutputFormat format = new OutputFormat(doc);
-        format.setIndenting(true);
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        XMLSerializer serializer = new XMLSerializer(baos, format);
+    private static final Logger logger = LoggerFactory.getLogger(FritzboxTr064Binding.class);
+
+    /***
+     * Helper method which converts XML Document into pretty formatted string
+     *
+     * @param doc to convert
+     * @return converted XML as String
+     */
+    public static String documentToString(Document doc) {
+
+        String strMsg = "";
         try {
-			serializer.serialize(doc);
-			strMsg = baos.toString("UTF-8");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return strMsg;
-	}
-	
-	
-	/***
-	 * Converts  a xml Node into String
-	 * @param node to convert
-	 * @return converted string
-	 */
-	public static String nodeToString(Node node) {
-		  StringWriter sw = new StringWriter();
-		  try {
-		    Transformer t = TransformerFactory.newInstance().newTransformer();
-		    t.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
-		    t.transform(new DOMSource(node), new StreamResult(sw));
-		  } catch (TransformerException te) {
-		    System.out.println("nodeToString Transformer Exception");
-		  }
-		  return sw.toString();
-		}
-	
+            DOMImplementation domImpl = doc.getImplementation();
+            DOMImplementationLS domImplLS = (DOMImplementationLS) domImpl.getFeature("LS", "3.0");
+            LSSerializer lsSerializer = domImplLS.createLSSerializer();
+            lsSerializer.getDomConfig().setParameter("format-pretty-print", true);
+
+            Writer stringWriter = new StringWriter();
+            LSOutput lsOutput = domImplLS.createLSOutput();
+            lsOutput.setEncoding("UTF-8");
+            lsOutput.setCharacterStream(stringWriter);
+            lsSerializer.write(doc, lsOutput);
+            strMsg = stringWriter.toString();
+        } catch (Exception e) {
+            logger.warn("Error occured when converting document to string", e);
+        }
+        return strMsg;
+    }
+
+    /***
+     * Converts a xml Node into String
+     *
+     * @param node to convert
+     * @return converted string
+     */
+    public static String nodeToString(Node node) {
+        StringWriter sw = new StringWriter();
+        try {
+            Transformer t = TransformerFactory.newInstance().newTransformer();
+            t.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+            t.transform(new DOMSource(node), new StreamResult(sw));
+        } catch (TransformerException te) {
+            logger.warn("nodeToString Transformer Exception", te);
+        }
+        return sw.toString();
+    }
 
 }
