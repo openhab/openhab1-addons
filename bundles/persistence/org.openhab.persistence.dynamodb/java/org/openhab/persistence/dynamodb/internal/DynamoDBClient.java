3ddf8534381a239a70f8f2824c0d36652594e49e
Sami Salonen <ssalonen@gmail.com>
2016-05-16 07:51:38 +0300

    Copyright notice

--- bundles/persistence/org.openhab.persistence.dynamodb/java/org/openhab/persistence/dynamodb/internal/DynamoDBClient.java
+++ bundles/persistence/org.openhab.persistence.dynamodb/java/org/openhab/persistence/dynamodb/internal/DynamoDBClient.java
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2010-2016, openHAB.org and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.openhab.persistence.dynamodb.internal;
 
 import org.slf4j.Logger;
