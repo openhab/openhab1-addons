ff685ffb965817a453f0619d9f9664012ebea358
Laurens Van Acker <laurens@van.acker.be>
2016-06-03 10:52:11 +0200

    Added functions for OH2 and a new line at the end

--- bundles/binding/org.openhab.binding.sonance/src/main/java/org/openhab/binding/sonance/internal/SonanceBinding.java
+++ bundles/binding/org.openhab.binding.sonance/src/main/java/org/openhab/binding/sonance/internal/SonanceBinding.java
@@ -182,6 +182,14 @@ public class SonanceBinding extends AbstractActiveBinding<SonanceBindingProvider
         }
     }
 
+    protected void addBindingProvider(SonanceBindingProvider bindingProvider) {
+        super.addBindingProvider(bindingProvider);
+    }
+
+    protected void removeBindingProvider(SonanceBindingProvider bindingProvider) {
+        super.removeBindingProvider(bindingProvider);
+    }
+
     /*
      * {@inheritDoc}
      */
