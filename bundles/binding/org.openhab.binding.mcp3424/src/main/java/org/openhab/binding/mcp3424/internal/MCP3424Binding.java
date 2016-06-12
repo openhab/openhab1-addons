50daf4733e0d33cb59f1a67638a000e1f6417936
falkena <>
2016-06-12 12:45:39 +0200

    Replace usage of EnumMap with if decisions.

--- bundles/binding/org.openhab.binding.mcp3424/src/main/java/org/openhab/binding/mcp3424/internal/MCP3424Binding.java
+++ bundles/binding/org.openhab.binding.mcp3424/src/main/java/org/openhab/binding/mcp3424/internal/MCP3424Binding.java
@@ -1,40 +1,39 @@
 /**
- * Copyright (c) 2010-2015, openHAB.org and others.
+ * Copyright (c) 2010-2016, openHAB.org and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.openhab.binding.mcp23017.internal;
+package org.openhab.binding.mcp3424.internal;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import org.apache.commons.lang.StringUtils;
-import org.openhab.binding.mcp23017.MCP23017BindingProvider;
+import org.openhab.binding.mcp3424.MCP3424BindingProvider;
 import org.openhab.core.binding.AbstractActiveBinding;
 import org.openhab.core.binding.BindingProvider;
-import org.openhab.core.library.types.OnOffType;
-import org.openhab.core.library.types.OpenClosedType;
+import org.openhab.core.items.Item;
+import org.openhab.core.library.items.DimmerItem;
+import org.openhab.core.library.items.NumberItem;
+import org.openhab.core.library.types.DecimalType;
+import org.openhab.core.library.types.PercentType;
 import org.openhab.core.types.Command;
 import org.openhab.core.types.State;
 import org.osgi.framework.BundleContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.pi4j.gpio.extension.mcp.MCP23017GpioProvider;
+import com.pi4j.gpio.extension.mcp.MCP3424GpioProvider;
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPin;
-import com.pi4j.io.gpio.GpioPinDigitalInput;
-import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.GpioPinAnalogInput;
 import com.pi4j.io.gpio.Pin;
-import com.pi4j.io.gpio.PinMode;
-import com.pi4j.io.gpio.PinPullResistance;
-import com.pi4j.io.gpio.PinState;
-import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
-import com.pi4j.io.gpio.event.GpioPinListenerDigital;
+import com.pi4j.io.gpio.event.GpioPinAnalogValueChangeEvent;
+import com.pi4j.io.gpio.event.GpioPinListenerAnalog;
 import com.pi4j.io.i2c.I2CBus;
 import com.pi4j.io.i2c.I2CFactory.UnsupportedBusNumberException;
 
@@ -42,19 +41,18 @@ import com.pi4j.io.i2c.I2CFactory.UnsupportedBusNumberException;
  * Implement this class if you are going create an actively polling service like
  * querying a Website/Device.
  * 
- * @author Diego A. Fliess
  * @author Alexander Falkenstern
- * @since 1.8.0
+ * @since 1.8.3
  */
-public class MCP23017Binding extends AbstractActiveBinding<MCP23017BindingProvider> implements GpioPinListenerDigital {
+public class MCP3424Binding extends AbstractActiveBinding<MCP3424BindingProvider> implements GpioPinListenerAnalog {
 
-	private static final Logger logger = LoggerFactory.getLogger(MCP23017Binding.class);
+	private static final Logger logger = LoggerFactory.getLogger(MCP3424Binding.class);
 	
 	private final GpioController gpio = GpioFactory.getInstance();
 
 	private Map<String, GpioPin> gpioPins = new HashMap<String, GpioPin>();
 
-	private static final Map<Integer, MCP23017GpioProvider> mcpProviders = new HashMap<Integer, MCP23017GpioProvider>();
+	private static final Map<Integer, MCP3424GpioProvider> mcpProviders = new HashMap<Integer, MCP3424GpioProvider>();
 	
 	/**
 	 * The BundleContext. This is only valid when the bundle is ACTIVE. It is
@@ -65,17 +63,17 @@ public class MCP23017Binding extends AbstractActiveBinding<MCP23017BindingProvid
 	private BundleContext bundleContext;
 
 	/**
-	 * the refresh interval which is used to poll values from the mcp23017 server
+	 * the refresh interval which is used to poll values from the mcp3424 server
 	 * (optional, defaults to 60000ms)
 	 */
 	private long refreshInterval = 60000;
-	
-    /**
-     * the polling interval mcp23071 check interrupt register (optional, defaults to 50ms)
-     */
-	private int pollingInterval = 50;
 
-	public MCP23017Binding() {
+    /**
+     * the polling interval mcp3424 check interrupt register (optional, defaults to 50ms)
+     */
+    private int pollingInterval = 50;
+
+	public MCP3424Binding() {
 	}
 
 	/**
@@ -106,9 +104,9 @@ public class MCP23017Binding extends AbstractActiveBinding<MCP23017BindingProvid
         }
 		
 		// read further config parameters here ...
-		logger.debug("mcp23017 activated " + this.hashCode());
+		logger.debug("mcp3424 activated " + this.hashCode());
 		setProperlyConfigured(true);
-		logger.debug("mcp23017 activated and ProperlyConfigured " + this.hashCode());
+		logger.debug("mcp3424 activated and ProperlyConfigured " + this.hashCode());
 	}
 
 	/**
@@ -120,7 +118,7 @@ public class MCP23017Binding extends AbstractActiveBinding<MCP23017BindingProvid
 	 */
 	public void modified(final Map<String, Object> configuration) {
 		// update the internal configuration accordingly
-		logger.debug("mcp23017 modified");
+		logger.debug("mcp3424 modified");
 	}
 
 	/**
@@ -144,7 +142,7 @@ public class MCP23017Binding extends AbstractActiveBinding<MCP23017BindingProvid
 		this.bundleContext = null;
 		// deallocate resources here that are no longer needed and
 		// should be reset when activating this binding again
-		logger.debug("mcp23017 deactivated");
+		logger.debug("mcp3424 deactivated");
 		mcpProviders.clear();
 		gpio.shutdown();	
 	}
@@ -162,7 +160,7 @@ public class MCP23017Binding extends AbstractActiveBinding<MCP23017BindingProvid
 	 */
 	@Override
 	protected String getName() {
-		return "mcp23017 Refresh Service";
+		return "mcp3424 Refresh Service";
 	}
 
 	/**
@@ -181,7 +179,7 @@ public class MCP23017Binding extends AbstractActiveBinding<MCP23017BindingProvid
 		/*first call contains all, better use activate ? if you have providers*/
 		logger.debug("addBindingProvider: " + Arrays.toString(provider.getItemNames().toArray()));
 		for (String itemName: provider.getItemNames()) {
-			bindGpioPin((MCP23017BindingProvider)provider, itemName);
+			bindGpioPin((MCP3424BindingProvider)provider, itemName);
 		}
 	}
 
@@ -191,123 +189,116 @@ public class MCP23017Binding extends AbstractActiveBinding<MCP23017BindingProvid
 		/*shutdown call contains all better use deactivate*/
 		logger.debug("removeBindingProvider: " + Arrays.toString(provider.getItemNames().toArray()));
 		for (String itemName: provider.getItemNames()) {
-			unBindGpioPin((MCP23017BindingProvider) provider, itemName);
+			unBindGpioPin((MCP3424BindingProvider) provider, itemName);
 		}
 	}
 
 	@Override
 	public void bindingChanged(BindingProvider provider, String itemName) {
-		if (provider instanceof MCP23017BindingProvider) {
+		if (provider instanceof MCP3424BindingProvider) {
 			if (provider.getItemNames().contains(itemName)) {
-				bindGpioPin((MCP23017BindingProvider) provider, itemName);
+				bindGpioPin((MCP3424BindingProvider) provider, itemName);
 				logger.debug("bindingChanged item bound " + itemName + " - " + Arrays.toString(provider.getItemNames().toArray()));
 			} else {
-				unBindGpioPin((MCP23017BindingProvider) provider, itemName);
+				unBindGpioPin((MCP3424BindingProvider) provider, itemName);
 				logger.debug("bindingChanged item unbound " + itemName + " - " + Arrays.toString(provider.getItemNames().toArray()));
 			}
 		}
 		super.bindingChanged(provider, itemName);
 	}
 
-	private void bindGpioPin(MCP23017BindingProvider provider, String itemName) {
+	private void bindGpioPin(MCP3424BindingProvider provider, String itemName) {
 		try {
 			int address = provider.getBusAddress(itemName);
-			MCP23017GpioProvider mcp = mcpProviders.get(address);
+			MCP3424GpioProvider mcp = mcpProviders.get(address);
 			if(mcp == null)
 			{
 				try {
-					mcp = new MCP23017GpioProvider(I2CBus.BUS_1, address);
-					mcp.setPollingTime(pollingInterval);
+				    int gain = provider.getGain(itemName);
+                    int resolution = provider.getResolution(itemName);
+					mcp = new MCP3424GpioProvider(I2CBus.BUS_1, address, resolution, gain);
+					mcp.setMonitorInterval(pollingInterval);
 				} catch (UnsupportedBusNumberException ex) {
 					throw new IllegalArgumentException("Tried to access not available I2C bus");
 				}
 				mcpProviders.put(address, mcp);
 			}
-
+			
+            mcp.setMonitorEnabled(false);
 			Pin pin = provider.getPin(itemName);
-			PinMode mode = provider.getPinMode(itemName);
-			if (mode.equals(PinMode.DIGITAL_OUTPUT)) {
-				GpioPinDigitalOutput output = gpio.provisionDigitalOutputPin(mcp, pin, itemName, provider.getDefaultState(itemName));
-				gpioPins.put(itemName, output);
 
-				logger.debug("Provisioned Digital Output for " + itemName );
-			} else if (mode.equals(PinMode.DIGITAL_INPUT))  {
-				
-				 /** Note: MCP23017 has no internal pull-down, so I used pull-up and 
-				 *  inverted the button reading logic with a "not" 
-				 **/
-				GpioPinDigitalInput input = gpio.provisionDigitalInputPin(mcp, pin, itemName, PinPullResistance.OFF);
-				input.setDebounce(20);
-				input.addListener(this);
-				gpioPins.put(itemName, input);
+			GpioPinAnalogInput input = gpio.provisionAnalogInputPin(mcp, pin, itemName);
+            input.addListener(this);
+            input.setTag(provider.getItem(itemName));
+            gpioPins.put(itemName, input);
 
-				logger.debug("Provisioned Digital Input for " + itemName );
-			} else {
-				throw new IllegalArgumentException("Invalid Pin Mode in config " + mode.name());
-			}
+            mcp.setEventThreshold(0, input);
+            mcp.setMonitorEnabled(true);
+
+            logger.debug("Provisioned Analog Input for " + itemName);
 		} catch (IOException e) {
 			logger.error("IO ERROR " + e.getMessage());
 		}
 	}
 
-	private void unBindGpioPin(MCP23017BindingProvider provider, String itemName) {
+	private void unBindGpioPin(MCP3424BindingProvider provider, String itemName) {
 		GpioPin pin = gpioPins.remove(itemName);
 		gpio.unprovisionPin(pin);
-		logger.debug("unbound " + itemName );
+		logger.debug("unbound " + itemName);
 	}
 	
-	/**
-	 * @{inheritDoc
-	 */
-	@Override
-	protected void internalReceiveCommand(String itemName, Command command) {
-		// the code being executed when a command was sent on the openHAB
-		// event bus goes here. This method is only called if one of the
-		// BindingProviders provide a binding for the given 'itemName'.
-		logger.debug("internalReceiveCommand({},{}) is called!", itemName, command);
-		
-		// the configuration is guaranteed not to be null, because the component
-		// definition has the
-		// configuration-policy set to require. If set to 'optional' then the
-		// configuration may be null
-		if (command instanceof OnOffType) {
-			final OnOffType switchCommand = (OnOffType) command;
-			
-			for (MCP23017BindingProvider provider : this.providers) {
-				if (provider.providesBindingFor(itemName)) {
-					GpioPin pin = gpioPins.get(itemName);
-					if (switchCommand == OnOffType.OFF) {
-						gpio.setState(PinState.LOW, (GpioPinDigitalOutput)pin);
-					} else if (switchCommand == OnOffType.ON) {
-						gpio.setState(PinState.HIGH, (GpioPinDigitalOutput)pin);
-					}
-				}
-			}
-		}
-	}
-
-	/** Note: MCP23017 has no internal pull-down, so I used pull-up and 
+    /**
+     * @{inheritDoc
+     */
+    @Override
+    protected void internalReceiveCommand(String itemName, Command newState) {
+        super.internalReceiveCommand(itemName, newState);
+        // the code being executed when a command was sent on the openHAB
+        // event bus goes here. This method is only called if one of the
+        // BindingProviders provide a binding for the given 'itemName'.
+        logger.debug("internalReceiveCommand({},{}) is called!", itemName, newState);
+    }
+	
+	/** Note: MCP3424 has no internal pull-down, so I used pull-up and 
 	 *  inverted the button reading logic with a "not" 
 	 **/
 	@Override
-	public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
-		GpioPin pin = event.getPin();
-		OpenClosedType state = OpenClosedType.CLOSED; // Assume we are high...
-		if (event.getState() == PinState.LOW) { // To err is human...
-			state = OpenClosedType.OPEN;
-		}
-		this.eventPublisher.postUpdate(pin.getName(), state);
-		logger.debug(" --> GPIO PIN STATE CHANGE: {} = {}", pin, state);
+	public void handleGpioPinAnalogValueChangeEvent(GpioPinAnalogValueChangeEvent event) {
+	    GpioPin pin = event.getPin();
+        MCP3424GpioProvider provider = (MCP3424GpioProvider)pin.getProvider();
+
+        if (pin.getTag() instanceof NumberItem) {
+            double value = provider.getAnalogValue(pin.getPin());
+            try {
+                this.eventPublisher.postUpdate(pin.getName(), new DecimalType(value));
+//                this.eventPublisher.sendCommand(pin.getName(), new DecimalType(value));
+            } catch (NumberFormatException e) {
+                logger.warn("Unable to convert '{}' for item {} to number",
+                            new String[] { String.valueOf(value), pin.getName() });
+            }
+            logger.debug(" --> GPIO CHANNEL CHANGE: {} = {}", pin, value);
+        } else if (pin.getTag() instanceof DimmerItem){
+            double value = provider.getPercentValue(pin.getPin());
+            try {
+                this.eventPublisher.postUpdate(pin.getName(), new PercentType((int)value));
+//              this.eventPublisher.sendCommand(pin.getName(), new PercentType(value));
+            } catch (NumberFormatException e) {
+                logger.warn("Unable to convert '{}' for item {} to number",
+                              new String[] { String.valueOf(value), pin.getName() });
+            }
+            logger.debug(" --> GPIO CHANNEL CHANGE: {} = {}", pin, value);
+        }
 	}
 
-	/**
-	 * @{inheritDoc
-	 */
-	@Override
-	protected void internalReceiveUpdate(String itemName, State newState) {
-		// the code being executed when a state was sent on the openHAB
-		// event bus goes here. This method is only called if one of the
-		// BindingProviders provide a binding for the given 'itemName'.
-		logger.debug("internalReceiveUpdate({},{}) is called!", itemName, newState);
-	}
+    /**
+     * @{inheritDoc
+     */
+    @Override
+    protected void internalReceiveUpdate(String itemName, State newState) {
+        super.internalReceiveUpdate(itemName, newState);
+        // the code being executed when a state was sent on the openHAB
+        // event bus goes here. This method is only called if one of the
+        // BindingProviders provide a binding for the given 'itemName'.
+        logger.debug("internalReceiveUpdate({},{}) is called!", itemName, newState);
+    }
 }
