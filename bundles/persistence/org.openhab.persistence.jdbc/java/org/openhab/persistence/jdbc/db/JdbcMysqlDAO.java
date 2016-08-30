dd2b8a0ea62477851f01fd5a105aa25a96fe6d5d
Helmut Lehmeyer <helmut.lehmeyer@gmail.com>
2016-08-30 13:25:14 +0200

    JDBC now with high precision by fractional seconds in general. - after review (#4605)
    * mysql can use fractal seconds since 5.6. - after review
    
    added to configure store and read date format:
    
        #jdbc:sqltype.TABLEPRIMARYKEY = TIMESTAMP
        #jdbc:sqltype.TABLEPRIMARYVALUE = NOW()
    
    needed to update YANK and HicariCP
    
    now we can differentiate between database and driver versions, if
    needed.
    
    * - second review

--- bundles/persistence/org.openhab.persistence.jdbc/java/org/openhab/persistence/jdbc/db/JdbcMysqlDAO.java
+++ bundles/persistence/org.openhab.persistence.jdbc/java/org/openhab/persistence/jdbc/db/JdbcMysqlDAO.java
@@ -9,6 +9,7 @@
 package org.openhab.persistence.jdbc.db;
 
 import org.knowm.yank.Yank;
+import org.openhab.persistence.jdbc.utils.DbMetaData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,6 +17,10 @@ import org.slf4j.LoggerFactory;
  * Extended Database Configuration class. Class represents
  * the extended database-specific configuration. Overrides and supplements the
  * default settings from JdbcBaseDAO. Enter only the differences to JdbcBaseDAO here.
+ *
+ * since driver version >= 6.0 sometimes timezone conversation is needed: ?serverTimezone=UTC
+ * example: dbProps.setProperty("jdbcUrl", "jdbc:mysql://192.168.0.181:3306/ItemTypeTest3?serverTimezone=UTC");//mysql
+ * 5.7
  *
  * @author Helmut Lehmeyer
  * @since 1.8.0
@@ -42,7 +47,7 @@ public class JdbcMysqlDAO extends JdbcBaseDAO {
      */
     private void initSqlTypes() {
         logger.debug("JDBC::initSqlTypes: Initialize the type array");
-        // sqlTypes.put("STRINGITEM", "VARCHAR(65500)");//jdbc max 21845
+        sqlTypes.put("STRINGITEM", "VARCHAR(21717)");// mysql using utf-8 max 65535/3 = 21845, using 21845-128 = 21717
     }
 
     /**
@@ -64,6 +69,18 @@ public class JdbcMysqlDAO extends JdbcBaseDAO {
         // databaseProps.setProperty("dataSourceClassName", "com.mysql.jdbc.jdbc2.optional.MysqlDataSource");
         databaseProps.setProperty("maximumPoolSize", "3");
         databaseProps.setProperty("minimumIdle", "2");
+    }
+
+    @Override
+    public void initAfterFirstDbConnection() {
+        logger.debug("JDBC::initAfterFirstDbConnection: Initializing step, after db is connected.");
+        dbMeta = new DbMetaData();
+        // Initialize sqlTypes, depending on DB version for example
+        if (dbMeta.isDbVersionGreater(5, 5)) {
+            sqlTypes.put("DATETIMEITEM", "TIMESTAMP(3)");
+            sqlTypes.put("tablePrimaryKey", "TIMESTAMP(3)");
+            sqlTypes.put("tablePrimaryValue", "NOW(3)");
+        }
     }
 
     /**************
