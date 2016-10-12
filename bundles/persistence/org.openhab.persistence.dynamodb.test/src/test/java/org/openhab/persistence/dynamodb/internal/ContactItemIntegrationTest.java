b94af3fa916f3c9a090c9499e35cddc677777680
Sami Salonen <ssalonen@gmail.com>
2016-10-12 04:52:19 +0300

    Persistence service implementation for AWS DynamoDB (#4542)
    * Initial commit of dynamodb binding
    
    * working store&query
    
    * cleanup
    
    * using constants
    
    * removing obsolete stuff
    
    * interface
    
    * consistent naming
    
    * wip
    
    * tests, bug fixes
    
    * more clear test
    
    * rest of the tests
    
    * formatting
    
    * rename
    
    * skip integration tests if no properties specified
    
    * accidental smarthome import
    
    * compare actual states
    
    * cleanup
    
    * Use round instead of setScale
    
    * Copyright notice
    
    * more strict comparison of bigdecimals
    
    * less verbose logging
    
    * README
    
    * Method renaming
    
    * test comments
    
    * rm readme
    
    * Timezone bug fix (local vs UTC)
    
    * More tests with datetimetypes having local and utc timezones
    
    * Remove local sourcepath from dynamodb .classpath
    
    * PR 4542 code review: @return javadoc
    
    * PR 4542 code review: AbstractTwoItemIntegrationTest improved javadoc for the class
    
    * PR 4542 code review: clarify switch item and contact item test comments
    
    * PR 4542 code review: AbstractDynamoDBItem getter/setter design choices documented better
    
    * PR 4542 code review: DynamoDBClient java doc fix
    
    * PR 4542 code review: Using parameterized logging. Improved log message when region is invalid.
    
    * PR 4542 code review: Service activated log message is now the last statement
    
    * PR 4542 code review: DynamoDBPersistenceService now tries connection again on later store/query even if first connection was unsucessfull
    
    * PR 4542 code review: log message cleanup
    
    * Cleanly catch dynamodb query exceptions
    
    * better error messages on different connection errors.
    
    * catch-all exception handling when testing connection
    
    * @author added
    
    * PR 4542 code review: java doc correction (dynamodb is not a time series db)
    
    * PR 4542 code review: cleaner handling of configuration creation. Null & empty service configuration now explicitly handled
    
    * java7 compat fix

---
+++ bundles/persistence/org.openhab.persistence.dynamodb.test/src/test/java/org/openhab/persistence/dynamodb/internal/ContactItemIntegrationTest.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2010-2016, openHAB.org and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.persistence.dynamodb.internal;
+
+import java.util.Date;
+
+import org.junit.BeforeClass;
+import org.openhab.core.library.items.ContactItem;
+import org.openhab.core.library.types.OnOffType;
+import org.openhab.core.library.types.OpenClosedType;
+import org.openhab.core.types.State;
+
+/**
+ * 
+ * @author Sami Salonen
+ *
+ */
+public class ContactItemIntegrationTest extends AbstractTwoItemIntegrationTest {
+
+    private static final String name = "contact";
+    private static final OpenClosedType state1 = OpenClosedType.CLOSED;
+    private static final OpenClosedType state2 = OpenClosedType.OPEN;
+    // There is no OpenClosedType state value between CLOSED and OPEN.
+    // Omit extended query tests AbstractTwoItemIntegrationTest by setting stateBetween to null.
+    private static final OnOffType stateBetween = null;
+
+    @BeforeClass
+    public static void storeData() throws InterruptedException {
+        ContactItem item = (ContactItem) items.get(name);
+        item.setState(state1);
+        beforeStore = new Date();
+        Thread.sleep(10);
+        service.store(item);
+        afterStore1 = new Date();
+        Thread.sleep(10);
+        item.setState(state2);
+        service.store(item);
+        Thread.sleep(10);
+        afterStore2 = new Date();
+
+        logger.info("Created item between {} and {}", AbstractDynamoDBItem.DATEFORMATTER.format(beforeStore),
+                AbstractDynamoDBItem.DATEFORMATTER.format(afterStore1));
+    }
+
+    @Override
+    protected String getItemName() {
+        return name;
+    }
+
+    @Override
+    protected State getFirstItemState() {
+        return state1;
+    }
+
+    @Override
+    protected State getSecondItemState() {
+        return state2;
+    }
+
+    @Override
+    protected State getQueryItemStateBetween() {
+        return stateBetween;
+    }
+
+}
