824d75aa28af9925b25caf36ca9a9a75a655e9fd
Slawomir Jaranowski <s.jaranowski@gmail.com>
2016-11-07 10:16:12 +0100

    Fatek PLC binding (#2784)
    Signed-off-by: Slawomir Jaranowski <s.jaranowski@gmail.com> (github: slawekjaranowski)

---
+++ bundles/binding/org.openhab.binding.fatekplc/src/main/java/org/openhab/binding/fatekplc/items/FatekPercentItem.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2010-2016, openHAB.org and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.fatekplc.items;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+
+import org.openhab.core.items.Item;
+import org.openhab.core.library.types.PercentType;
+import org.openhab.core.types.State;
+import org.openhab.model.item.binding.BindingConfigParseException;
+
+import com.github.s4u.jfatek.registers.Reg;
+import com.github.s4u.jfatek.registers.RegValue;
+
+
+/**
+ * Common implementation for Percent items.
+ *
+ * @author Slawomir Jaranowski
+ * @since 1.9.0
+ */
+public abstract class FatekPercentItem extends FatekPLCItem {
+
+	protected BigDecimal factor;
+
+	public FatekPercentItem(Item item, List<String> confItems) throws BindingConfigParseException {
+		super(item, confItems);
+
+		String sFactor = getParamsFromConf(confItems, "factor", null);
+		if (sFactor != null) {
+			factor = new BigDecimal(sFactor);
+		}
+	}
+
+	@Override
+	public State getState(Map<Reg, RegValue> response) {
+
+		int v = response.get(reg1).intValueUnsigned();
+
+		if (factor != null) {
+			v = new BigDecimal(v).multiply(factor).intValue();
+		}
+
+		v = Math.min(v, 100);
+
+		if (v == 0) {
+			return PercentType.ZERO;
+		}
+
+		if (v == 100) {
+			return PercentType.HUNDRED;
+		}
+
+		return new PercentType(v);
+	}
+
+}
