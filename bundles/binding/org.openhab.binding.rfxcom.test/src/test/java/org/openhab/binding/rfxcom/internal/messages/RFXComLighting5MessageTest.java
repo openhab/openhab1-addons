46429169a7b4a27e14a42ba046772708364dacbb
Martin van Wingerden <martinvw@users.noreply.github.com>
2016-12-17 01:33:17 +0100

    Add support for IT message to support the Flamingo FA500 and others (#4898)
    * Add support for IT message to support the Flamingo FA500
    
    fixes openhab/openhab2-addons/#1019
    
    Signed-off-by: Martin van Wingerden <martinvw@mtin.nl> (github: martinvw)
    
    * Fixed review comments
    - Added myself as author
    - removed * imports
    - explained the command which are only supported by certain sub types.
    
    Signed-off-by: Martin van Wingerden <martinvw@mtin.nl> (github: martinvw)
    
    * Improved the explanation for  the commands which are only supported by certain sub types.
    
    Signed-off-by: Martin van Wingerden <martinvw@mtin.nl> (github: martinvw)

--- bundles/binding/org.openhab.binding.rfxcom.test/src/test/java/org/openhab/binding/rfxcom/internal/messages/RFXComLighting5MessageTest.java
+++ bundles/binding/org.openhab.binding.rfxcom.test/src/test/java/org/openhab/binding/rfxcom/internal/messages/RFXComLighting5MessageTest.java
@@ -8,9 +8,17 @@
  */
 package org.openhab.binding.rfxcom.internal.messages;
 
+import static org.junit.Assert.assertEquals;
+import static org.openhab.binding.rfxcom.internal.messages.RFXComBaseMessage.PacketType.LIGHTING5;
+import static org.openhab.binding.rfxcom.internal.messages.RFXComLighting5Message.Commands.ON;
+import static org.openhab.binding.rfxcom.internal.messages.RFXComLighting5Message.SubType.IT;
+
+import javax.xml.bind.DatatypeConverter;
+
 import org.junit.Test;
+import org.openhab.binding.rfxcom.RFXComValueSelector;
 import org.openhab.binding.rfxcom.internal.RFXComException;
-import org.openhab.binding.rfxcom.internal.messages.RFXComBaseMessage.PacketType;
+import org.openhab.core.library.types.OnOffType;
 
 /**
  * Test for RFXCom-binding
@@ -20,14 +28,25 @@ import org.openhab.binding.rfxcom.internal.messages.RFXComBaseMessage.PacketType
  */
 public class RFXComLighting5MessageTest {
     @Test
-    public void checkForSupportTest() throws RFXComException {
-        RFXComMessageFactory.getMessageInterface(PacketType.LIGHTING5);
+    public void basicBoundaryCheck() throws RFXComException {
+        RFXComTestHelper.basicBoundaryCheck(LIGHTING5);
     }
 
     @Test
-    public void basicBoundaryCheck() throws RFXComException {
-        RFXComTestHelper.basicBoundaryCheck(PacketType.LIGHTING5);
+    public void convertFromStateItMessage() throws RFXComException {
+        RFXComMessageInterface itMessageObject = RFXComMessageFactory.getMessageInterface(LIGHTING5);
+        itMessageObject.convertFromState(RFXComValueSelector.COMMAND, "2061.1", IT, OnOffType.ON, (byte) 0);
+
+        byte[] message = itMessageObject.decodeMessage();
+        String hexMessage = DatatypeConverter.printHexBinary(message);
+
+        assertEquals("Message is not as expected", "0A140F0000080D01010000", hexMessage);
+
+        RFXComLighting5Message msg = (RFXComLighting5Message) RFXComMessageFactory.getMessageInterface(message);
+        assertEquals("SubType", IT, msg.subType);
+        assertEquals("Sensor Id", "2061.1", msg.generateDeviceId());
+        assertEquals("Command", ON, msg.command);
     }
 
-    // TODO please add tests for real messages
+    // TODO please add more tests for different messages
 }
