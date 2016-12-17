8bcc7c8d43f2d5d577494d8c299ae516c1398fdf
Vladimir Grebenschikov <vova@fbsd.ru>
2016-12-17 22:41:21 +0300

    Leave only EPL license in Moka7 sources, after approval by the author

--- bundles/binding/org.openhab.binding.plclogo/src/main/java/Moka7/S7CpuInfo.java
+++ bundles/binding/org.openhab.binding.plclogo/src/main/java/Moka7/S7CpuInfo.java
@@ -1,16 +1,13 @@
 /*=============================================================================|
 |  PROJECT Moka7                                                         1.0.2 |
 |==============================================================================|
-|  Copyright (C) 2013, 2016 Davide Nardella                                    |
+|  Copyright (c) 2013-2016 by Davide Nardella                                  |
 |  All rights reserved.                                                        |
 |==============================================================================|
-|  SNAP7 is free software: you can redistribute it and/or modify               |
-|  it under the terms of the Lesser GNU General Public License as published by |
-|  the Free Software Foundation, either version 3 of the License, or under     |
-|  EPL Eclipse Public License 1.0.                                             |
-|                                                                              |
-|  This means that you have to chose in advance which take before you import   |
-|  the library into your project.                                              |
+|  This program and the accompanying materials                                 |
+|  are made available under the terms of the Eclipse Public License v1.0       |
+|  which accompanies this distribution, and is available at                    |
+|  http://www.eclipse.org/legal/epl-v10.html                                   |
 |                                                                              |
 |  SNAP7 is distributed in the hope that it will be useful,                    |
 |  but WITHOUT ANY WARRANTY; without even the implied warranty of              |
