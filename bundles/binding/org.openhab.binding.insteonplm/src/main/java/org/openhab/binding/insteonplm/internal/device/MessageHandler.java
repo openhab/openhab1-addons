openhab/openhab1-addons

commit 0d13cc0761633efd7998f89bca31e6887403490a

insteonplm: add support for smoke bridge (this time for real) (#5054)
* insteonplm: added (untested!) venstart th definitions

* insteonplm: updated comments in openhab_default.cfg

* insteonplm: added support for smoke bridge

bundles/binding/org.openhab.binding.insteonplm/src/main/java/org/openhab/binding/insteonplm/internal/device/MessageHandler.java
@@ -955,7 +955,7 @@ public abstract class MessageHandler {
             try {
                 // first do the bit manipulations to focus on the right area
                 int mask = getIntParameter("mask", 0xFFFF);
-                int rawValue = extractValue(msg);
+                int rawValue = extractValue(msg, group);
                 int cooked = (rawValue & mask) >> getIntParameter("rshift", 0);
                 // now do an arbitrary transform on the data
                 double value = transform(cooked);
@@ -971,16 +971,20 @@ public abstract class MessageHandler {
             return (raw);
         }
 
-        private int extractValue(Msg msg) throws FieldException {
+        private int extractValue(Msg msg, int group) throws FieldException {
             String lowByte = getStringParameter("low_byte", "");
-            if (lowByte == "") {
+            if (!lowByte.equals("")) {
                 logger.error("{} handler misconfigured, missing low_byte!", nm());
                 return 0;
             }
-
-            int value = msg.getByte(lowByte) & 0xFF;
+			int value = 0;
+			if (lowByte.equals("group")) {
+				value = group; 
+			} else {
+				value = msg.getByte(lowByte) & 0xFF;
+			}
             String highByte = getStringParameter("high_byte", "");
-            if (highByte != "") {
+            if (!highByte.equals("")) {
                 value |= (msg.getByte(highByte) & 0xFF) << 8;
             }
             return (value);

bundles/binding/org.openhab.binding.insteonplm/src/main/resources/device_features.xml
@@ -764,4 +764,12 @@
 	<command-handler default="true">NoOpCommandHandler</command-handler>
 	<poll-handler>NoPollHandler</poll-handler> <!-- polled by ThermostatData1bGroup -->
 </feature>
+<feature name="ReceiveBroadcast">
+	<message-dispatcher>DefaultDispatcher</message-dispatcher>
+	<!-- handles out-of band status messages -->
+	<message-handler cmd="0x11" ext="0" match_cmd1="0x11" low_byte="group">NumberMsgHandler</message-handler>
+	<message-handler default="true">NoOpMsgHandler</message-handler>
+	<poll-handler>NoPollHandler</poll-handler> <!-- broadcast messages only, no polling! -->
+</feature>
+
 </xml>

bundles/binding/org.openhab.binding.insteonplm/src/main/resources/device_types.xml
@@ -468,5 +468,11 @@ Example entry:
    </feature_group>
    <feature name="lastheardfrom">GenericLastTime</feature>
  </device>
+ <device productKey="F00.00.22">
+   <model>2982-222</model>
+   <description>Insteon Smoke Bridge</description>
+   <feature name="notification">ReceiveBroadcast</feature>
+   <feature name="lastheardfrom">GenericLastTime</feature>
+ </device>
 
 </xml>

commit c8b04aec012b5718becbc54b458734b9c367f774

insteonplm: fixed bug introduced with PR #5054

bundles/binding/org.openhab.binding.insteonplm/src/main/java/org/openhab/binding/insteonplm/internal/device/MessageHandler.java
@@ -973,7 +973,7 @@ public abstract class MessageHandler {
 
         private int extractValue(Msg msg, int group) throws FieldException {
             String lowByte = getStringParameter("low_byte", "");
-            if (!lowByte.equals("")) {
+            if (lowByte.equals("")) {
                 logger.error("{} handler misconfigured, missing low_byte!", nm());
                 return 0;
             }