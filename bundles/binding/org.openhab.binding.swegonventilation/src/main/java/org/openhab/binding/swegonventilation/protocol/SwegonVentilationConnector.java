97b13adc1d572b8f2a8801cd1bf9c5f7ad09c5df
Pauli Anttila <pauli.anttila@gmail.com>
2017-06-28 19:35:35 +0300

    Support for raw UDP PDU's
    Signed-off-by: Pauli Anttila <pauli.anttila@gmail.com>

--- bundles/binding/org.openhab.binding.swegonventilation/src/main/java/org/openhab/binding/swegonventilation/protocol/SwegonVentilationConnector.java
+++ bundles/binding/org.openhab.binding.swegonventilation/src/main/java/org/openhab/binding/swegonventilation/protocol/SwegonVentilationConnector.java
@@ -20,30 +20,34 @@ public abstract class SwegonVentilationConnector {
 
     /**
      * Procedure for connect to Swegon ventilation system.
-     * 
+     *
      * @throws SwegonVentilationException
      */
     public abstract void connect() throws SwegonVentilationException;
 
     /**
      * Procedure for disconnect from Swegon ventilation system.
-     * 
+     *
      * @throws SwegonVentilationException
      */
     public abstract void disconnect() throws SwegonVentilationException;
 
     /**
      * Procedure for receiving datagram from Swegon ventilation system.
-     * 
+     *
      * @throws SwegonVentilationException
      */
     public abstract byte[] receiveDatagram() throws SwegonVentilationException;
 
     public int calculateCRC(byte[] bytes, int len) {
+        return calculateCRC(bytes, 0, len);
+    }
+
+    public static int calculateCRC(byte[] bytes, int start, int end) {
         int crc = 0xFFFF; // initial value
         int polynomial = 0x1021; // 0001 0000 0010 0001 (0, 5, 12)
 
-        for (int index = 0; index < len; index++) {
+        for (int index = start; index < end; index++) {
             byte b = bytes[index];
             for (int i = 0; i < 8; i++) {
                 boolean bit = ((b >> (7 - i) & 1) == 1);
