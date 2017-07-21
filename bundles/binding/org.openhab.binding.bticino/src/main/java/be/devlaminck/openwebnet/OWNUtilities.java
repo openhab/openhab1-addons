c9c190828c8eea04799ebd82b9f66e0d0396733b
9037568 <namraccr@gmail.com>
2017-07-21 00:53:24 -0400

    Formatting fixes.

--- bundles/binding/org.openhab.binding.bticino/src/main/java/be/devlaminck/openwebnet/OWNUtilities.java
+++ bundles/binding/org.openhab.binding.bticino/src/main/java/be/devlaminck/openwebnet/OWNUtilities.java
@@ -26,135 +26,135 @@ import java.util.GregorianCalendar;
  */
 public class OWNUtilities {
 
-	// ------------------------------------------------------------------------
+    // ------------------------------------------------------------------------
 
-	/**
-	 * OWN Control Messages
-	 */
-	public final static String MSG_OPEN_ACK = "*#*1##";
-	public final static String MSG_OPEN_NACK = "*#*0##";
+    /**
+     * OWN Control Messages
+     */
+    public final static String MSG_OPEN_ACK = "*#*1##";
+    public final static String MSG_OPEN_NACK = "*#*0##";
 
-	// ------------------------------------------------------------------------
+    // ------------------------------------------------------------------------
 
-	/**
-	 * Create the frame to send to the own gateway
-	 * 
-	 * @param c
-	 * @return
-	 */
-	public static String createFrame(ProtocolRead c) {
-		String frame = null;
-		String address[] = null;
-		String who = null;
-		String what = null;
-		String where = null;
+    /**
+     * Create the frame to send to the own gateway
+     * 
+     * @param c
+     * @return
+     */
+    public static String createFrame(ProtocolRead c) {
+        String frame = null;
+        String address[] = null;
+        String who = null;
+        String what = null;
+        String where = null;
 
-		who = c.getProperty("who");
-		what = c.getProperty("what");
-		address = c.getProperty("address").split("\\*");
-		where = address[0];
-		frame = "*" + who + "*" + what + "*" + where + "##";
-		return (frame);
-	}
+        who = c.getProperty("who");
+        what = c.getProperty("what");
+        address = c.getProperty("address").split("\\*");
+        where = address[0];
+        frame = "*" + who + "*" + what + "*" + where + "##";
+        return (frame);
+    }
 
-	// ------------------------------------------------------------------------
+    // ------------------------------------------------------------------------
 
-	/**
-	 * Get datetime on format "dd/MM/yyyy HH:mm:ss"
-	 * 
-	 * @return
-	 */
-	public static String getDateTime() {
-		Calendar calendar = new GregorianCalendar();
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-		return (sdf.format(calendar.getTime()));
-	}
+    /**
+     * Get datetime on format "dd/MM/yyyy HH:mm:ss"
+     * 
+     * @return
+     */
+    public static String getDateTime() {
+        Calendar calendar = new GregorianCalendar();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        return (sdf.format(calendar.getTime()));
+    }
 
-	// ------------------------------------------------------------------------
+    // ------------------------------------------------------------------------
 
-	/**
-	 * Convert temperature from bticino bus format to readeable format
-	 * 
-	 * @param temperature
-	 * @return
-	 */
-	public static String convertTemperature(String temperature) {
-		String temp = "";
-		if (!temperature.substring(0, 1).equalsIgnoreCase("0")) {
-			temp += "-";
-		}
-		temp += temperature.substring(1, 3);
-		temp += ".";
-		temp += temperature.substring(3);
-		return (temp);
-	}
+    /**
+     * Convert temperature from bticino bus format to readable format
+     * 
+     * @param temperature
+     * @return
+     */
+    public static String convertTemperature(String temperature) {
+        String temp = "";
+        if (!temperature.substring(0, 1).equalsIgnoreCase("0")) {
+            temp += "-";
+        }
+        temp += temperature.substring(1, 3);
+        temp += ".";
+        temp += temperature.substring(3);
+        return (temp);
+    }
 
-	// ------------------------------------------------------------------------
+    // ------------------------------------------------------------------------
 
-	/**
-	 * Convert day from number to name
-	 * 
-	 * @param dayNumber
-	 * @return
-	 */
-	public static String dayName(String dayNumber) {
-		String dayName = null;
-		switch (Integer.parseInt(dayNumber)) {
-		case (0):
-			dayName = "Sunday";
-			break;
-		case (1):
-			dayName = "Monday";
-			break;
-		case (2):
-			dayName = "Tuesday";
-			break;
-		case (3):
-			dayName = "Wednesday";
-			break;
-		case (4):
-			dayName = "Thursday";
-			break;
-		case (5):
-			dayName = "Friday";
-			break;
-		case (6):
-			dayName = "Saturday";
-			break;
-		default:
-			dayName = "Invalid day number [" + dayNumber + "]";
-		}
-		return (dayName);
-	}
+    /**
+     * Convert day from number to name
+     * 
+     * @param dayNumber
+     * @return
+     */
+    public static String dayName(String dayNumber) {
+        String dayName = null;
+        switch (Integer.parseInt(dayNumber)) {
+            case (0):
+                dayName = "Sunday";
+                break;
+            case (1):
+                dayName = "Monday";
+                break;
+            case (2):
+                dayName = "Tuesday";
+                break;
+            case (3):
+                dayName = "Wednesday";
+                break;
+            case (4):
+                dayName = "Thursday";
+                break;
+            case (5):
+                dayName = "Friday";
+                break;
+            case (6):
+                dayName = "Saturday";
+                break;
+            default:
+                dayName = "Invalid day number [" + dayNumber + "]";
+        }
+        return (dayName);
+    }
 
-	// ------------------------------------------------------------------------
+    // ------------------------------------------------------------------------
 
-	/**
-	 * Get model name from model number
-	 * 
-	 * @param modelNumber
-	 * @return
-	 */
-	public static String gatewayModel(String modelNumber) {
-		String model = null;
-		switch (new Integer(Integer.parseInt(modelNumber))) {
-		case (2):
-			model = "MHServer";
-		case (4):
-			model = "MH20F0";
-		case (6):
-			model = "F452V";
-		case (11):
-			model = "MHServer2";
-		case (12):
-			model = "F453AV";
-		case (13):
-			model = "H4684";
-		default:
-			model = "Unknown";
-		}
-		return (model);
-	}
+    /**
+     * Get model name from model number
+     * 
+     * @param modelNumber
+     * @return
+     */
+    public static String gatewayModel(String modelNumber) {
+        String model = null;
+        switch (new Integer(Integer.parseInt(modelNumber))) {
+            case (2):
+                model = "MHServer";
+            case (4):
+                model = "MH20F0";
+            case (6):
+                model = "F452V";
+            case (11):
+                model = "MHServer2";
+            case (12):
+                model = "F453AV";
+            case (13):
+                model = "H4684";
+            default:
+                model = "Unknown";
+        }
+        return (model);
+    }
 
-	// ------------------------------------------------------------------------
+    // ------------------------------------------------------------------------
 }
