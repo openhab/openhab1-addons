6622f880c6cc9ba8b2fb3d182f79e3b0be753e95
rinal13 <32493689+rinal13@users.noreply.github.com>
2018-02-04 15:42:04 +0100

    Enhancement Heating Control
    + add OWN specifics for heating mode controls
    + update ConvertTemperature to be working with temperature offset

--- bundles/binding/org.openhab.binding.bticino/src/main/java/be/devlaminck/openwebnet/OWNUtilities.java
+++ bundles/binding/org.openhab.binding.bticino/src/main/java/be/devlaminck/openwebnet/OWNUtilities.java
@@ -38,7 +38,7 @@ public class OWNUtilities {
 
     /**
      * Create the frame to send to the own gateway
-     * 
+     *
      * @param c
      * @return
      */
@@ -53,7 +53,32 @@ public class OWNUtilities {
         what = c.getProperty("what");
         address = c.getProperty("address").split("\\*");
         where = address[0];
-        frame = "*" + who + "*" + what + "*" + where + "##";
+
+        if (who.equalsIgnoreCase("4")) {
+            // Heating Control
+            if (what.substring(0, 2).equalsIgnoreCase("14")) {
+                // Change Temperature Set Point
+                frame = "*#" + who + "*#" + where + "*#" + what + "##";
+            } else {
+                switch (Integer.parseInt(what)) {
+                    case 40:
+                        // Request Temperature Probe
+                        frame = "*" + who + "*" + what + "*" + where + "##";
+                        break;
+                    case 110:
+                        // Change to Manual Mode with fixed SP 20°C
+                        frame = "*#" + who + "*#" + where + "*#14*0200*3##";
+                        break;
+                    default:
+                        // Change Modes
+                        frame = "*" + who + "*" + what + "*#" + where + "##";
+                        break;
+                }
+            }
+        } else {
+            frame = "*" + who + "*" + what + "*" + where + "##";
+        }
+
         return (frame);
     }
 
@@ -61,7 +86,7 @@ public class OWNUtilities {
 
     /**
      * Get datetime on format "dd/MM/yyyy HH:mm:ss"
-     * 
+     *
      * @return
      */
     public static String getDateTime() {
@@ -74,18 +99,31 @@ public class OWNUtilities {
 
     /**
      * Convert temperature from bticino bus format to readable format
-     * 
+     *
      * @param temperature
      * @return
      */
     public static String convertTemperature(String temperature) {
         String temp = "";
-        if (!temperature.substring(0, 1).equalsIgnoreCase("0")) {
-            temp += "-";
+
+        switch (temperature.length()) {
+            case 1:
+                // for positive set point shift at probe
+                temp += temperature.substring(0, 1);
+                break;
+            case 2:
+                // for negative set point shift at probe
+                if (!temperature.substring(0, 1).equalsIgnoreCase("0")) {
+                    temp += "-";
+                }
+                temp += temperature.substring(1, 2);
+                break;
+            default:
+                temp += temperature.substring(1, 3);
+                temp += ".";
+                temp += temperature.substring(3);
+                break;
         }
-        temp += temperature.substring(1, 3);
-        temp += ".";
-        temp += temperature.substring(3);
         return (temp);
     }
 
@@ -93,7 +131,7 @@ public class OWNUtilities {
 
     /**
      * Convert day from number to name
-     * 
+     *
      * @param dayNumber
      * @return
      */
@@ -131,7 +169,7 @@ public class OWNUtilities {
 
     /**
      * Get model name from model number
-     * 
+     *
      * @param modelNumber
      * @return
      */
