009cc82493c1a7815ba723566f2487523656896c
Kai Kreuzer <kai@openhab.org>
2019-01-07 19:08:08 +0100

    updated to 2019
    Signed-off-by: Kai Kreuzer <kai@openhab.org>

--- bundles/action/org.openhab.action.telegram/src/main/java/org/openhab/action/telegram/internal/Telegram.java
+++ bundles/action/org.openhab.action.telegram/src/main/java/org/openhab/action/telegram/internal/Telegram.java
@@ -1,6 +1,6 @@
 /**
- * Copyright (c) 2010-2018 by the respective copyright holders.
- * <p>
+ * Copyright (c) 2010-2019 by the respective copyright holders.
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
--- bundles/binding/org.openhab.binding.cardio2e/src/main/java/org/openhab/binding/cardio2e/internal/code/Cardio2eDecoder.java
+++ bundles/binding/org.openhab.binding.cardio2e/src/main/java/org/openhab/binding/cardio2e/internal/code/Cardio2eDecoder.java
@@ -1,12 +1,11 @@
 /**
- * Copyright (c) 2010-2017, openHAB.org and others.
+ * Copyright (c) 2010-2019 by the respective copyright holders.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.openhab.binding.cardio2e.internal.code;
 
 import java.util.ArrayList;
