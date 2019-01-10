6e9aaaccb963a3b668648b1a3273e8cef6cc07d0
Kai Kreuzer <kai@openhab.org>
2019-01-10 09:37:30 +0100

    updated to 2019 (#5756)
    Signed-off-by: Kai Kreuzer <kai@openhab.org>

--- bundles/binding/org.openhab.binding.mqtt/src/main/java/org/openhab/binding/mqtt/internal/MqttEventBusBinding.java
+++ bundles/binding/org.openhab.binding.mqtt/src/main/java/org/openhab/binding/mqtt/internal/MqttEventBusBinding.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010-2018 by the respective copyright holders.
+ * Copyright (c) 2010-2019 by the respective copyright holders.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
