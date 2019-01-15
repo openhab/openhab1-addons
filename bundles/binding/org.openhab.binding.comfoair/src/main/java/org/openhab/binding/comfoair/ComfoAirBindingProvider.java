24c89bfaf2f83e3f851295c3b019e818f4676501
gieemek <gieemek666@gmail.com>
2019-01-15 08:57:01 +0100

    Add files via upload

--- bundles/binding/org.openhab.binding.comfoair/src/main/java/org/openhab/binding/comfoair/ComfoAirBindingProvider.java
+++ bundles/binding/org.openhab.binding.comfoair/src/main/java/org/openhab/binding/comfoair/ComfoAirBindingProvider.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010-2019 by the respective copyright holders.
+ * Copyright (c) 2010-2018 by the respective copyright holders.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -21,6 +21,8 @@ import org.openhab.core.binding.BindingProvider;
  *
  * @author Holger Hees
  * @since 1.3.0
+ * @author Grzegorz Miasko
+ * @since 1.14.0
  */
 public interface ComfoAirBindingProvider extends BindingProvider {
 
