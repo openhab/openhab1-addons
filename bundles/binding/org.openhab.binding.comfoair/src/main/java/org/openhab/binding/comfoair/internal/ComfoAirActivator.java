fe314f963b48268d4ef966e5d326cc62451f2ff5
gieemek <gieemek666@gmail.com>
2019-01-23 22:34:23 +0100

    Add files via upload

--- bundles/binding/org.openhab.binding.comfoair/src/main/java/org/openhab/binding/comfoair/internal/ComfoAirActivator.java
+++ bundles/binding/org.openhab.binding.comfoair/src/main/java/org/openhab/binding/comfoair/internal/ComfoAirActivator.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010-2019 by the respective copyright holders.
+ * Copyright (c) 2010-2017 by the respective copyright holders.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -45,7 +45,7 @@ public final class ComfoAirActivator implements BundleActivator {
 
     /**
      * Returns the bundle context of this bundle
-     * 
+     *
      * @return the bundle context
      */
     public static BundleContext getContext() {
