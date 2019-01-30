9b9a344d97045d8ecfd171323ce64569052ef5b7
Guenther Schreiner <guenther.schreiner@smile.de>
2019-01-30 08:41:12 +0100

    fix: clean handling of unrare situation where bridge goes into idle mode

--- bundles/binding/org.openhab.binding.velux/src/main/java/org/openhab/binding/velux/bridge/slip/SlipVeluxBridge.java
+++ bundles/binding/org.openhab.binding.velux/src/main/java/org/openhab/binding/velux/bridge/slip/SlipVeluxBridge.java
@@ -187,6 +187,7 @@ public class SlipVeluxBridge extends VeluxBridge {
         logger.info("io(): socket I/O failed {} times.", bridgeInstance.veluxBridgeConfiguration().retries);
         logger.trace("io(): shutting down connection.");
         connectivity.close();
+        connectivity = null;
         logger.trace("io() finishes with failure by throwing exception.");
         throw lastIOE;
     }
