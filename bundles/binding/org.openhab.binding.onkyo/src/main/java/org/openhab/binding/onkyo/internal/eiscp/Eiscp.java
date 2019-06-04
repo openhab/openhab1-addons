da95b35c394c1b24d048d15df76d0a88b04da0f0
Wouter Born <github@maindrain.net>
2019-06-04 23:10:39 +0200

    Update license to EPL-2.0
    Updates the license headers to EPL-2.0.
    Also updates the LICENSE file and adds a NOTICE files for each add-on.
    
    Signed-off-by: Wouter Born <github@maindrain.net>

--- bundles/binding/org.openhab.binding.onkyo/src/main/java/org/openhab/binding/onkyo/internal/eiscp/Eiscp.java
+++ bundles/binding/org.openhab.binding.onkyo/src/main/java/org/openhab/binding/onkyo/internal/eiscp/Eiscp.java
@@ -1,10 +1,14 @@
 /**
- * Copyright (c) 2010-2019 by the respective copyright holders.
+ * Copyright (c) 2010-2019 Contributors to the openHAB project
  *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
  */
 package org.openhab.binding.onkyo.internal.eiscp;
 
