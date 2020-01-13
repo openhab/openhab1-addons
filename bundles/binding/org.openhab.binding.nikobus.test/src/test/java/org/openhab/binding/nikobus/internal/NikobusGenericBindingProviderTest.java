fcfb93b26826bd61ad89f68eaefbf9c606612fe9
Wouter Born <github@maindrain.net>
2020-01-13 14:10:10 +0100

    Update copyright headers to 2020 (#5927)
    Signed-off-by: Wouter Born <github@maindrain.net>

--- bundles/binding/org.openhab.binding.nikobus.test/src/test/java/org/openhab/binding/nikobus/internal/NikobusGenericBindingProviderTest.java
+++ bundles/binding/org.openhab.binding.nikobus.test/src/test/java/org/openhab/binding/nikobus/internal/NikobusGenericBindingProviderTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010-2019 Contributors to the openHAB project
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information.
